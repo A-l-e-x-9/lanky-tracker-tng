ce17ac607ffd3decb5d069d2b2ec07b10d30dc4b
A-l-e-x-99 <alexs23743@gmail.com>
2025-09-02 09:44:16 -0400

    Add files via upload

--- src/renderer/src/components/levels/4-galleon/enemies/Caverns.tsx
+++ src/renderer/src/components/levels/4-galleon/enemies/Caverns.tsx
@@ -1,7 +1,7 @@
 import DropPool from '@renderer/components/pools/Drops'
 import { useDefeatKaboom, useDefeatKlobber, useDefeatKosha } from '@renderer/hooks/enemies'
 import { usePlayGalleon } from '@renderer/hooks/galleon'
-import { useAnyKong } from '@renderer/hooks/kongs'
+import { useAnyKong, useCamera } from '@renderer/hooks/kongs'
 import GalleonCheck from '../check'
 
 const CavernsEnemies: React.FC = () => {
@@ -10,6 +10,7 @@ const CavernsEnemies: React.FC = () => {
   const kaboom = useDefeatKaboom()
   const kosha = useDefeatKosha()
   const anyKong = useAnyKong()
+  const hasFairyCam = useCamera()
   return (
     <DropPool>
       <GalleonCheck
@@ -47,6 +48,42 @@ const CavernsEnemies: React.FC = () => {
         name="Galleon Enemy: Coconut Tunnel"
         region="Galleon Caverns"
         canGetLogic={inStage && anyKong}
+      />
+      <GalleonCheck
+        id={4400}
+        name="Galleon Enemy Photo: Chest Room 0"
+        region="Galleon Caverns"
+        canGetLogic={inStage && klobber && hasCamera}
+      />
+      <GalleonCheck
+        id={4401}
+        name="Galleon Enemy Photo: Chest Room 1"
+        region="Galleon Caverns"
+        canGetLogic={inStage && kaboom && hasCamera}
+      />
+      <GalleonCheck
+        id={4402}
+        name="Galleon Enemy Photo: Vine Cannon"
+        region="Galleon Caverns"
+        canGetLogic={inStage && kaboom && hasCamera}
+      />
+      <GalleonCheck
+        id={4403}
+        name="Galleon Enemy Photo: Cranky Cannon"
+        region="Galleon Caverns"
+        canGetLogic={inStage && kaboom && hasCamera}
+      />
+      <GalleonCheck
+        id={4404}
+        name="Galleon Enemy Photo: Peanut Tunnel"
+        region="Galleon Caverns"
+        canGetLogic={inStage && kosha && hasCamera}
+      />
+      <GalleonCheck
+        id={4406}
+        name="Galleon Enemy Photo: Coconut Tunnel"
+        region="Galleon Caverns"
+        canGetLogic={inStage && anyKong && hasCamera}
       />
     </DropPool>
   )
--- src/renderer/src/components/levels/4-galleon/enemies/index.tsx
+++ src/renderer/src/components/levels/4-galleon/enemies/index.tsx
@@ -1,10 +1,12 @@
 import CavernsEnemies from './Caverns'
 import LighthouseEnemies from './Lighthouse'
+import UnderWaterEnemies from './Underwater'
 
 const EnemyLocations: React.FC = () => (
   <>
     <CavernsEnemies />
     <LighthouseEnemies />
+    <UnderwaterEnemies />
   </>
 )
 
