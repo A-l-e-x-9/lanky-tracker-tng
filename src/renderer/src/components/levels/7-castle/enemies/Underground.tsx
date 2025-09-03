df0f17f7ec0d6db6dfbe4a2c37c868a6929e9639
A-l-e-x-99 <alexs23743@gmail.com>
2025-09-03 07:34:42 -0400

    Add files via upload

--- src/renderer/src/components/levels/7-castle/enemies/Underground.tsx
+++ src/renderer/src/components/levels/7-castle/enemies/Underground.tsx
@@ -12,7 +12,8 @@ import {
   usePeanut,
   usePineapple,
   useSlam,
-  useClimbing
+  useClimbing,
+  useCamera
 } from '@renderer/hooks/kongs'
 import CastleCheck from '../check'
 
@@ -33,6 +34,7 @@ const UndergroundEnemies: React.FC = () => {
   const feather = useFeather()
   const hasClimbing = useClimbing()
   const preOpenedCrypt = useOpenCrypt()
+  const hasFairyCam = useCamera()
   return (
     <DropPool>
       <CastleCheck
@@ -217,6 +219,189 @@ const UndergroundEnemies: React.FC = () => {
         name="Castle Enemy: Dungeon Outside Lanky Room"
         region="Castle Underground"
         canGetLogic={inStage && anyKong && kosha}
+      />
+      <CastleCheck
+        id={7427}
+        name="Castle Enemy Photo: Low Cave Near Crypt"
+        region="Castle Underground"
+        canGetLogic={inStage && anyKong && kosha && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && anyKong && kosha && hasFairyCam}
+      />
+      <CastleCheck
+        id={7428}
+        name="Castle Enemy Photo: Low Cave Near Stair Right"
+        region="Castle Underground"
+        canGetLogic={inStage && anyKong && kosha && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && anyKong && kosha && hasFairyCam}
+      />
+      <CastleCheck
+        id={7420}
+        name="Castle Enemy Photo: Low Cave Near Stair Left"
+        region="Castle Underground"
+        canGetLogic={inStage && anyKong && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && anyKong && hasFairyCam}
+      />
+      <CastleCheck
+        id={7430}
+        name="Castle Enemy Photo: Low Cave Near Mausoleum"
+        region="Castle Underground"
+        canGetLogic={inStage && anyKong && bat && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && anyKong && bat && hasFairyCam}
+      />
+      <CastleCheck
+        id={7431}
+        name="Castle Enemy Photo: Low Cave Near Funky"
+        region="Castle Underground"
+        canGetLogic={inStage && anyKong && bat && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && anyKong && bat && hasFairyCam}
+      />
+      <CastleCheck
+        id={7432}
+        name="Castle Enemy Photo: Low Cave Near Tag"
+        region="Castle Underground"
+        canGetLogic={inStage && anyKong && bat && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && anyKong && bat && hasFairyCam}
+      />
+      <CastleCheck
+        id={7433}
+        name="Castle Enemy Photo: Crypt Diddy Coffin 0"
+        region="Castle Underground"
+        canGetLogic={inStage && (peanut || preOpenedCrypt) && charge && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && (peanut || preOpenedCrypt) && charge && hasFairyCam}
+      />
+      <CastleCheck
+        id={7434}
+        name="Castle Enemy Photo: Crypt Diddy Coffin 1"
+        region="Castle Underground"
+        canGetLogic={inStage && (peanut || preOpenedCrypt) && charge && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && (peanut || preOpenedCrypt) && charge && hasFairyCam}
+      />
+      <CastleCheck
+        id={7435}
+        name="Castle Enemy Photo: Crypt Diddy Coffin 2"
+        region="Castle Underground"
+        canGetLogic={inStage && (peanut || preOpenedCrypt) && charge && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && (peanut || preOpenedCrypt) && charge && hasFairyCam}
+      />
+      <CastleCheck
+        id={7436}
+        name="Castle Enemy Photo: Crypt Diddy Coffin 3"
+        region="Castle Underground"
+        canGetLogic={inStage && (peanut || preOpenedCrypt) && charge && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && (peanut || preOpenedCrypt) && charge && hasFairyCam}
+      />
+      <CastleCheck
+        id={7437}
+        name="Castle Enemy Photo: Crypt Chunky Coffin 0"
+        region="Castle Underground"
+        canGetLogic={inStage && (pineapple || preOpenedCrypt) && slam && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && (pineapple || preOpenedCrypt) && slam && hasFairyCam}
+      />
+      <CastleCheck
+        id={7438}
+        name="Castle Enemy Photo: Crypt Chunky Coffin 1"
+        region="Castle Underground"
+        canGetLogic={inStage && (pineapple || preOpenedCrypt) && slam && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && (pineapple || preOpenedCrypt) && slam && hasFairyCam}
+      />
+      <CastleCheck
+        id={7439}
+        name="Castle Enemy Photo: Crypt Chunky Coffin 2"
+        region="Castle Underground"
+        canGetLogic={inStage && (pineapple || preOpenedCrypt) && slam && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && (pineapple || preOpenedCrypt) && slam && hasFairyCam}
+      />
+      <CastleCheck
+        id={7440}
+        name="Castle Enemy Photo: Crypt Chunky Coffin 3"
+        region="Castle Underground"
+        canGetLogic={inStage && (pineapple || preOpenedCrypt) && slam && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && (pineapple || preOpenedCrypt) && slam && hasFairyCam}
+      />
+      <CastleCheck
+        id={7441}
+        name="Castle Enemy Photo: Crypt Minecart Entry"
+        region="Castle Underground"
+        canGetLogic={inStage && (coconut || preOpenedCrypt) && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && (coconut || preOpenedCrypt) && hasFairyCam}
+      />
+      <CastleCheck
+        id={7442}
+        name="Castle Enemy Photo: Crypt Fork"
+        region="Castle Underground"
+        canGetLogic={inStage && (coconut || peanut || pineapple || preOpenedCrypt) && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && (coconut || peanut || pineapple || preOpenedCrypt) && hasFairyCam}
+      />
+      <CastleCheck
+        id={7443}
+        name="Castle Enemy Photo: Crypt Near Diddy"
+        region="Castle Underground"
+        canGetLogic={inStage && (coconut || peanut || pineapple || preOpenedCrypt) && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && (coconut || peanut || pineapple || preOpenedCrypt) && hasFairyCam}
+      />
+      <CastleCheck
+        id={7444}
+        name="Castle Enemy Photo: Crypt Near Chunky"
+        region="Castle Underground"
+        canGetLogic={inStage && (coconut || peanut || pineapple || preOpenedCrypt) && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && (coconut || peanut || pineapple || preOpenedCrypt) && hasFairyCam}
+      />
+      <CastleCheck
+        id={7445}
+        name="Castle Enemy Photo: Mausoleum Tiny"
+        region="Castle Underground"
+        canGetLogic={inStage && (feather || grape || preOpenedCrypt) && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && (feather || grape || preOpenedCrypt) && hasFairyCam}
+      />
+      <CastleCheck
+        id={7446}
+        name="Castle Enemy Photo: Mausoleum Lanky Switch"
+        region="Castle Underground"
+        canGetLogic={inStage && (feather || grape || preOpenedCrypt) && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && (feather || grape || preOpenedCrypt) && hasFairyCam}
+      />
+      <CastleCheck
+        id={7447}
+        name="Castle Enemy Photo: Mausoleum Lanky Gate"
+        region="Castle Underground"
+        canGetLogic={inStage && (feather || grape || preOpenedCrypt) && hasClimbing && hasFairyCam}
+        canGetBreak={inStage && (feather || grape || preOpenedCrypt) && hasFairyCam}
+      />
+      <CastleCheck
+        id={7448}
+        name="Castle Enemy Photo: Upper Cave Near Dungeon"
+        region="Castle Underground"
+        canGetLogic={inStage && anyKong && bat && hasFairyCam}
+      />
+      <CastleCheck
+        id={7450}
+        name="Castle Enemy Photo: Upper Cave Near Warp 1 Entrance"
+        region="Castle Underground"
+        canGetLogic={inStage && anyKong && bat && hasFairyCam}
+      />
+      <CastleCheck
+        id={7451}
+        name="Castle Enemy Photo: Upper Cave Near Central Warp Area Entrance"
+        region="Castle Underground"
+        canGetLogic={inStage && anyKong && hasFairyCam}
+      />
+      <CastleCheck
+        id={7419}
+        name="Castle Enemy Photo: Dungeon Face Room"
+        region="Castle Underground"
+        canGetLogic={inStage && dk && canSlam && hasFairyCam}
+      />
+      <CastleCheck
+        id={7420}
+        name="Castle Enemy Photo: Dungeon Chair Room"
+        region="Castle Underground"
+        canGetLogic={inStage && diddy && canSlam && kosha && hasFairyCam}
+      />
+      <CastleCheck
+        id={7421}
+        name="Castle Enemy Photo: Dungeon Outside Lanky Room"
+        region="Castle Underground"
+        canGetLogic={inStage && anyKong && kosha && hasFairyCam}
       />
     </DropPool>
   )
