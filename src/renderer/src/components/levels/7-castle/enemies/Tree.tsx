9ca8104e1ed72492be7f4ddc900bf3559df4e304
A-l-e-x-99 <alexs23743@gmail.com>
2025-09-03 07:28:11 -0400

    Add files via upload

--- src/renderer/src/components/levels/7-castle/enemies/Tree.tsx
+++ src/renderer/src/components/levels/7-castle/enemies/Tree.tsx
@@ -1,11 +1,13 @@
 import DropPool from '@renderer/components/pools/Drops'
 import { useCastleTree } from '@renderer/hooks/castle'
 import { useDefeatBat } from '@renderer/hooks/enemies'
+import { useCamera } from '@renderer/hooks/kongs'
 import CastleCheck from '../check'
 
 const TreeEnemies: React.FC = () => {
   const tree = useCastleTree()
   const bat = useDefeatBat()
+  const hasFairyCam = useCamera()
   return (
     <DropPool>
       <CastleCheck
@@ -19,6 +21,18 @@ const TreeEnemies: React.FC = () => {
         name="Castle Enemy: Tree Start 1"
         region="Castle Surroundings"
         canGetLogic={tree && bat}
+      />
+      <CastleCheck
+        id={7470}
+        name="Castle Enemy Photo: Tree Start 0"
+        region="Castle Surroundings"
+        canGetLogic={tree && bat && hasFairyCam}
+      />
+      <CastleCheck
+        id={7471}
+        name="Castle Enemy Photo: Tree Start 1"
+        region="Castle Surroundings"
+        canGetLogic={tree && bat && hasFairyCam}
       />
     </DropPool>
   )
