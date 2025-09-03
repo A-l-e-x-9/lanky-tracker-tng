2bb34ddbafe0c49f05ed0faf954efcd9d62ef733
A-l-e-x-99 <alexs23743@gmail.com>
2025-09-03 07:40:09 -0400

    Add files via upload

--- src/renderer/src/components/levels/8-helm/enemies/Doors.tsx
+++ src/renderer/src/components/levels/8-helm/enemies/Doors.tsx
@@ -1,10 +1,11 @@
 import { useHelmDoors } from '@renderer/hooks/helm'
-import { useAnyKong } from '@renderer/hooks/kongs'
+import { useAnyKong, useCamera } from '@renderer/hooks/kongs'
 import HelmCheck from '../check'
 
 const HelmDoorsEnemies: React.FC = () => {
   const helmDoors = useHelmDoors()
   const anyKong = useAnyKong()
+  const hasFairyCam = useCamera()
   return (
     <>
       <HelmCheck
@@ -18,6 +19,18 @@ const HelmDoorsEnemies: React.FC = () => {
         name="Helm Enemy: Nav Room Right"
         canGetLogic={helmDoors.in && anyKong}
         canGetBreak={helmDoors.out && anyKong}
+      />
+      <HelmCheck
+        id={8417}
+        name="Helm Enemy Photo: Nav Room Left"
+        canGetLogic={helmDoors.in && anyKong && hasFairyCam}
+        canGetBreak={helmDoors.out && anyKong && hasFairyCam}
+      />
+      <HelmCheck
+        id={8418}
+        name="Helm Enemy Photo: Nav Room Right"
+        canGetLogic={helmDoors.in && anyKong && hasFairyCam}
+        canGetBreak={helmDoors.out && anyKong && hasFairyCam}
       />
     </>
   )
