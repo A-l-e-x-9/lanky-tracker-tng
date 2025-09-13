b6531c0d3eb520398b3bf7584d7f309da4a5523e
A-l-e-x-9 <alexs23743@gmail.com>
2025-09-13 00:52:16 +0000

    Deploying to gh-pages from @ A-l-e-x-9/lanky-tracker-tng@9aec320907b49112a3062ffce64645c0b6e76ec8 🚀

--- assets/index-DnXy1TEQ.js
+++ assets/index-DnXy1TEQ.js
@@ -11898,6 +11898,7 @@ const useCurrentBananaMedalCount = () => useDonkStore(useShallow((state) => stat
 const useCurrentPearlCount = () => useDonkStore(useShallow((state) => state.consumables.pearls));
 const useCurrentFairyCount = () => useDonkStore(useShallow((state) => state.consumables.fairies));
 const useBean = () => useDonkStore(useShallow((state) => state.consumables.bean));
+const useCurrentGBCount = () => useDonkStore(useShallow((state) => state.consumables.goldBananas));
 const useFastArcade = () => useDonkStore(useShallow((state) => state.fastChecks.factoryArcade));
 const useFastMermaid = () => useDonkStore(useShallow((state) => state.fastChecks.galleonMermaid));
 const usePlayFactory = () => usePlayLevel("Frantic Factory");
@@ -13598,29 +13599,51 @@ const usePlayLevel = (level) => {
   const islesKremTop = useIslesKremTop();
   const islesKremAscent = useIslesKremAscent();
   const islesFungiIsland = useIslesFungiIsland();
+  const currentGB = useCurrentGBCount();
+  const [
+    bLocker1,
+    bLocker2,
+    bLocker3,
+    bLocker4,
+    bLocker5,
+    bLocker6,
+    bLocker7,
+    bLocker8
+  ] = useDonkStore(
+    useShallow((state) => [
+      state.bLocker.bLocker1,
+      state.bLocker.bLocker2,
+      state.bLocker.bLocker3,
+      state.bLocker.bLocker4,
+      state.bLocker.bLocker5,
+      state.bLocker.bLocker6,
+      state.bLocker.bLocker7,
+      state.bLocker.bLocker8
+    ])
+  );
   if (level1 === level) {
-    return true;
+    return currentGB >= bLocker1;
   }
   if (level2 === level) {
-    return islesUpper && (openLobbies || key1);
+    return currentGB >= bLocker2 && islesUpper && (openLobbies || key1);
   }
   if (level3 === level) {
-    return islesKremAscent && (openLobbies || key2);
+    return currentGB >= bLocker3 && islesKremAscent && (openLobbies || key2);
   }
   if (level4 === level) {
-    return (openLobbies || key2) && dive;
+    return currentGB >= bLocker4 && (openLobbies || key2) && dive;
   }
   if (level5 === level) {
-    return islesFungiIsland;
+    return currentGB >= bLocker5 && islesFungiIsland;
   }
   if (level6 === level) {
-    return islesUpper && (openLobbies || key5);
+    return currentGB >= bLocker6 && islesUpper && (openLobbies || key5);
   }
   if (level7 === level) {
-    return openLobbies || key5;
+    return currentGB >= bLocker7 && (openLobbies || key5);
   }
   if (level8 === level) {
-    return islesKremTop && (openLobbies || key6 && key7);
+    return currentGB >= bLocker8 && islesKremTop && (openLobbies || key6 && key7);
   }
   return false;
 };
