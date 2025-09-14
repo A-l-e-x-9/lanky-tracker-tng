fad8692db26cb5036255c393c7e09b9a23985af3
A-l-e-x-99 <alexs23743@gmail.com>
2025-09-14 19:06:19 -0400

    Add files via upload
    Starting to fix the problem of some checks not showing up when you click "group by region".

--- src/renderer/src/components/levels/0-isles/regions/AztecLobby.tsx
+++ src/renderer/src/components/levels/0-isles/regions/AztecLobby.tsx
@@ -1,5 +1,19 @@
 import AztecBarrel from '../gold-bananas/tiny/AztecBarrel'
+import AztecLobbyDK from '../wrinkly/AztecLobbyDK'
+import AztecLobbyDiddy from '../wrinkly/AztecLobbyDiddy'
+import AztecLobbyLanky from '../wrinkly/AztecLobbyLanky'
+import AztecLobbyTiny from '../wrinkly/AztecLobbyTiny'
+import AztecLobbyChunky from '../wrinkly/AztecLobbyChunky'
 
-const AztecLobbyChecks: React.FC = () => <AztecBarrel />
+const AztecLobbyChecks: React.FC = () => (
+<>
+  <AztecBarrel />
+  <AztecLobbyDK />
+  <AztecLobbyDiddy />
+  <AztecLobbyLanky />
+  <AztecLobbyTiny />
+  <AztecLobbyChunky />
+</>
+)
 
 export default AztecLobbyChecks
