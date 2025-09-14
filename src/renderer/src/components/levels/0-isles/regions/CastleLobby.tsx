a962a7a1a728c8f28e3a775533ada4d9cb87eb7a
A-l-e-x-99 <alexs23743@gmail.com>
2025-09-14 19:13:51 -0400

    Adding new checks.

--- src/renderer/src/components/levels/0-isles/regions/CastleLobby.tsx
+++ src/renderer/src/components/levels/0-isles/regions/CastleLobby.tsx
@@ -2,12 +2,24 @@ import CastleLobby from '../dirt/CastleLobby'
 import CastleLobbyEnemies from '../enemies/CastleLobby'
 import CastleBarrel from '../gold-bananas/lanky/CastleBarrel'
 import DiddyKasplat from '../kasplats/Diddy'
+import CastleLobbyBoulder from '../boulders/CastleLobby'
+import CastleLobbyDK from '../wrinkly/CastleLobbyDK'
+import CastleLobbyDiddy from '../wrinkly/CastleLobbyDiddy'
+import CastleLobbyLanky from '../wrinkly/CastleLobbyLanky'
+import CastleLobbyTiny from '../wrinkly/CastleLobbyTiny'
+import CastleLobbyChunky from '../wrinkly/CastleLobbyChunky'
 
 const CastleLobbyChecks: React.FC = () => (
   <>
     <CastleBarrel />
     <DiddyKasplat />
     <CastleLobby />
+    <CastleLobbyBoulder />
+    <CastleLobbyDK />
+    <CastleLobbyDiddy />
+    <CastleLobbyLanky />
+    <CastleLobbyTiny />
+    <CastleLobbyChunky />
     <CastleLobbyEnemies />
   </>
 )
