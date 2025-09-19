62793ed138e81826bdb85e8f0422138f8ba76a5d
A-l-e-x-99 <alexs23743@gmail.com>
2025-09-19 07:59:25 -0400

    Add files via upload

--- src/renderer/src/components/levels/7-castle/enemies/Library.tsx
+++ src/renderer/src/components/levels/7-castle/enemies/Library.tsx
@@ -16,49 +16,89 @@ const LibraryEnemies: React.FC = () => {
         id={7357}
         name="Castle Enemy: Library Gauntlet Fork Left 0"
         region="Castle Rooms"
-        canGetLogic={inStage && dk && canSlam && bat}
+        canGetLogic={inStage && dk && canSlam && bat.in}
+        canGetBreak={inStage && dk && canSlam && bat.out}
       />
       <CastleCheck
         id={7358}
         name="Castle Enemy: Library Gauntlet Fork Left 1"
         region="Castle Rooms"
-        canGetLogic={inStage && dk && canSlam && bat}
+        canGetLogic={inStage && dk && canSlam && bat.in}
+        canGetBreak={inStage && dk && canSlam && bat.out}
       />
       <CastleCheck
         id={7359}
         name="Castle Enemy: Library Gauntlet Fork Center"
         region="Castle Rooms"
-        canGetLogic={inStage && dk && canSlam && bat}
+        canGetLogic={inStage && dk && canSlam && bat.in}
+        canGetBreak={inStage && dk && canSlam && bat.out}
       />
       <CastleCheck
         id={7360}
         name="Castle Enemy: Library Gauntlet Fork Right"
         region="Castle Rooms"
-        canGetLogic={inStage && dk && canSlam && bat}
+        canGetLogic={inStage && dk && canSlam && bat.in}
+        canGetBreak={inStage && dk && canSlam && bat.out}
       />
       <CastleCheck
         id={7457}
         name="Castle Enemy Photo: Library Gauntlet Fork Left 0"
         region="Castle Rooms"
-        canGetLogic={inStage && dk && canSlam && bat && hasFairyCam}
+        canGetLogic={inStage && dk && canSlam && hasFairyCam}
       />
       <CastleCheck
         id={7458}
         name="Castle Enemy Photo: Library Gauntlet Fork Left 1"
         region="Castle Rooms"
-        canGetLogic={inStage && dk && canSlam && bat && hasFairyCam}
+        canGetLogic={inStage && dk && canSlam && hasFairyCam}
       />
       <CastleCheck
         id={7459}
         name="Castle Enemy Photo: Library Gauntlet Fork Center"
         region="Castle Rooms"
-        canGetLogic={inStage && dk && canSlam && bat && hasFairyCam}
+        canGetLogic={inStage && dk && canSlam && hasFairyCam}
       />
       <CastleCheck
         id={7460}
         name="Castle Enemy Photo: Library Gauntlet Fork Right"
         region="Castle Rooms"
-        canGetLogic={inStage && dk && canSlam && bat && hasFairyCam}
+        canGetLogic={inStage && dk && canSlam && hasFairyCam}
+      />
+      <CastleCheck
+        id={7461}
+        name="Castle Enemy Photo: Library Book 0"
+        region="Castle Rooms"
+        canGetLogic={inStage && dk && canSlam && hasFairyCam}
+      />
+      <CastleCheck
+        id={7462}
+        name="Castle Enemy Photo: Library Book 1"
+        region="Castle Rooms"
+        canGetLogic={inStage && dk && canSlam && hasFairyCam}
+      />
+      <CastleCheck
+        id={7463}
+        name="Castle Enemy Photo: Library Book 2"
+        region="Castle Rooms"
+        canGetLogic={inStage && dk && canSlam && hasFairyCam}
+      />
+      <CastleCheck
+        id={7464}
+        name="Castle Enemy Photo: Library Book 3"
+        region="Castle Rooms"
+        canGetLogic={inStage && dk && canSlam && hasFairyCam}
+      />
+      <CastleCheck
+        id={7465}
+        name="Castle Enemy Photo: Library Book 4"
+        region="Castle Rooms"
+        canGetLogic={inStage && dk && canSlam && hasFairyCam}
+      />
+      <CastleCheck
+        id={7466}
+        name="Castle Enemy Photo: Library Book 5"
+        region="Castle Rooms"
+        canGetLogic={inStage && dk && canSlam && hasFairyCam}
       />
     </DropPool>
   )
