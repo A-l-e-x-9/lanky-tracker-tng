037e4dc7fc75014633bfdd00f53ac34b8e5e1b4f
A-l-e-x-99 <alexs23743@gmail.com>
2025-09-21 18:02:40 -0400

    Add files via upload

--- src/renderer/src/components/levels/0-isles/wrinkly/AztecLobbyChunky.tsx
+++ src/renderer/src/components/levels/0-isles/wrinkly/AztecLobbyChunky.tsx
@@ -1,5 +1,5 @@
-import WrinklyPool from '@renderer/components/pools/WrinklyDoors'
-import { useAztecLobbyChunky } from '@renderer/hooks/isles'
+import WrinklyPool from '@renderer/src/components/pools/WrinklyDoors'
+import { useAztecLobbyChunky } from '@renderer/src/hooks/isles'
 import IslesCheck from '../check'
 
 /*Since Version 4 of the DK64 Randomizer, you can have major rewards in Wrinkly Kong's hint doors (and hints on checks).*/
