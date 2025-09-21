71a8bb32d950c136ffe440334e7b0bcf71211ade
A-l-e-x-99 <alexs23743@gmail.com>
2025-09-21 19:56:08 -0400

    Add files via upload

--- src/renderer/src/components/levels/1-japes/boss.tsx
+++ src/renderer/src/components/levels/1-japes/boss.tsx
@@ -1,6 +1,6 @@
-import BossPool from '@renderer/components/pools/Bosses'
-import { useJapesRambi, useJapesSideArea, usePlayJapes } from '@renderer/hooks/japes'
-import { useAnyKong, useClimbing, useVine } from '@renderer/hooks/kongs'
+import BossPool from '@renderer/src/components/pools/Bosses'
+import { useJapesRambi, useJapesSideArea, usePlayJapes } from '@renderer/src/hooks/japes'
+import { useAnyKong, useClimbing, useVine } from '@renderer/src/hooks/kongs'
 import JapesCheck from './check'
 
 const BossCheck: React.FC = () => {
