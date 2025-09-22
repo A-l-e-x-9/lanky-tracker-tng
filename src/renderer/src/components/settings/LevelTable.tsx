a6ec38637bab76e26757398e8fa3f14f971889e5
A-l-e-x-99 <alexs23743@gmail.com>
2025-09-21 23:08:27 -0400

    Update LevelTable.tsx

--- src/renderer/src/components/settings/LevelTable.tsx
+++ src/renderer/src/components/settings/LevelTable.tsx
@@ -1,4 +1,4 @@
-import useDonkStore from '@renderer/src/src/store'
+import useDonkStore from '@renderer/src/store'
 import { useShallow } from 'zustand/react/shallow'
 
 import SimpleKey from '@renderer/src/components/settings/SimpleKey'
