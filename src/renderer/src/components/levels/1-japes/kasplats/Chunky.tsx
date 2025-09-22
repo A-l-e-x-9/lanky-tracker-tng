0aa406f084becd7d8a256c1df40a7a990ff1a17f
A-l-e-x-99 <alexs23743@gmail.com>
2025-09-21 20:30:46 -0400

    Add files via upload

--- src/renderer/src/components/levels/1-japes/kasplats/Chunky.tsx
+++ src/renderer/src/components/levels/1-japes/kasplats/Chunky.tsx
@@ -1,6 +1,6 @@
-import KasplatPool from '@renderer/components/pools/Kasplats'
-import VanillaKasplat from '@renderer/components/pools/VanillaKasplat'
-import { useChunkyKasplat } from '@renderer/hooks/japes'
+import KasplatPool from '@renderer/src/components/pools/Kasplats'
+import VanillaKasplat from '@renderer/src/components/pools/VanillaKasplat'
+import { useChunkyKasplat } from '@renderer/src/hooks/japes'
 import JapesCheck from '../check'
 
 const ChunkyKasplat: React.FC = () => {
