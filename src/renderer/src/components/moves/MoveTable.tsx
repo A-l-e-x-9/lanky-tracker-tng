7e0b260099af859700d45e5bf43136c41e035142
A-l-e-x-99 <alexs23743@gmail.com>
2025-09-21 22:57:11 -0400

    Add files via upload

--- src/renderer/src/components/moves/MoveTable.tsx
+++ src/renderer/src/components/moves/MoveTable.tsx
@@ -1,4 +1,4 @@
-import useDonkStore from '@renderer/store'
+import useDonkStore from '@renderer/src/store'
 import { useShallow } from 'zustand/react/shallow'
 
 import CountSelector from '../settings/CountSelector'
