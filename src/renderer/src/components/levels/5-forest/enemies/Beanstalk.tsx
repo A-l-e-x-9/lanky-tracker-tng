b4ddd73e323452d2c63dbf3491567efff7d9fe22
A-l-e-x-99 <alexs23743@gmail.com>
2025-10-07 18:03:16 -0400

    More bug fixing

--- src/renderer/src/components/levels/5-forest/enemies/Beanstalk.tsx
+++ src/renderer/src/components/levels/5-forest/enemies/Beanstalk.tsx
@@ -1,52 +1,49 @@
 import DropPool from '@renderer/components/pools/Drops'
-import { useDefeatKlump, useDefeatZinger } from '@renderer/hooks/enemies'
-import { useForestBean, useForestBeanHalf, usePlayForest } from '@renderer/hooks/forest'
+import { useDefeatKlump } from '@renderer/hooks/enemies'
+import { useForestBean } from '@renderer/hooks/forest'
 import ForestCheck from '../check'
 import { useHunky } from '@renderer/hooks/kongs'
 
 const BeanstalkEnemies: React.FC = () => {
-  const inStage = usePlayForest()
-  const zinger = useDefeatZinger()
   const klump = useDefeatKlump()
   const beanstalk = useForestBean()
-  const beanHalf = useForestBeanHalf()
   const hasHunky = useHunky()
   return (
     <DropPool>
       <ForestCheck
         id={5317}
         name="Enemy 0 at Beanstalk"
-        region="Forest Center And Beanstalk"
+        region="Forest Area 2"
         canGetLogic={beanstalk && klump}
       />
       <ForestCheck
         id={5318}
         name="Enemy 1 at Beanstalk"
-        region="Forest Center And Beanstalk"
+        region="Forest Area 2"
         canGetLogic={beanstalk && klump}
       />
       <ForestCheck
         id={5319}
         name="Giant Tomato 0"
-        region="Forest Center And Beanstalk"
+        region="Forest Area 2"
         canGetLogic={beanstalk && hasHunky}
       />
       <ForestCheck
         id={5320}
         name="Giant Tomato 1"
-        region="Forest Center And Beanstalk"
+        region="Forest Area 2"
         canGetLogic={beanstalk && hasHunky}
       />
       <ForestCheck
         id={5321}
         name="Giant Tomato 2"
-        region="Forest Center And Beanstalk"
+        region="Forest Area 2"
         canGetLogic={beanstalk && hasHunky}
       />
       <ForestCheck
         id={5322}
         name="Giant Tomato 3"
-        region="Forest Center And Beanstalk"
+        region="Forest Area 2"
         canGetLogic={beanstalk && hasHunky}
       />
     </DropPool>
