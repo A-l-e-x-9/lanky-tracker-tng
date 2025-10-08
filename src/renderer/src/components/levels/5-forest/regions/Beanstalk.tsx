a25f932551c1272ac046a3ce1a893f8a2d530ed7
A-l-e-x-99 <alexs23743@gmail.com>
2025-10-07 22:52:02 -0400

    Bug fixing

--- src/renderer/src/components/levels/5-forest/regions/Beanstalk.tsx
+++ src/renderer/src/components/levels/5-forest/regions/Beanstalk.tsx
@@ -1,6 +1,7 @@
 import BeanstalkDirt from '../dirt/Beanstalk'
 import ChunkyApple from '../gold-bananas/chunky/Apple'
 import BeanCheck from '../gold-bananas/tiny/BeanCheck'
+import BeanstalkEnemies from '../enemies/Beanstalk'
 
 const BeanstalkChecks: React.FC = () => (
   <>
