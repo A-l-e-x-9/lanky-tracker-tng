c0f6651780a898f3168343bc6b47f950a3af29a6
A-l-e-x-99 <alexs23743@gmail.com>
2025-10-09 22:47:56 -0400

    More renaming of checks

--- src/renderer/src/components/levels/8-helm/medals/index.tsx
+++ src/renderer/src/components/levels/8-helm/medals/index.tsx
@@ -20,16 +20,16 @@ const MedalLocations: React.FC = () => {
   const triangle = useTriangle()
   return (
     <BananaMedalPool>
-      <HelmCheck id={8100} name="Helm DK Banana Medal" canGetLogic={helmMachine.in && bongos} canGetBreak={helmMachine.out && bongos} />
+      <HelmCheck id={8100} name="Shutting Off the Blast-o-Matic: DK" canGetLogic={helmMachine.in && bongos} canGetBreak={helmMachine.out && bongos} />
       <HelmCheck
         id={8101}
-        name="Helm Diddy Banana Medal"
+        name="Shutting Off the Blast-o-Matic: Diddy"
         canGetLogic={helmMachine.in && guitar && rocket}
         canGetBreak={helmMachine.out && guitar && rocket}
       />
-      <HelmCheck id={8102} name="Helm Lanky Banana Medal" canGetLogic={helmMachine.in && trombone} canGetBreak={helmMachine.out && trombone} />
-      <HelmCheck id={8103} name="Helm Tiny Banana Medal" canGetLogic={helmMachine.in && sax} canGetBreak={helmMachine.out && sax} />
-      <HelmCheck id={8104} name="Helm Chunky Banana Medal" canGetLogic={helmMachine.in && triangle} canGetBreak={helmMachine.out && triangle} />
+      <HelmCheck id={8102} name="Shutting Off the Blast-o-Matic: Lanky" canGetLogic={helmMachine.in && trombone} canGetBreak={helmMachine.out && trombone} />
+      <HelmCheck id={8103} name="Shutting Off the Blast-o-Matic: Tiny" canGetLogic={helmMachine.in && sax} canGetBreak={helmMachine.out && sax} />
+      <HelmCheck id={8104} name="Shutting Off the Blast-o-Matic: Chunky" canGetLogic={helmMachine.in && triangle} canGetBreak={helmMachine.out && triangle} />
     </BananaMedalPool>
   )
 }
