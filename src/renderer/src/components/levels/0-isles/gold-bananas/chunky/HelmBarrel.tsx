b895dbde1670bb57eab238543c644557aed7c576
A-l-e-x-99 <alexs23743@gmail.com>
2025-10-09 22:44:32 -0400

    More renaming

--- src/renderer/src/components/levels/0-isles/gold-bananas/chunky/HelmBarrel.tsx
+++ src/renderer/src/components/levels/0-isles/gold-bananas/chunky/HelmBarrel.tsx
@@ -6,7 +6,7 @@ const HelmBarrel: React.FC = () => {
   return (
     <IslesCheck
       id={43}
-      name="Isles Chunky Helm Lobby Barrel"
+      name="Chunky's Helm Lobby Barrel"
       region="Caves-Helm Lobbies"
       canGetLogic={helm.in}
       canGetBreak={helm.out}
