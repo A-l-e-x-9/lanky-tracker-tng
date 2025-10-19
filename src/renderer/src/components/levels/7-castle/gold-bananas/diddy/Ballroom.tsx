ca891805dd30b067b22ec53b217c24c2e4c856d4
A-l-e-x-99 <alexs23743@gmail.com>
2025-10-18 22:32:16 -0400

    Apparently, Diddy's ballroom doesn't qualify as a gauntlet.

--- src/renderer/src/components/levels/7-castle/gold-bananas/diddy/Ballroom.tsx
+++ src/renderer/src/components/levels/7-castle/gold-bananas/diddy/Ballroom.tsx
@@ -1,16 +1,13 @@
-import Miniboss from '@renderer/components/pools/Miniboss'
 import { useDiddyRoomGb } from '@renderer/hooks/castle'
 import CastleCheck from '../../check'
 
 const DiddyBallroom: React.FC = () => (
-<Miniboss>
   <CastleCheck
     id={7011}
     name="Diddy's Ballroom Barrel"
     region="Various Castle Rooms"
     canGetLogic={useDiddyRoomGb()}
   />
-</Miniboss>
 )
 
 export default DiddyBallroom
