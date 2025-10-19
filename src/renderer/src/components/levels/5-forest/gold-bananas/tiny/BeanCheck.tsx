193de2fb68a74f1aa78e8f09def55873adfdb6e5
A-l-e-x-99 <alexs23743@gmail.com>
2025-10-18 22:41:11 -0400

    Another "gauntlet" that was WotH in my gauntlets-off seed...

--- src/renderer/src/components/levels/5-forest/gold-bananas/tiny/BeanCheck.tsx
+++ src/renderer/src/components/levels/5-forest/gold-bananas/tiny/BeanCheck.tsx
@@ -1,16 +1,13 @@
-import Miniboss from '@renderer/components/pools/Miniboss'
 import { useTinyBeanGb } from '@renderer/hooks/forest'
 import ForestCheck from '../../check'
 
 const BeanCheck: React.FC = () => (
-<Miniboss>
   <ForestCheck
     id={5033}
     name="Planting The Bean"
     region="Forest Area 2"
     canGetLogic={useTinyBeanGb()}
   />
-</Miniboss>
 )
 
 export default BeanCheck
