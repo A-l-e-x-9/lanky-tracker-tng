d71e30cf1c457b998b82d411903bd7471263faa2
A-l-e-x-99 <alexs23743@gmail.com>
2025-10-22 22:46:45 -0400

    Actually fixing glitched check

--- src/renderer/src/components/levels/0-isles/gold-bananas/chunky/TrianglePad.tsx
+++ src/renderer/src/components/levels/0-isles/gold-bananas/chunky/TrianglePad.tsx
@@ -4,7 +4,7 @@ import IslesCheck from '../../check'
 const TrianglePad: React.FC = () => {
   const music = useCheckChunkyMusicPad()
   return (
-    <IslesCheck id={41} name="Chunky's Triangle Pad" region="DK Island" canGetLogic={music} />
+    <IslesCheck id={41} name="Chunky's Triangle Pad" region="DK Island" canGetLogic={music.in} canGetBreak={music.out} />
   )
 }
 
