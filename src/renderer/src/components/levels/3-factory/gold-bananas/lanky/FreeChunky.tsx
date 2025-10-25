c4c033647d292a7329399ff3b3236bf7fa9de803
A-l-e-x-99 <alexs23743@gmail.com>
2025-10-25 10:45:28 -0400

    Fixing a related glitched check

--- src/renderer/src/components/levels/3-factory/gold-bananas/lanky/FreeChunky.tsx
+++ src/renderer/src/components/levels/3-factory/gold-bananas/lanky/FreeChunky.tsx
@@ -4,14 +4,17 @@ import FactoryCheck from '../../check'
 
 const FreeChunky: React.FC = () => {
 const logic = useLankyFreeChunkyGb()
+return (
 <GBPool>
   <FactoryCheck
     id={3022}
     name="Freeing Chunky Kong"
     region="Storage and Arcade Area"
-    canGetLogic={useLankyFreeChunkyGb()}
+    canGetLogic={logic.in}
+    canGetBreak={logic.out}
   />
 </GBPool>
+)
 }
 
 export default FreeChunky
