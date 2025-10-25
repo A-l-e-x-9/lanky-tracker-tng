6f5846b0d8d51242e4231400aea16838c9090615
A-l-e-x-99 <alexs23743@gmail.com>
2025-10-24 22:53:05 -0400

    Fixing glitched check

--- src/renderer/src/components/levels/1-japes/gold-bananas/tiny/HiveInside.tsx
+++ src/renderer/src/components/levels/1-japes/gold-bananas/tiny/HiveInside.tsx
@@ -1,16 +1,18 @@
 import { useTinyHiveGb } from '@renderer/hooks/japes'
+import GBPool from '@renderer/components/pools'
 import JapesCheck from '../../check'
 
 const HiveInside: React.FC = () => {
   const hiveGb = useTinyHiveGb()
   return (
+  <GBPool>
     <JapesCheck
       id={1033}
       name="Inside Tiny's Hive"
       region="Hive Area"
-      canGetLogic={hiveGb.in}
-      canGetBreak={hiveGb.out}
+      canGetLogic={hiveGb}
     />
+  </GBPool>
   )
 }
 
