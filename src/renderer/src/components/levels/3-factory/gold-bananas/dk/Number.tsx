6d4f8106ef6a27954ff628e5c471d5f6e0af21a0
A-l-e-x-99 <alexs23743@gmail.com>
2025-10-30 20:27:20 -0400

    Updating more checks

--- src/renderer/src/components/levels/3-factory/gold-bananas/dk/Number.tsx
+++ src/renderer/src/components/levels/3-factory/gold-bananas/dk/Number.tsx
@@ -1,13 +1,16 @@
+import GBPool from '@renderer/components/pools/GB'
 import { useDkNumberGb } from '@renderer/hooks/factory'
 import FactoryCheck from '../../check'
 
 const NumberGame: React.FC = () => (
+<GBPool>
   <FactoryCheck
     id={3001}
     name="DK's Number Game"
     region="Testing Room"
     canGetLogic={useDkNumberGb()}
   />
+  </GBPool>
 )
 
 export default NumberGame
