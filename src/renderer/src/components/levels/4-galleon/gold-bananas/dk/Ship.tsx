9d3e27474ae6e3fec1d41784dca2d17c1668ae23
A-l-e-x-99 <alexs23743@gmail.com>
2025-11-01 23:05:58 -0400

    grr

--- src/renderer/src/components/levels/4-galleon/gold-bananas/dk/Ship.tsx
+++ src/renderer/src/components/levels/4-galleon/gold-bananas/dk/Ship.tsx
@@ -1,4 +1,4 @@
-import GBPool '@renderer/components/pools/GB'
+import GBPool from '@renderer/components/pools/GB'
 import { useDk5DoorShipGb } from '@renderer/hooks/galleon'
 import GalleonCheck from '../../check'
 
