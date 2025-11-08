4137e08fb5a4aaebeb98baa76f4c7a7d888c366f
A-l-e-x-99 <alexs23743@gmail.com>
2025-11-08 16:58:41 -0500

    And another one

--- src/renderer/src/components/levels/5-forest/gold-bananas/tiny/BeanCheck.tsx
+++ src/renderer/src/components/levels/5-forest/gold-bananas/tiny/BeanCheck.tsx
@@ -2,15 +2,19 @@ import { useTinyBeanGb } from '@renderer/hooks/forest'
 import GBPool from '@renderer/components/pools/GB'
 import ForestCheck from '../../check'
 
-const BeanCheck: React.FC = () => (
+const BeanCheck: React.FC = () => {
+const canDo = useTinyBeanGb()
+return (
 <GBPool>
   <ForestCheck
     id={5033}
     name="Planting The Bean"
     region="Forest Area 2"
-    canGetLogic={useTinyBeanGb()}
+    canGetLogic={canDo.in}
+    canGetBreak={canDo.out}
   />
   </GBPool>
 )
+}
 
 export default BeanCheck
