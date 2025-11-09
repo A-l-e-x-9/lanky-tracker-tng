e2b9bd69ee1335eaad78525eeb02223c83419e9c
A-l-e-x-99 <alexs23743@gmail.com>
2025-11-08 20:25:28 -0500

    More updated checks

--- src/renderer/src/components/levels/4-galleon/gold-bananas/chunky/Seasick.tsx
+++ src/renderer/src/components/levels/4-galleon/gold-bananas/chunky/Seasick.tsx
@@ -2,15 +2,19 @@ import GBPool from '@renderer/components/pools/GB'
 import { useChunkySeasickGb } from '@renderer/hooks/galleon'
 import GalleonCheck from '../../check'
 
-const SeasickShip: React.FC = () => (
+const SeasickShip: React.FC = () => {
+const canDo = useChunkySeasickGb()
+return (
 <GBPool>
   <GalleonCheck
     id={4042}
     name="Chunky's Seasick Ship"
     region="Lighthouse Area"
-    canGetLogic={useChunkySeasickGb()}
+    canGetLogic={canDo.in}
+    canGetBreak={canDo.out}
   />
   </GBPool>
 )
+}
 
 export default SeasickShip
--- src/renderer/src/components/levels/4-galleon/kasplats/Diddy.tsx
+++ src/renderer/src/components/levels/4-galleon/kasplats/Diddy.tsx
@@ -3,17 +3,21 @@ import VanillaKasplat from '@renderer/components/pools/VanillaKasplat'
 import { useKevin } from '@renderer/hooks/galleon'
 import GalleonCheck from '../check'
 
-const DiddyKasplat: React.FC = () => (
+const DiddyKasplat: React.FC = () => {
+const canDo = useKevin()
+return (
   <KasplatPool>
     <VanillaKasplat>
       <GalleonCheck
         id={4051}
         name="Kevin the Kasplat (Lighthouse Alcove)"
         region="Lighthouse Area"
-        canGetLogic={useKevin()}
+        canGetLogic={canDo.in}
+        canGetBreak={canDo.out}
       />
     </VanillaKasplat>
   </KasplatPool>
 )
+}
 
 export default DiddyKasplat
