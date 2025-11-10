7767197f8a943317ffbb12e1fa8e760082eb9361
A-l-e-x-99 <alexs23743@gmail.com>
2025-11-09 23:31:25 -0500

    It said I could get DK's medal despite not enough GB's. Fixing this.

--- src/renderer/src/hooks/japes/medals/dk.ts
+++ src/renderer/src/hooks/japes/medals/dk.ts
@@ -14,7 +14,7 @@ export const useDkMedalInLogic = (): number => {
   const bananaports = useBananaportAll()
   const shuffleBananas = useShuffleColoredBananas()
 
-  if (!inStage) {
+  if (!inStage.in) {
     return 0
   }
   if (!kong) {
@@ -44,12 +44,12 @@ export const useDkMedalInLogic = (): number => {
     bananas += 10
   }
 
-  if (kongGates) {
+  if (kongGates.in) {
     bananas += 9 // Between the 4 warps
     if (gun) {
       bananas += 10 // balloon by Cranky
     }
-    if (haveRambiCage) {
+    if (haveRambiCage.in) {
       bananas += 10 // rambi gate + hut
     }
   }
@@ -65,7 +65,18 @@ export const useDkMedalOutLogic = (): number => {
   const bananas = useDkMedalInLogic()
   const shuffleBananas = useShuffleColoredBananas()
 
-  if (!inStage) {
+  const inStage = usePlayJapes()
+  const haveRambiCage = useJapesRambi()
+  const kongGates = useJapesKongGates()
+  const kong = useDk()
+  const vine = useVine()
+  const pad = useBlast()
+  const gun = useCoconut()
+  const climbing = useClimbing()
+  const bananaports = useBananaportAll()
+  const shuffleBananas = useShuffleColoredBananas()
+
+  if (!inStage.out) {
     return 0
   }
   if (!kong) {
@@ -74,5 +85,36 @@ export const useDkMedalOutLogic = (): number => {
   if (shuffleBananas) {
     return 100
   }
-  return vine && climbing ? bananas : bananas + 5 //the bunch in front of a Troff 'n' Scoff door
+
+  let bananas = 5 //the bunch in front of the Troff 'n' Scoff near Lanky's painting room?
+  if (climbing) {
+    bananas += 15 //Bunches on the trees in front of Funky's, behind the Barrel Cannon, Diddy's cage
+  }
+  if (climbing || bananaports) {
+    bananas += 16 //six singles surrounding the mount-ehn and a balloon in front of Snide's
+  }
+  if (vine && climbing) {
+    // Starting area between the vines.
+    bananas += 5
+  }
+  if (gun) {
+    // Two balloons after first tunnel.
+    bananas += 20
+  }
+  if (vine && climbing && pad) {
+    // Blast course
+    bananas += 10
+  }
+
+  if (kongGates.out) {
+    bananas += 9 // Between the 4 warps
+    if (gun) {
+      bananas += 10 // balloon by Cranky
+    }
+    if (haveRambiCage.out) {
+      bananas += 10 // rambi gate + hut
+    }
+  }
+
+  return bananas
 }
