065c8485949316f603f5ee1fc119614f967226d1
A-l-e-x-99 <alexs23743@gmail.com>
2025-11-09 23:21:41 -0500

    It said I could do this check despite not enough GB's. Fixed it.

--- src/renderer/src/components/levels/1-japes/gold-bananas/diddy/TopMountain.tsx
+++ src/renderer/src/components/levels/1-japes/gold-bananas/diddy/TopMountain.tsx
@@ -2,15 +2,19 @@ import GBPool from '@renderer/components/pools/GB'
 import { useDiddyMountainGb } from '@renderer/hooks/japes'
 import JapesCheck from '../../check'
 
-const TopMountain: React.FC = () => (
+const TopMountain: React.FC = () => {
+const canDo = useDiddyMountainGb()
+return (
 <GBPool>
   <JapesCheck
     id={1011}
     name="Top of Diddy's Mountain"
     region="Japes Hillside"
-    canGetLogic={useDiddyMountainGb()}
+    canGetLogic={canDo.in}
+    canGetBreak={canDo.out}
   />
   </GBPool>
 )
+}
 
 export default TopMountain
