b3773703419c154e6624fae978b46184d3c1cbf5
A-l-e-x-99 <alexs23743@gmail.com>
2025-11-14 21:01:46 -0500

    Finally making the Castle boulder check visible in region grouping.

--- src/renderer/src/components/levels/7-castle/regions/Museum.tsx
+++ src/renderer/src/components/levels/7-castle/regions/Museum.tsx
@@ -1,8 +1,10 @@
 import MuseumEnemies from '../enemies/Museum'
+import BoulderCheck from '../boulder'
 import ChunkyMuseum from '../gold-bananas/chunky/Museum'
 
 const MuseumChecks: React.FC = () => (
   <>
+    <BoulderCheck />
     <ChunkyMuseum />
     <MuseumEnemies />
   </>
