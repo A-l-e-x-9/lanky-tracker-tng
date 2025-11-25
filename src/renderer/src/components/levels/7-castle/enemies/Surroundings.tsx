53a75ca97804fe30480f94c15b1ae1ad974ff938
A-l-e-x-99 <alexs23743@gmail.com>
2025-11-25 10:48:11 -0500

    And another one.

--- src/renderer/src/components/levels/7-castle/enemies/Surroundings.tsx
+++ src/renderer/src/components/levels/7-castle/enemies/Surroundings.tsx
@@ -15,85 +15,92 @@ const SurroundingsEnemies: React.FC = () => {
         id={7300}
         name="Enemy Near Folding Bridge Over Moat"
         region="Creepy Castle Main"
-        canGetLogic={inStage && anyKong}
+        canGetLogic={inStage.in && anyKong}
+        canGetBreak={inStage.out && anyKong}
       />
       <CastleCheck
         id={7301}
         name="Enemy Near Bottom Wooden Extrusion"
         region="Creepy Castle Main"
-        canGetLogic={inStage && anyKong}
+        canGetLogic={inStage.in && anyKong}
+        canGetBreak={inStage.out && anyKong}
       />
       <CastleCheck
         id={7302}
         name="Enemy on Bottom Wooden Extrusion"
         region="Creepy Castle Main"
-        canGetLogic={inStage && anyKong && kosha.in}
-        canGetBreak={inStage && anyKong && kosha.out}
+        canGetLogic={inStage.in && anyKong && kosha.in}
+        canGetBreak={inStage.out && anyKong && kosha.out}
       />
       <CastleCheck
         id={7303}
         name="Enemy on Wooden Extrusion Near Cranky's"
         region="Creepy Castle Main"
-        canGetLogic={inStage && anyKong && kosha.in}
-        canGetBreak={inStage && anyKong && kosha.out}
+        canGetLogic={inStage.in && anyKong && kosha.in}
+        canGetBreak={inStage.out && anyKong && kosha.out}
       />
       <CastleCheck
         id={7304}
         name="Enemy Between Museum and Shed"
         region="Creepy Castle Main"
-        canGetLogic={inStage && anyKong}
+        canGetLogic={inStage.in && anyKong}
+        canGetBreak={inStage.out && anyKong}
       />
       <CastleCheck
         id={7305}
         name="Enemy Near Library Switch"
         region="Creepy Castle Main"
-        canGetLogic={inStage && anyKong}
+        canGetLogic={inStage.in && anyKong}
+        canGetBreak={inStage.out && anyKong}
       />
       <CastleCheck
         id={7306}
         name="Enemy Near Snide's"
         region="Creepy Castle Main"
-        canGetLogic={inStage && anyKong && kosha.in}
-        canGetBreak={inStage && anyKong && kosha.out}
+        canGetLogic={inStage.in && anyKong && kosha.in}
+        canGetBreak={inStage.out && anyKong && kosha.out}
       />
       <CastleCheck
         id={7307}
         name="Enemy Between Cranky's and Museum"
         region="Creepy Castle Main"
-        canGetLogic={inStage && anyKong}
+        canGetLogic={inStage.in && anyKong}
+        canGetBreak={inStage.out && anyKong}
       />
       <CastleCheck
         id={7308}
         name="Enemy at Crypt Entrance"
         region="Creepy Castle Main"
-        canGetLogic={inStage && anyKong && hasClimbing}
-        canGetBreak={inStage && anyKong}
+        canGetLogic={inStage.in && anyKong && hasClimbing}
+        canGetBreak={inStage.out && anyKong}
       />
       <CastleCheck
         id={7309}
         name="Enemy Near Low Kasplat"
         region="Creepy Castle Main"
-        canGetLogic={inStage && anyKong && hasClimbing}
-        canGetBreak={inStage && anyKong}
+        canGetLogic={inStage.in && anyKong && hasClimbing}
+        canGetBreak={inStage.out && anyKong}
       />
       <CastleCheck
         id={7310}
         name="Enemy Near Lowest Troff 'n' Scoff Door"
         region="Creepy Castle Main"
-        canGetLogic={inStage && anyKong && hasClimbing}
-        canGetBreak={inStage && anyKong}
+        canGetLogic={inStage.in && anyKong && hasClimbing}
+        canGetBreak={inStage.out && anyKong}
       />
       <CastleCheck
         id={7311}
         name="Enemy Near Steps to Dungeon"
         region="Creepy Castle Main"
-        canGetLogic={inStage && anyKong}
+        canGetLogic={inStage.in && anyKong}
+        canGetBreak={inStage.out && anyKong}
       />
       <CastleCheck
         id={7312}
         name="Enemy Between Tree and Ladder to Headphones"
         region="Creepy Castle Main"
-        canGetLogic={inStage && anyKong}
+        canGetLogic={inStage.in && anyKong}
+        canGetBreak={inStage.out && anyKong}
       />
     </DropPool>
   )
