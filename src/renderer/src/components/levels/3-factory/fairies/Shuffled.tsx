292ae1d3159e3d2657f56b8a50c6e71df0916a7e
A-l-e-x-99 <alexs23743@gmail.com>
2025-11-26 10:47:00 -0500

    What's an AztecCheck to the Factory, anyway? =_=

--- src/renderer/src/components/levels/3-factory/fairies/Shuffled.tsx
+++ src/renderer/src/components/levels/3-factory/fairies/Shuffled.tsx
@@ -20,139 +20,139 @@ const Shuffled: React.FC = () => {
   const highGrab = useHighGrab()
   return (
     <FairyPool>
-      <AztecCheck
+      <FactoryCheck
         id={13000}
         name="Shuffled Fairy: Vanilla Location #1 (tunnel to DK's number game)"
         region="Testing Room"
         canGetLogic={vanillaFairy1.in}
         canGetBreak={vanillaFairy1.out}
       />
-      <AztecCheck
+      <FactoryCheck
         id={13001}
         name="Shuffled Fairy: Vanilla Location #2 (Funky's after Tiny's dart game)"
         region="Testing Room"
         canGetLogic={vanillaFairy2}
       />
-      <AztecCheck
+      <FactoryCheck
         id={13002}
         name="Shuffled Fairy: At the vanilla entrance"
         region="Factory Start Area"
         canGetLogic={isBreathing.in}
         canGetBreak={isBreathing.out}
       />
-      <AztecCheck
+      <FactoryCheck
         id={13003}
         name="Shuffled Fairy: Down the pole to the Prod Room"
         region="Factory Start Area"
         canGetLogic={isBreathing.in && hasClimbing}
         canGetBreak={isBreathing.out}
       />
-      <AztecCheck
+      <FactoryCheck
         id={13004}
         name="Shuffled Fairy: Bottom of Prod Room"
         region="Prod Room"
         canGetLogic={isBreathing.in}
         canGetBreak={isBreathing.out}
       />
-      <AztecCheck
+      <FactoryCheck
         id={13005}
         name="Shuffled Fairy: Middle of Prod Room"
         region="Prod Room"
         canGetLogic={prodRoomOn.in && hasClimbing && hasCam}
         canGetBreak={prodRoomOn.out && hasCam}
       />
-      <AztecCheck
+      <FactoryCheck
         id={13006}
         name="Shuffled Fairy: Top of Prod Room"
         region="Prod Room"
         canGetLogic={prodRoomTop.in && hasCam}
         canGetBreak={prodRoomTop.out && hasCam}
       />
-      <AztecCheck
+      <FactoryCheck
         id={13007}
         name="Shuffled Fairy: In Storage"
         region="Storage and Arcade Area"
         canGetLogic={isBreathing.in}
         canGetBreak={isBreathing.out}
       />
-      <AztecCheck
+      <FactoryCheck
         id={13008}
         name="Shuffled Fairy: At Cranky's/Candy's"
         region="Storage and Arcade Area"
         canGetLogic={isBreathing.in}
         canGetBreak={isBreathing.out}
       />
-      <AztecCheck
+      <FactoryCheck
         id={13009}
         name="Shuffled Fairy: In the tunnel to the Arcade room"
         region="Storage and Arcade Area"
         canGetLogic={isBreathing.in && (hasClimbing || hasBananaports)}
         canGetBreak={isBreathing.out && (hasClimbing || hasBananaports)}
       />
-      <AztecCheck
+      <FactoryCheck
         id={13010}
         name="Shuffled Fairy: Suffering through DK Arcade"
         region="Storage and Arcade Area"
         canGetLogic={isBreathing.in && (hasClimbing || hasBananaports)}
         canGetBreak={isBreathing.out && (hasClimbing || hasBananaports)}
       />
-      <AztecCheck
+      <FactoryCheck
         id={13011}
         name="Shuffled Fairy: Top of Diddy's Block Tower"
         region="Testing Room"
         canGetLogic={canDoBlockTower.in && hasCam}
         canGetBreak={canDoBlockTower.out && hasCam}
       />
-      <AztecCheck
+      <FactoryCheck
         id={13012}
         name="Shuffled Fairy: Boxes to Tiny's Dartboard"
         region="Testing Room"
         canGetLogic={canDoTesting.in && hasCam}
         canGetBreak={canDoTesting.out && hasCam}
       />
-      <AztecCheck
+      <FactoryCheck
         id={13013}
         name="Shuffled Fairy: Pole to the R&D Room"
         region="Testing Room"
         canGetLogic={canDoTesting.in && hasCam && hasClimbing}
         canGetBreak={canDoTesting.out && hasCam}
       />
-      <AztecCheck
+      <FactoryCheck
         id={13014}
         name="Shuffled Fairy: In Diddy's Pincode Room"
         region="R&D Room"
         canGetLogic={canDoTesting.in && hasCam && (hasClimbing || hasBananaports) && hasGuitar}
         canGetBreak={canDoTesting.out && hasCam && (hasClimbing || hasBananaports) && hasGuitar}
       />
-      <AztecCheck
+      <FactoryCheck
         id={13015}
         name="Shuffled Fairy: Tunnel to Tiny's Car Race"
         region="R&D Room"
         canGetLogic={canDoTesting.in && hasCam && (hasClimbing || hasBananaports)}
         canGetBreak={canDoTesting.out && hasCam && (hasClimbing || hasBananaports)}
       />
-      <AztecCheck
+      <FactoryCheck
         id={13016}
         name="Shuffled Fairy: In Chunky's Toy Monster Room"
         region="R&D Room"
         canGetLogic={canDoTesting.in && hasCam && (hasClimbing || hasBananaports) && hasPrimatePunch && hasTriangle}
         canGetBreak={canDoTesting.out && hasCam && (hasClimbing || hasBananaports) highGrab && hasPrimatePunch && hasTriangle}
       />
-      <AztecCheck
+      <FactoryCheck
         id={13017}
         name="Shuffled Fairy: At the chute to the Storage Room"
         region="R&D Room"
         canGetLogic={canDoTesting.in && hasCam && (hasClimbing || hasBananaports)}
         canGetBreak={canDoTesting.out && hasCam && (hasClimbing || hasBananaports)}
       />
-      <AztecCheck
+      <FactoryCheck
         id={13018}
         name="Shuffled Fairy: In Chunky's Dark Room"
         region="Storage and Arcade Area"
         canGetLogic={isBreathing.in && hasPrimatePunch}
         canGetBreak={isBreathing.out && hasPrimatePunch}
       />
-      <AztecCheck
+      <FactoryCheck
         id={13019}
         name="Shuffled Fairy: In the Crusher"
         region="Prod Room"
