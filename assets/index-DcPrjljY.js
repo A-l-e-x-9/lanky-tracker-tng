e5bc1cf7f4220c62669068b79cca722838988827
A-l-e-x-9 <alexs23743@gmail.com>
2025-11-26 06:38:08 +0000

    Deploying to gh-pages from @ A-l-e-x-9/lanky-tracker-tng@f6206a55aa94a29416cea622ec75d714094431d1 🚀

--- assets/index-DcPrjljY.js
+++ assets/index-DcPrjljY.js
@@ -18742,24 +18742,190 @@ const Shuffled$7 = () => {
 };
 const ShuffledDirtLocations = () => useShuffleDirt() ? /* @__PURE__ */ jsxRuntimeExports.jsx(Shuffled$7, {}) : null;
 const Shuffled$6 = () => {
-  const fairyLogic = useGeneralFairy();
+  const hasCam = useCamera();
+  const hasDiving = useDive();
+  const hasGrapes = useGrape();
+  const isBreathing = useGeneralFairy();
+  const vanillaFairy1 = useTinyFairy();
+  const vanillaFairy2 = useLlamaFairy();
+  const canReachVases = useChunkyVaseGb();
+  const canReachBack = useAztecBack();
+  const canGoInDK5DT = useDk5DoorGb();
+  const canGoInDiddy5DT = useDiddy5DoorGb();
+  const canGoInLanky5DT = useLanky5DoorGb();
+  const canGoInChunky5DT = useChunky5DoorGb();
+  const canGoInLlamaTemple = useAztecLlamaTemple();
+  const canGoInTinyTemple = useAztecTinyTemple();
+  const iceMelted = useTinyTempleIce();
   return /* @__PURE__ */ jsxRuntimeExports.jsxs(FairyPool, { children: [
     /* @__PURE__ */ jsxRuntimeExports.jsx(
       AztecCheck,
       {
-        id: 2280,
-        name: "Aztec Fairy Location #1",
-        canGetLogic: fairyLogic.in,
-        canGetBreak: fairyLogic.out
+        id: 12e3,
+        name: "Shuffled Fairy: Vanilla Location #1 (Tiny's 5DT room)",
+        region: "5 Door Temple",
+        canGetLogic: vanillaFairy1.in,
+        canGetBreak: vanillaFairy1.out
       }
     ),
     /* @__PURE__ */ jsxRuntimeExports.jsx(
       AztecCheck,
       {
-        id: 2281,
-        name: "Aztec Fairy Location #2",
-        canGetLogic: fairyLogic.in,
-        canGetBreak: fairyLogic.out
+        id: 12001,
+        name: "Shuffled Fairy: Vanilla Location #2 (Llama Temple)",
+        region: "Llama Temple",
+        canGetLogic: vanillaFairy2.in,
+        canGetBreak: vanillaFairy2.out
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 12002,
+        name: "Shuffled Fairy: Vase room",
+        region: "Aztec Caves",
+        canGetLogic: canReachVases.in && hasCam,
+        canGetBreak: canReachVases.out && hasCam
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 12003,
+        name: "Shuffled Fairy: At the oasis",
+        region: "Aztec Main Area",
+        canGetLogic: isBreathing.in,
+        canGetBreak: isBreathing.out
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 12004,
+        name: "Shuffled Fairy: Behind the Tiny Temple",
+        region: "Aztec Main Area",
+        canGetLogic: isBreathing.in,
+        canGetBreak: isBreathing.out
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 12005,
+        name: "Shuffled Fairy: On the path to Cranky's",
+        region: "Aztec Caves",
+        canGetLogic: canReachBack.in && hasCam,
+        canGetBreak: canReachBack.out && hasCam
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 12006,
+        name: "Shuffled Fairy: At Chunky's Bonus Barrel",
+        region: "Aztec Caves",
+        canGetLogic: canReachBack.in && hasCam,
+        canGetBreak: canReachBack.out && hasCam
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 12007,
+        name: "Shuffled Fairy: At the Totem",
+        region: "Aztec Main Area",
+        canGetLogic: canReachBack.in && hasCam,
+        canGetBreak: canReachBack.out && hasCam
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 12008,
+        name: "Shuffled Fairy: At the Gong Tower",
+        region: "Aztec Main Area",
+        canGetLogic: canReachBack.in && hasCam,
+        canGetBreak: canReachBack.out && hasCam
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 12009,
+        name: "Shuffled Fairy: Inside DK's 5DT room",
+        region: "5 Door Temple",
+        canGetLogic: canGoInDK5DT.in && hasCam,
+        canGetBreak: canGoInDK5DT.out && hasCam
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 12010,
+        name: "Shuffled Fairy: Inside Diddy's 5DT room",
+        region: "5 Door Temple",
+        canGetLogic: canGoInDiddy5DT.in && hasCam,
+        canGetBreak: canGoInDiddy5DT.out && hasCam
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 12011,
+        name: "Shuffled Fairy: Inside Lanky's 5DT room",
+        region: "5 Door Temple",
+        canGetLogic: canGoInLanky5DT.in && hasCam,
+        canGetBreak: canGoInLanky5DT.out && hasCam
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 12012,
+        name: "Shuffled Fairy: Inside Chunky's 5DT room",
+        region: "5 Door Temple",
+        canGetLogic: canGoInChunky5DT.in && hasCam,
+        canGetBreak: canGoInChunky5DT.out && hasCam
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 12013,
+        name: "Shuffled Fairy: Over the Swimming Pool",
+        region: "Llama Temple",
+        canGetLogic: canGoInLlamaTemple.in && hasCam,
+        canGetBreak: canGoInLlamaTemple.out && hasCam
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 12014,
+        name: "Shuffled Fairy: Playing Match Game and doing BLANK",
+        region: "Llama Temple",
+        canGetLogic: canGoInLlamaTemple.in && hasGrapes && hasCam,
+        canGetBreak: canGoInLlamaTemple.out && hasGrapes && hasCam
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 12015,
+        name: "Shuffled Fairy: Over the Tiny Temple's pool",
+        region: "Tiny Temple",
+        canGetLogic: canGoInTinyTemple.in && hasCam,
+        canGetBreak: canGoInTinyTemple.out && hasCam
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 12016,
+        name: "Shuffled Fairy: At Tiny's cage",
+        region: "Tiny Temple",
+        canGetLogic: canGoInTinyTemple.in && iceMelted && hasDiving.in && hasCam,
+        canGetBreak: canGoInTinyTemple.out && iceMelted && hasDiving.out && hasCam
       }
     )
   ] });
@@ -19113,7 +19279,8 @@ const ClassicChecks$5 = () => /* @__PURE__ */ jsxRuntimeExports.jsxs("div", { cl
   /* @__PURE__ */ jsxRuntimeExports.jsx(BoulderLocations$2, {}),
   /* @__PURE__ */ jsxRuntimeExports.jsx(BossCheck$6, {}),
   /* @__PURE__ */ jsxRuntimeExports.jsx(ShopLocations$5, {}),
-  /* @__PURE__ */ jsxRuntimeExports.jsx(DropPool, { children: /* @__PURE__ */ jsxRuntimeExports.jsx(EnemyLocations$6, {}) })
+  /* @__PURE__ */ jsxRuntimeExports.jsx(DropPool, { children: /* @__PURE__ */ jsxRuntimeExports.jsx(EnemyLocations$6, {}) }),
+  /* @__PURE__ */ jsxRuntimeExports.jsx(ShuffledFairies, {})
 ] });
 const AztecChecks = () => {
   const grp = useDonkStore(useShallow((state) => state.ui.groupByRegion));
