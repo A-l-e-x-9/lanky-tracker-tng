3ca210118f7f7303964d59b212ba5cd1e05306d6
A-l-e-x-99 <alexs23743@gmail.com>
2025-11-25 21:37:55 -0500

    It said I could kill Japes hive enemies when I can't reach the hive. Fixing.

--- src/renderer/src/components/levels/1-japes/enemies/HiveInterior.tsx
+++ src/renderer/src/components/levels/1-japes/enemies/HiveInterior.tsx
@@ -16,52 +16,57 @@ const HiveInteriorEnemies: React.FC = () => {
         id={1342}
         name="Enemy in the Hive's Main Room"
         region="Hive Area"
-        canGetLogic={hiveIn && zinger.in}
-        canGetBreak={hiveIn && zinger.out}
+        canGetLogic={hiveIn.in && zinger.in}
+        canGetBreak={hiveIn.out && zinger.out}
       />
       <JapesCheck
         id={1335}
         name="Enemy in the Hive's First Room"
         region="Hive Area"
-        canGetLogic={hiveIn && klaptrap}
+        canGetLogic={hiveIn.in && klaptrap}
+        canGetBreak={hiveIn.out && klaptrap}
       />
       <JapesCheck
         id={1336}
         name="Enemy 0 in the Hive's Second Room"
         region="Hive Area"
-        canGetLogic={hiveIn && klaptrap && canSlam}
+        canGetLogic={hiveIn.in && klaptrap && canSlam}
+        canGetBreak={hiveIn.out && klaptrap && canSlam}
       />
       <JapesCheck
         id={1337}
         name="Enemy 1 in the Hive's Second Room"
         region="Hive Area"
-        canGetLogic={hiveIn && klaptrap && canSlam}
+        canGetLogic={hiveIn.in && klaptrap && canSlam}
+        canGetBreak={hiveIn.out && klaptrap && canSlam}
       />
       <JapesCheck
         id={1338}
         name="Enemy 0 in the Hive's Third Room"
         region="Hive Area"
-        canGetLogic={hiveIn && klaptrap && canSlam}
+        canGetLogic={hiveIn.in && klaptrap && canSlam}
+        canGetBreak={hiveIn.out && klaptrap && canSlam}
       />
       <JapesCheck
         id={1339}
         name="Enemy 1 in the Hive's Third Room"
         region="Hive Area"
-        canGetLogic={hiveIn && klaptrap && canSlam}
+        canGetLogic={hiveIn.in && klaptrap && canSlam}
+        canGetBreak={hiveIn.out && klaptrap && canSlam}
       />
       <JapesCheck
         id={1340}
         name="Enemy 2 in the Hive's Third Room"
         region="Hive Area"
-        canGetLogic={hiveIn && zinger.in && canSlam}
-        canGetBreak={hiveIn && zinger.out && canSlam}
+        canGetLogic={hiveIn.in && zinger.in && canSlam}
+        canGetBreak={hiveIn.out && zinger.out && canSlam}
       />
       <JapesCheck
         id={1341}
         name="Enemy 3 in the Hive's Third Room"
         region="Hive Area"
-        canGetLogic={hiveIn && zinger.in && canSlam}
-        canGetBreak={hiveIn && zinger.out && canSlam}
+        canGetLogic={hiveIn.in && zinger.in && canSlam}
+        canGetBreak={hiveIn.out && zinger.out && canSlam}
       />
     </DropPool>
   )
