2493fbdb6b053254096f54b2990efc08763989a4
A-l-e-x-99 <alexs23743@gmail.com>
2025-11-26 23:49:03 -0500

    Fixing a duplicate ID issue

--- src/renderer/src/components/levels/2-aztec/enemies/Llama.tsx
+++ src/renderer/src/components/levels/2-aztec/enemies/Llama.tsx
@@ -11,56 +11,56 @@ const LlamaTempleEnemies: React.FC = () => {
   return (
     <DropPool>
       <AztecCheck
-        id={2351}
+        id={2354}
         name="Enemy at Music Pad to Free Lanky"
         region="Llama Temple"
         canGetLogic={llama.in && klaptrap}
         canGetBreak={llama.out && klaptrap}
       />
       <AztecCheck
-        id={2352}
+        id={2355}
         name="Enemy at Music Pad to Raise Statues"
         region="Llama Temple"
         canGetLogic={llama.in && klaptrap}
         canGetBreak={llama.out && klaptrap}
       />
       <AztecCheck
-        id={2353}
+        id={2356}
         name="Enemy 0 in the Matching Room"
         region="Llama Temple"
         canGetLogic={llama.in && hasGrapes}
         canGetBreak={llama.out && hasGrapes}
       />
       <AztecCheck
-        id={2354}
+        id={2357}
         name="Enemy 1 in the Matching Room"
         region="Llama Temple"
         canGetLogic={llama.in && hasGrapes}
         canGetBreak={llama.out && hasGrapes}
       />
       <AztecCheck
-        id={2355}
+        id={2358}
         name="Enemy at Right of Pool"
         region="Llama Temple"
         canGetLogic={llama.in}
         canGetBreak={llama.out}
       />
       <AztecCheck
-        id={2356}
+        id={2359}
         name="Enemy at Left of Pool"
         region="Llama Temple"
         canGetLogic={llama.in}
         canGetBreak={llama.out}
       />
       <AztecCheck
-        id={2357}
+        id={2360}
         name="Enemy at Melon Crate"
         region="Llama Temple"
         canGetLogic={llama.in && klaptrap}
         canGetBreak={llama.out && klaptrap}
       />
       <AztecCheck
-        id={2358}
+        id={2361}
         name="Enemy at Quicksand Tunnel Opening Switch"
         region="Llama Temple"
         canGetLogic={llama.in && klaptrap}
