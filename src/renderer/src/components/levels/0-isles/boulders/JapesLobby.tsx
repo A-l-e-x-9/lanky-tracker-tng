c848e248b745310ce5f2bc483ece2f262d0b0d13
A-l-e-x-99 <alexs23743@gmail.com>
2025-11-30 17:09:14 -0500

    (sigh) Domino effect much?

--- src/renderer/src/components/levels/0-isles/boulders/JapesLobby.tsx
+++ src/renderer/src/components/levels/0-isles/boulders/JapesLobby.tsx
@@ -4,13 +4,15 @@ import IslesCheck from '../check'
 
 /*Version 5 of the Randomizer now allows you to have major rewards in the boulders that Chunky can toss. This is for the boulder in Japes Lobby, on top of the Trombone Pad.*/
 const JapesLobby: React.FC = () => {
+const canDo = useJapesLobby()
   return (
     <BoulderPool>
         <IslesCheck
           id={9}
           name="Japes Lobby Boulder"
           region="Japes-Forest Lobbies"
-          canGetLogic={useJapesLobby()}
+          canGetLogic={canDo.in}
+          canGetBreak={canDo.out}
         />
     </BoulderPool>
   )
