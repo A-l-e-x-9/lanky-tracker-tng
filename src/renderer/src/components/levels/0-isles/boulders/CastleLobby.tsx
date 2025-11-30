93598ea01a6e0facf581fe5f4dbe42be4015d76e
A-l-e-x-99 <alexs23743@gmail.com>
2025-11-30 16:57:58 -0500

    Fixed another bad check.

--- src/renderer/src/components/levels/0-isles/boulders/CastleLobby.tsx
+++ src/renderer/src/components/levels/0-isles/boulders/CastleLobby.tsx
@@ -4,13 +4,15 @@ import IslesCheck from '../check'
 
 /*Version 5 of the Randomizer now allows you to have major rewards in the boulders that Chunky can toss. This is for the boulder on the Lanky Pad in Castle Lobby.*/
 const CastleLobby: React.FC = () => {
+const canDo = useCastleLobby()
   return (
     <BoulderPool>
         <IslesCheck
           id={7}
           name="Boulder on the Lanky Pad in Castle Lobby"
           region="Caves-Helm Lobbies"
-          canGetLogic={useCastleLobby()}
+          canGetLogic={canDo.in}
+          canGetBreak={canDo.out}
         />
     </BoulderPool>
   )
