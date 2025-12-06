2caf9619783da932a8e5c280f0c29a70a6256e24
A-l-e-x-99 <alexs23743@gmail.com>
2025-12-06 09:45:15 -0500

    It's never simple with this code, is it!?!

--- src/renderer/src/components/levels/0-isles/kasplats/Diddy.tsx
+++ src/renderer/src/components/levels/0-isles/kasplats/Diddy.tsx
@@ -3,17 +3,21 @@ import VanillaKasplat from '@renderer/components/pools/VanillaKasplat'
 import { useCastleKasplat } from '@renderer/hooks/isles'
 import IslesCheck from '../check'
 
-const DiddyKasplat: React.FC = () => (
+const DiddyKasplat: React.FC = () => {
+const canDo = useCastleKasplat()
+return (
   <KasplatPool>
     <VanillaKasplat>
       <IslesCheck
         id={51}
         name="Kasplat in the Castle Lobby"
         region="Caves-Helm Lobbies"
-        canGetLogic={useCastleKasplat()}
+        canGetLogic={canDo.in}
+        canGetBreak={canDo.out}
       />
     </VanillaKasplat>
   </KasplatPool>
 )
+}
 
 export default DiddyKasplat
--- src/renderer/src/components/levels/0-isles/kasplats/Tiny.tsx
+++ src/renderer/src/components/levels/0-isles/kasplats/Tiny.tsx
@@ -3,17 +3,21 @@ import VanillaKasplat from '@renderer/components/pools/VanillaKasplat'
 import { useFactoryKasplat } from '@renderer/hooks/isles'
 import IslesCheck from '../check'
 
-const TinyKasplat: React.FC = () => (
+const TinyKasplat: React.FC = () => {
+const canDo = useFactoryKasplat()
+return (
   <KasplatPool>
     <VanillaKasplat>
       <IslesCheck
         id={53}
         name="Kasplat in the Factory Lobby Box"
         region="Japes-Forest Lobbies"
-        canGetLogic={useFactoryKasplat()}
+        canGetLogic={canDo.in}
+        canGetBreak={canDo.out}
       />
     </VanillaKasplat>
   </KasplatPool>
 )
+}
 
 export default TinyKasplat
