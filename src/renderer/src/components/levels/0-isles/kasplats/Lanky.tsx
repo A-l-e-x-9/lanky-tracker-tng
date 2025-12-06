81bcba526858c7e5f9cca378cacbb08fe21cffc4
A-l-e-x-99 <alexs23743@gmail.com>
2025-12-06 09:37:50 -0500

    And another one.

--- src/renderer/src/components/levels/0-isles/kasplats/Lanky.tsx
+++ src/renderer/src/components/levels/0-isles/kasplats/Lanky.tsx
@@ -3,17 +3,21 @@ import VanillaKasplat from '@renderer/components/pools/VanillaKasplat'
 import { useCavesKasplat } from '@renderer/hooks/isles'
 import IslesCheck from '../check'
 
-const LankyKasplat: React.FC = () => (
+const LankyKasplat: React.FC = () => {
+const canDo = useCavesKasplat()
+return (
   <KasplatPool>
     <VanillaKasplat>
       <IslesCheck
         id={52}
         name="Kasplat Behind Left Caves Lobby Wall"
         region="Caves-Helm Lobbies"
-        canGetLogic={useCavesKasplat()}
+        canGetLogic={canDo.in}
+        canGetBreak={canDo.out}
       />
     </VanillaKasplat>
   </KasplatPool>
 )
+}
 
 export default LankyKasplat
