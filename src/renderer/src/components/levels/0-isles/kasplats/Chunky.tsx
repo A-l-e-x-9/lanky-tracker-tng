2c39b5d837501f9057d22615f63bb6de45b18a50
A-l-e-x-99 <alexs23743@gmail.com>
2025-12-06 09:36:00 -0500

    Fixing a faulty check.

--- src/renderer/src/components/levels/0-isles/kasplats/Chunky.tsx
+++ src/renderer/src/components/levels/0-isles/kasplats/Chunky.tsx
@@ -3,17 +3,21 @@ import VanillaKasplat from '@renderer/components/pools/VanillaKasplat'
 import { useGalleonKasplat } from '@renderer/hooks/isles'
 import IslesCheck from '../check'
 
-const ChunkyKasplat: React.FC = () => (
+const ChunkyKasplat: React.FC = () => {
+const canDo = useGalleonKasplat()
+return (
   <KasplatPool>
     <VanillaKasplat>
       <IslesCheck
         id={54}
         name="Galleon Lobby Kasplat"
         region="Japes-Forest Lobbies"
-        canGetLogic={useGalleonKasplat()}
+        canGetLogic={canDo.in}
+        canGetBreak={canDo.out}
       />
     </VanillaKasplat>
   </KasplatPool>
 )
+}
 
 export default ChunkyKasplat
