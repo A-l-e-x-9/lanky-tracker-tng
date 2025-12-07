662c5eb560ec9dffeb7b6beb68255299ddacb99e
A-l-e-x-99 <alexs23743@gmail.com>
2025-12-06 21:51:52 -0500

    More faulty checks to fix.

--- src/renderer/src/components/levels/0-isles/fairies/FactoryLobby.tsx
+++ src/renderer/src/components/levels/0-isles/fairies/FactoryLobby.tsx
@@ -3,17 +3,21 @@ import VanillaFairy from '@renderer/components/pools/VanillaFairy'
 import { useFactoryFairy } from '@renderer/hooks/isles'
 import IslesCheck from '../check'
 
-const FactoryLobbyFairy: React.FC = () => (
+const FactoryLobbyFairy: React.FC = () => {
+const canDo = useFactoryFairy()
+return (
   <FairyPool>
     <VanillaFairy>
       <IslesCheck
         id={81}
         name="Factory Lobby Fairy"
         region="Japes-Forest Lobbies"
-        canGetLogic={useFactoryFairy()}
+        canGetLogic={canDo.in}
+        canGetBreak={canDo.out}
       />
     </VanillaFairy>
   </FairyPool>
 )
+}
 
 export default FactoryLobbyFairy
