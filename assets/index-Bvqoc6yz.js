fb60cea1a34225e6b4c040b6d7b2005102d6049c
A-l-e-x-9 <alexs23743@gmail.com>
2025-12-17 14:27:49 +0000

    Deploying to gh-pages from @ A-l-e-x-9/lanky-tracker-tng@99286888b1e2006576e19beb54a2fe3912129c5d 🚀

--- assets/index-Bvqoc6yz.js
+++ assets/index-Bvqoc6yz.js
@@ -14776,7 +14776,6 @@ const Shuffled$n = () => {
   const canGetInCastleLobby = usePlayLobby("Creepy Castle");
   const canDoIslesDirt1 = canReachFungiLobby;
   const canDoIslesDirt2InLogic = useIslesRocket() && canReachFungiLobby && useRocket();
-  const canDoIslesDirt2OutLogic = canReachAztecLobby.out && hasBoulderTech && (hasDiddy || hasTiny) || canReachWaterfall && (hasDK || isHinaKagiyama);
   const canReachFactoryLobby = useIslesKremAscent();
   const canReachHelmLobby = useIslesKremTop();
   const canGetInJapesLobby = usePlayLobby("Jungle Japes");
@@ -14837,7 +14836,7 @@ const Shuffled$n = () => {
         name: "Shuffled Melon Crate: On the Aztec Lobby roof",
         region: "DK Island",
         canGetLogic: canDoIslesDirt2InLogic,
-        canGetBreak: canDoIslesDirt2OutLogic
+        canGetBreak: canReachAztecLobby.out && hasBoulderTech && (hasDiddy || hasTiny) || canReachWaterfall && (hasDK || isHinaKagiyama)
       }
     ),
     /* @__PURE__ */ jsxRuntimeExports.jsx(
