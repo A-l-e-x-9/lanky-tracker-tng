7e4b33ee00757eade86873b878bf84bd77da5126
A-l-e-x-99 <alexs23743@gmail.com>
2025-12-18 17:00:07 -0500

    Fixing some more faulty checks.

--- src/renderer/src/components/levels/5-forest/enemies/MushExterior.tsx
+++ src/renderer/src/components/levels/5-forest/enemies/MushExterior.tsx
@@ -16,88 +16,92 @@ const MushExteriorEnemies: React.FC = () => {
         id={5323}
         name="Enemy 0 in the Yellow Tunnel"
         region="Forest Area 3"
-        canGetLogic={inStage && anyKong}
+        canGetLogic={inStage.in && anyKong}
+        canGetBreak={inStage.out && anyKong}
       />
       <ForestCheck
         id={5324}
         name="Enemy at Low Warp 5"
         region="Forest Area 3"
-        canGetLogic={inStage && anyKong}
+        canGetLogic={inStage.in && anyKong}
+        canGetBreak={inStage.out && anyKong}
       />
       <ForestCheck
         id={5325}
         name="Enemy Between Bounce Tag and Cranky's"
         region="Forest Area 3"
-        canGetLogic={inStage && anyKong}
+        canGetLogic={inStage.in && anyKong}
+        canGetBreak={inStage.out && anyKong}
       />
       <ForestCheck
         id={5326}
         name="Enemy at Giant Shroom's Rocketbarrel"
         region="Forest Area 3"
-        canGetLogic={inStage && anyKong}
+        canGetLogic={inStage.in && anyKong}
+        canGetBreak={inStage.out && anyKong}
       />
       <ForestCheck
         id={5327}
         name="Enemy between Rocketbarrel and Yellow Tunnel"
         region="Forest Area 3"
-        canGetLogic={inStage && zinger.in}
-        canGetBreak={inStage && zinger.out}
+        canGetLogic={inStage.in && zinger.in}
+        canGetBreak={inStage.out && zinger.out}
       />
       <ForestCheck
         id={5328}
         name="Enemy Near Cranky's"
         region="Forest Area 3"
-        canGetLogic={inStage && zinger.in}
-        canGetBreak={inStage && zinger.out}
+        canGetLogic={inStage.in && zinger.in}
+        canGetBreak={inStage.out && zinger.out}
       />
       <ForestCheck
         id={5329}
         name="Enemy at Pink Tunnel Bouncy Tag Barrel"
         region="Forest Area 3"
-        canGetLogic={inStage && zinger.in}
-        canGetBreak={inStage && zinger.out}
+        canGetLogic={inStage.in && zinger.in}
+        canGetBreak={inStage.out && zinger.out}
       />
       <ForestCheck
         id={5330}
         name="Enemy at Rear Tag"
         region="Forest Area 3"
-        canGetLogic={inStage && zinger.in}
-        canGetBreak={inStage && zinger.out}
+        canGetLogic={inStage.in && zinger.in}
+        canGetBreak={inStage.out && zinger.out}
       />
       <ForestCheck
         id={5331}
         name="Enemy Near DK Pad"
         region="Forest Area 3"
-        canGetLogic={inStage && zinger.in}
-        canGetBreak={inStage && zinger.out}
+        canGetLogic={inStage.in && zinger.in}
+        canGetBreak={inStage.out && zinger.out}
       />
       <ForestCheck
         id={5332}
         name="Enemy Near Face Puzzle Entrance"
         region="Forest Area 3"
-        canGetLogic={inStage && zinger.in}
-        canGetBreak={inStage && zinger.out}
+        canGetLogic={inStage.in && zinger.in}
+        canGetBreak={inStage.out && zinger.out}
       />
       <ForestCheck
         id={5333}
         name="Enemy Near Battle Arena"
         region="Forest Area 3"
-        canGetLogic={inStage && zinger.in}
-        canGetBreak={inStage && zinger.out}
+        canGetLogic={inStage.in && zinger.in}
+        canGetBreak={inStage.out && zinger.out}
       />
       <ForestCheck
         id={5334}
         name="Enemy Near High Warp 5"
         region="Forest Area 3"
-        canGetLogic={inStage && zinger.in}
-        canGetBreak={inStage && zinger.out}
+        canGetLogic={inStage.in && zinger.in}
+        canGetBreak={inStage.out && zinger.out}
       />
       <ForestCheck
         id={5335}
         name="Enemy at Top Of Mushroom"
         region="Forest Area 3"
-        canGetLogic={inStage && roof.in && klump}
-        canGetBreak={inStage && roof.out && klump}
+        canGetLogic={inStage.in && roof.in && klump}
+        canGetBreak={inStage.out && roof.out && klump}
       />
     </DropPool>
   )
