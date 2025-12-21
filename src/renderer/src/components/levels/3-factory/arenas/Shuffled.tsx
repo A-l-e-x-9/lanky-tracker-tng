942979889f9d5b88fdc2bf15241b0decedba3605
A-l-e-x-99 <alexs23743@gmail.com>
2025-12-21 11:10:35 -0500

    Part 2.

--- src/renderer/src/components/levels/3-factory/arenas/Shuffled.tsx
+++ src/renderer/src/components/levels/3-factory/arenas/Shuffled.tsx
@@ -2,83 +2,85 @@ import { useShallow } from 'zustand/react/shallow'
 import ArenaPool from '@renderer/components/pools/Arenas'
 import { useGeneralThing, useArena } from '@renderer/hooks/factory'
 import { useShuffledArenas, useBananaportAll } from '@renderer/hooks/settings'
-import { useClimbing, useVine } from '@renderer/hooks/kongs'
+import { useClimbing, useVine, usePunch } from '@renderer/hooks/kongs'
 import FactoryCheck from '../check'
 import useDonkStore from '@renderer/store'
 
 const Shuffled: React.FC = () => {
 const isBreathing = useGeneralThing()
 const canReachVanillaArena = useArena()
+const hasPrimatePunch = usePunch()
   return (
     <ArenaPool>
       <FactoryCheck
         id={13000}
-        name="Shuffled Battle Arena: Vanilla Arena (Lanky's reward for killing a Necky)"
-        region="Tiny Temple"
-        canGetLogic={canReachVanillaArena}
+        name="Shuffled Battle Arena: Vanilla Arena (in the R&D Room)"
+        region="R&D Room"
+        canGetLogic={canReachVanillaArena.in}
+        canGetBreak={canReachVanillaArena.out}
       />
       <FactoryCheck
         id={13001}
-        name="Shuffled Battle Arena: At the oasis"
-        region="Aztec Main Area"
+        name="Shuffled Battle Arena: In Chunky's dark room"
+        region="Storage and Arcade Area"
+        canGetLogic={isBreathing.in && hasPrimatePunch}
+        canGetBreak={isBreathing.out && hasPrimatePunch}
+      />
+      <FactoryCheck
+        id={13002}
+        name="Shuffled Battle Arena: On carpet at vanilla level start"
+        region="Factory Start Area"
         canGetLogic={isBreathing.in}
         canGetBreak={isBreathing.out}
       />
       <FactoryCheck
-        id={13002}
-        name="Shuffled Battle Arena: In Chunky's 5DT room"
-        region="5 Door Temple"
-        canGetLogic={canReachVanillaDirt2.in}
-        canGetBreak={canReachVanillaDirt2.out}
-      />
-      <FactoryCheck
         id={13003}
-        name="Shuffled Battle Arena: Back left of Tiny Temple's main room"
-        region="Tiny Temple"
-        canGetLogic={canReachTinyTemple.in}
-        canGetBreak={canReachTinyTemple.out}
+        name="Shuffled Battle Arena: Near carpet at vanilla level start"
+        region="Factory Start Area"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
       />
       <FactoryCheck
         id={13004}
-        name="Shuffled Battle Arena: TT starting room, low"
-        region="Tiny Temple"
-        canGetLogic={canReachTinyTemple.in}
-        canGetBreak={canReachTinyTemple.out}
+        name="Shuffled Battle Arena: Clock-in room, left side"
+        region="Factory Start Area"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
       />
       <FactoryCheck
         id={13005}
-        name="Shuffled Battle Arena: TT starting room, high"
-        region="Tiny Temple"
-        canGetLogic={canReachTinyTemple.in && hasDiddy && hasSlam}
-        canGetBreak={canReachTinyTemple.out && hasDiddy && hasSlam}
+        name="Shuffled Battle Arena: Clock-in room, right side"
+        region="Factory Start Area"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
       />
       <FactoryCheck
         id={13006}
-        name="Shuffled Battle Arena: Tiny's prison room"
-        region="Tiny Temple"
-        canGetLogic={canReachTinyTemple.in && iceMelted.in && hasDiving.in}
-        canGetBreak={canReachTinyTemple.out && iceMelted.out && hasDiving.out}
+        name="Shuffled Battle Arena: Top of the hatch"
+        region="Factory Start Area"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
       />
       <FactoryCheck
         id={13007}
-        name="Shuffled Battle Arena: Next to Tiny's cage"
-        region="Tiny Temple"
-        canGetLogic={canReachTinyTemple.in && iceMelted.in && hasDiving.in}
-        canGetBreak={canReachTinyTemple.out && iceMelted.out && hasDiving.out}
+        name="Shuffled Battle Arena: Center of the hatch"
+        region="Factory Start Area"
+        canGetLogic={isBreathing.in && hasClimbing}
+        canGetBreak={isBreathing.out}
       />
       <FactoryCheck
         id={13008}
-        name="Shuffled Battle Arena: In front of DK's room in the first tunnel"
-        region="Aztec Caves"
+        name="Shuffled Battle Arena: Bottom of the hatch"
+        region="Prod Room"
         canGetLogic={isBreathing.in}
         canGetBreak={isBreathing.out}
       />
       <FactoryCheck
         id={13009}
-        name="Shuffled Battle Arena: In DK's room in the first tunnel"
-        region="Aztec Caves"
-        canGetLogic={canReachDKKasplat.in}
-        canGetBreak={canReachDKKasplat.out}
+        name="Shuffled Battle Arena: In the tunnel to the Prod Room"
+        region="Prod Room"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
       />
       <FactoryCheck
         id={13010}
