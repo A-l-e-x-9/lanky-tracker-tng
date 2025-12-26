62f8653a3e0610ed85e9ba504f07fe903034f274
A-l-e-x-99 <alexs23743@gmail.com>
2025-12-25 23:14:16 -0500

    More flawed checks being fixed.

--- src/renderer/src/components/levels/5-forest/enemies/Owl.tsx
+++ src/renderer/src/components/levels/5-forest/enemies/Owl.tsx
@@ -16,85 +16,92 @@ const OwlEnemies: React.FC = () => {
         id={5342}
         name="Enemy 1 in the Yellow Tunnel"
         region="Forest Area 4"
-        canGetLogic={owlTree && zinger.in}
-        canGetBreak={owlTree && zinger.out}
+        canGetLogic={owlTree.in && zinger.in}
+        canGetBreak={owlTree.out && zinger.out}
       />
       <ForestCheck
         id={5343}
         name="Enemy 2 in the Yellow Tunnel"
         region="Forest Area 4"
-        canGetLogic={owlTree && zinger.in}
-        canGetBreak={owlTree && zinger.out}
+        canGetLogic={owlTree.in && zinger.in}
+        canGetBreak={owlTree.out && zinger.out}
       />
       <ForestCheck
         id={5344}
         name="Enemy 3 in the Yellow Tunnel"
         region="Forest Area 4"
-        canGetLogic={owlTree && klump}
+        canGetLogic={owlTree.in && klump}
+        canGetBreak={owlTree.out && klump}
       />
       <ForestCheck
         id={5345}
         name="Enemy 0 surrounding the big tree"
         region="Forest Area 4"
-        canGetLogic={owlTree && klump}
+        canGetLogic={owlTree.in && klump}
+        canGetBreak={owlTree.out && klump}
       />
       <ForestCheck
         id={5346}
         name="Enemy 1 surrounding the big tree"
         region="Forest Area 4"
-        canGetLogic={owlTree && klump}
+        canGetLogic={owlTree.in && klump}
+        canGetBreak={owlTree.out && klump}
       />
       <ForestCheck
         id={5347}
         name="Enemy at exit of yellow tunnel"
         region="Forest Area 4"
-        canGetLogic={owlTree && zinger.in}
-        canGetBreak={owlTree && zinger.out}
+        canGetLogic={owlTree.in && zinger.in}
+        canGetBreak={owlTree.out && zinger.out}
       />
       <ForestCheck
         id={5348}
         name="Enemy 0 at Melon Crate"
         region="Forest Area 4"
-        canGetLogic={owlTree && zinger.in}
-        canGetBreak={owlTree && zinger.out}
+        canGetLogic={owlTree.in && zinger.in}
+        canGetBreak={owlTree.out && zinger.out}
       />
       <ForestCheck
         id={5349}
         name="Enemy 1 at Melon Crate"
         region="Forest Area 4"
-        canGetLogic={owlTree && zinger.in}
-        canGetBreak={owlTree && zinger.out}
+        canGetLogic={owlTree.in && zinger.in}
+        canGetBreak={owlTree.out && zinger.out}
       />
       <ForestCheck
         id={5350}
         name="Enemy 2 at Melon Crate"
         region="Forest Area 4"
-        canGetLogic={owlTree && zinger.in}
-        canGetBreak={owlTree && zinger.out}
+        canGetLogic={owlTree.in && zinger.in}
+        canGetBreak={owlTree.out && zinger.out}
       />
       <ForestCheck
         id={5351}
         name="Enemy 0 in the tree stump's gauntlet"
         region="Forest Area 4"
-        canGetLogic={owlTree && mini && sax}
+        canGetLogic={owlTree.in && mini && sax}
+        canGetBreak={owlTree.out && mini && sax}
       />
       <ForestCheck
         id={5352}
         name="Enemy 1 in the tree stump's gauntlet"
         region="Forest Area 4"
-        canGetLogic={owlTree && mini && sax}
+        canGetLogic={owlTree.in && mini && sax}
+        canGetBreak={owlTree.out && mini && sax}
       />
       <ForestCheck
         id={5353}
         name="Enemy 2 in the tree stump's gauntlet"
         region="Forest Area 4"
-        canGetLogic={owlTree && mini && sax}
+        canGetLogic={owlTree.in && mini && sax}
+        canGetBreak={owlTree.out && mini && sax}
       />
       <ForestCheck
         id={5354}
         name="Enemy 3 in the tree stump's gauntlet"
         region="Forest Area 4"
-        canGetLogic={owlTree && mini && sax}
+        canGetLogic={owlTree.in && mini && sax}
+        canGetBreak={owlTree.out && mini && sax}
       />
     </DropPool>
   )
