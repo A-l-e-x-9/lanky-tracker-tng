a58d205a9f394f4759153ef7dd5533b1246de22f
A-l-e-x-99 <alexs23743@gmail.com>
2025-12-25 23:15:30 -0500

    And more.

--- src/renderer/src/components/levels/6-caves/enemies/Igloo.tsx
+++ src/renderer/src/components/levels/6-caves/enemies/Igloo.tsx
@@ -15,22 +15,22 @@ const IglooEnemies: React.FC = () => {
         id={6309}
         name="DK's 5-Door Igloo Right Enemy"
         region="Caves Igloo"
-        canGetLogic={igloo && bongos && kosha.in}
-        canGetBreak={igloo && bongos && kosha.out}
+        canGetLogic={igloo.in && bongos && kosha.in}
+        canGetBreak={igloo.out && bongos && kosha.out}
       />
       <CavesCheck
         id={6310}
         name="DK's 5-Door Igloo Left Enemy"
         region="Caves Igloo"
-        canGetLogic={igloo && bongos && kosha.in}
-        canGetBreak={igloo && bongos && kosha.out}
+        canGetLogic={igloo.in && bongos && kosha.in}
+        canGetBreak={igloo.out && bongos && kosha.out}
       />
       <CavesCheck
         id={6316}
         name="Enemy in Tiny's 5-Door Igloo"
         region="Caves Igloo"
-        canGetLogic={igloo && sax && kosha.in}
-        canGetBreak={igloo && sax && kosha.out}
+        canGetLogic={igloo.in && sax && kosha.in}
+        canGetBreak={igloo.out && sax && kosha.out}
       />
     </DropPool>
   )
