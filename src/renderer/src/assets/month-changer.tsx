6caaf63075afaaaffc35d00bab1a7283808ff6c6
A-l-e-x-99 <alexs23743@gmail.com>
2026-01-04 10:51:07 -0500

    This comment is possibly "null".

--- src/renderer/src/assets/month-changer.tsx
+++ src/renderer/src/assets/month-changer.tsx
@@ -3,19 +3,19 @@ const currentMonth = currentDate.getMonth()
 const getBody = document.getElementById('lanky-tracker')
 switch (currentMonth) {
   case 0: //January
-    getBody.style.backgroundColor = '#ffffff' //Makes the background white, because January is the dead of winter!
+    getBody!.style.backgroundColor = '#ffffff' //Makes the background white, because January is the dead of winter!
     break
   case 1: //February
-    getBody.style.backgroundColor = '#ff80ff' //Pink background, for Valentine's Day
+    getBody!.style.backgroundColor = '#ff80ff' //Pink background, for Valentine's Day
     break
   case 2: //March, outside of DST
-    getBody.style.backgroundColor = '#00c000' //Green background, for the coming of spring.
+    getBody!.style.backgroundColor = '#00c000' //Green background, for the coming of spring.
     break
   //See the DST version of this file for the other months.
   case 10: //November, outside of DST
-    getBody.style.backgroundColor = '#c00000' //Red background for late fall
+    getBody!.style.backgroundColor = '#c00000' //Red background for late fall
     break
   case 11: //December
-    getBody.style.backgroundImage = 'linear-gradient(135deg, #00c000, #ffffff, #c00000)' //Christmas background! :D
+    getBody!.style.backgroundImage = 'linear-gradient(135deg, #00c000, #ffffff, #c00000)' //Christmas background! :D
     break
 }
