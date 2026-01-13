bd14243ae69bb094d35b890686dbb3468e941701
A-l-e-x-99 <alexs23743@gmail.com>
2026-01-12 22:03:19 -0500

    Fixing CountSelector errors.

--- src/renderer/src/components/settings/CountSelector.tsx
+++ src/renderer/src/components/settings/CountSelector.tsx
@@ -15,6 +15,13 @@ type CountSelectorProps = {
 const CountSelector: React.FC<CountSelectorProps> = (props) => {
   const { storeKey, imgUrl, maxValue } = props
   const [num] = useDonkStore(useShallow((state) => [state[props.prefix][storeKey]]))
+  const bananaSeed = props.storeKey === 'goldBananas' && useDonkStore(useShallow((state) => state.winCondition.goldBananas)) ? 'all-bosses' : ''
+  const bpSeed = props.storeKey === 'dkBp' && props.storeKey === 'diddyBp' && props.storeKey === 'lankyBp' && props.storeKey === 'tinyBp' && props.storeKey === 'chunkyBp' && (useDonkStore(useShallow((state) => state.winCondition.goldBananas)) || useDonkStore(useShallow((state) => state.winCondition.kRoolChallenge))) ? 'all-bosses' : ''
+  const medalSeed = props.storeKey === 'bananaMedals' && useDonkStore(useShallow((state) => state.winCondition.bananaMedals)) ? 'all-bosses' : ''
+  const crownSeed = props.storeKey === 'crowns' && useDonkStore(useShallow((state) => state.winCondition.crowns)) ? 'all-bosses' : ''
+  const fairySeed = props.storeKey === 'fairies' && useDonkStore(useShallow((state) => state.winCondition.fairies)) ? 'all-bosses' : ''
+  const rainbowCoinSeed = props.storeKey === 'rainbowCoins' && useDonkStore(useShallow((state) => state.winCondition.rainbowCoins)) ? 'all-bosses' : ''
+  const pearlSeed = props.storeKey === 'pearls' && useDonkStore(useShallow((state) => state.winCondition.pearls)) ? 'all-bosses' : ''
 
   const clamp = (num: number): number => Math.min(Math.max(num, 0), maxValue)
 
