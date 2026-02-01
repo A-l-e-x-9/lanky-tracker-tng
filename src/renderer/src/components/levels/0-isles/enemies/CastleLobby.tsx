4182d6e6ced73706ed760ac03bd93ab564ef07c9
A-l-e-x-99 <alexs23743@gmail.com>
2026-02-01 14:50:06 -0500

    Starting to fix the errors...

--- src/renderer/src/components/levels/0-isles/enemies/CastleLobby.tsx
+++ src/renderer/src/components/levels/0-isles/enemies/CastleLobby.tsx
@@ -1,35 +1,35 @@
 import DropPool from '@renderer/components/pools/Drops'
 import { usePlayLobby } from '@renderer/hooks/isles'
-import { useDefeatKosha } from '@renderer/hooks/enemies'
+import { useDefeatToughEnemy } from '@renderer/hooks/enemies'
 import { useAnyKong } from '@renderer/hooks/kongs'
 import IslesCheck from '../check'
 
 const CastleLobbyEnemies: React.FC = () => {
   const playCastle = usePlayLobby('Creepy Castle')
   const anyKong = useAnyKong()
-  const kosha = useDefeatKosha()
+  const kosha = useDefeatToughEnemy()
   return (
     <DropPool>
       <IslesCheck
         id={315}
         name="Castle Lobby Left Enemy"
         region="Caves-Helm Lobbies"
-        canGetLogic={anyKong && playCastle.in && kosha.in}
-        canGetBreak={anyKong && playCastle.out && kosha.out}
+        canGetLogic={anyKong && playCastle.in && kosha}
+        canGetBreak={anyKong && playCastle.out && kosha}
       />
       <IslesCheck
         id={316}
         name="Castle Lobby Near Right Enemy"
         region="Caves-Helm Lobbies"
-        canGetLogic={anyKong && playCastle.in && kosha.in}
-        canGetBreak={anyKong && playCastle.out && kosha.out}
+        canGetLogic={anyKong && playCastle.in && kosha}
+        canGetBreak={anyKong && playCastle.out && kosha}
       />
       <IslesCheck
         id={317}
         name="Castle Lobby Far Right Enemy"
         region="Caves-Helm Lobbies"
-        canGetLogic={anyKong && playCastle.in && kosha.in}
-        canGetBreak={anyKong && playCastle.out && kosha.out}
+        canGetLogic={anyKong && playCastle.in && kosha}
+        canGetBreak={anyKong && playCastle.out && kosha}
       />
     </DropPool>
   )
