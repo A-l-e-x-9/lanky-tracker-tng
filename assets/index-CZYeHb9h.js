af8061a4544d812cb4933944273c120916305f3e
A-l-e-x-9 <alexs23743@gmail.com>
2026-02-01 13:59:52 +0000

    Deploying to gh-pages from @ A-l-e-x-9/lanky-tracker-tng@be42646c0ce62e502d4e9e3b1e159102a8a4e276 🚀

--- assets/index-CZYeHb9h.js
+++ assets/index-CZYeHb9h.js
@@ -42575,10 +42575,9 @@ const CountSelector = (props) => {
 };
 const SimpleIcon = (props) => {
   const isDKRapSeed = (props.storeKey === "dk" || props.storeKey === "coconut" || props.storeKey === "strong" || props.storeKey === "diddy" || props.storeKey === "peanut" || props.storeKey === "guitar" || props.storeKey === "rocket" || props.storeKey === "lanky" || props.storeKey === "grape" || props.storeKey === "stand" || props.storeKey === "trombone" || props.storeKey === "balloon" || props.storeKey === "tiny" || props.storeKey === "twirl" || props.storeKey === "mini" || props.storeKey === "chunky" || props.storeKey === "pineapple" || props.storeKey === "orange" || props.storeKey === "barrel" || props.storeKey === "climbing" || props.storeKey === "cranky") && useDonkStore(useShallow((state) => state.winCondition.takeItToTheFridge)) ? "all-bosses" : "";
-  const isKRoolChallengeSeed = props.storeKey === "snide" && useDonkStore(useShallow((state) => state.winCondition.kRoolChallenge)) ? "all-bosses" : "";
   const beanSeed = props.storeKey === "bean" && useDonkStore(useShallow((state) => state.winCondition.theBean)) ? "all-bosses" : "";
   const value = useDonkStore((state) => state[props.prefix][props.storeKey]);
-  const classes = `simple-icon ${props.prefix}-${props.storeKey} ${value ? "have" : "have-not"} ${props.className} ${isDKRapSeed} ${isKRoolChallengeSeed} ${beanSeed}`;
+  const classes = `simple-icon ${props.prefix}-${props.storeKey} ${value ? "have" : "have-not"} ${props.className} ${isDKRapSeed} ${beanSeed}`;
   return /* @__PURE__ */ jsxRuntimeExports.jsxs("div", { className: classes, onClick: () => props.updateItem(props.storeKey, !value), children: [
     /* @__PURE__ */ jsxRuntimeExports.jsx(
       "img",
