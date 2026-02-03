fc785e187f29b7e9bdbd23a75ebac2f30e874a94
A-l-e-x-99 <alexs23743@gmail.com>
2026-02-03 10:50:45 -0500

    Fixed an error in the "bonuses" win con; putting in shuffled Helm Arenas

--- src/renderer/src/components/levels/8-helm/index.tsx
+++ src/renderer/src/components/levels/8-helm/index.tsx
@@ -1,23 +1,38 @@
+import useDonkStore from '@renderer/store'
+import { useShallow } from 'zustand/react/shallow'
 import DropPool from '@renderer/components/pools/Drops'
 import BlastArena from './arenas/Blast'
 import BossCheck from './boss'
 import EnemyLocations from './enemies'
 import EndOfHelmFairies from './fairies/End'
 import MedalLocations from './medals'
+import ShuffledArenas from './arenas/Shuffled'
+import ShuffledCrates from './crates/Shuffled'
+import ShuffledDirt from './dirt/Shuffled'
 import ShuffledFairies from './fairies/Shuffled'
 
 const HelmChecks: React.FC = () => {
+const isKremKaptureSeed = useDonkStore(useShallow((state) => state.winCondition.kremlingKapture)) ? 'foolish' : ''
   return (
+  <>
     <div className="grid">
       <BlastArena />
       <MedalLocations />
       <BossCheck />
       <EndOfHelmFairies />
+    </div>
+    <div className={`grid ${isKremKaptureSeed}`}>
       <DropPool>
         <EnemyLocations />
       </DropPool>
+    </div>
+    <div className="grid">
+      <ShuffledArenas />
+      <ShuffledCrates />
+      <ShuffledDirt />
       <ShuffledFairies />
     </div>
+  </>
   )
 }
 
