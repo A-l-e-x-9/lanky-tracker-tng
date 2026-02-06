999e109ab2d7699d62c700f107a72d79cb675846
A-l-e-x-99 <alexs23743@gmail.com>
2026-02-05 22:49:51 -0500

    Shuffled shops now actually work for Isles! :D

--- src/renderer/src/components/levels/0-isles/shops.tsx
+++ src/renderer/src/components/levels/0-isles/shops.tsx
@@ -1,13 +1,35 @@
+import { useShallow } from 'zustand/react/shallow'
+import useDonkStore from '@renderer/store'
 import ShopGenerator from '@renderer/components/pools/ShopGenerator'
 import ShopPool from '@renderer/components/pools/Shops'
 import { useIslesKremAscent } from '@renderer/hooks/isles'
-import { useShuffledShops } from '@renderer/hooks/settings'
 import { useCranky, useSnide } from '@renderer/hooks/kongs'
 
 const Vanilla: React.FC = () => {
   const kremAscent = useIslesKremAscent()
   const cranky = useCranky()
   const snide = useSnide()
+  const shuffledShops = useDonkStore(useShallow((state) => state.shuffledIslesShops.islesSwitchUp))
+  if (shuffledShops) {
+  return (
+  <>
+    <ShopGenerator
+      baseId={110}
+      baseName="Isles Cranky"
+      level="DK Isles"
+      region="Shops"
+      inLogic={kremAscent && cranky}
+    />
+    <ShopGenerator
+      baseId={140}
+      baseName="Turn in Isles Blueprint for"
+      level="DK Isles"
+      region="Shops"
+      inLogic={snide}
+    />
+  </>
+  )
+  } else {
   return (
   <>
     <ShopGenerator
@@ -26,36 +48,11 @@ const Vanilla: React.FC = () => {
     />
   </>
   )
-}
-
-const Shuffled: React.FC = () => {
-  const kremAscent = useIslesKremAscent()
-  const cranky = useCranky()
-  const snide = useSnide()
-
-  return (
-    <>
-      <ShopGenerator
-        baseId={140}
-        baseName="Isles Cranky Location"
-        level="DK Isles"
-        region="Shops"
-        inLogic={cranky}
-      />
-      <ShopGenerator
-        baseId={170}
-        baseName="Isles Snide Location"
-        level="DK Isles"
-        region="Shops"
-        inLogic={kremAscent && snide}
-      />
-    </>
-  )
+  }
 }
 
 const ShopLocations: React.FC = () => {
-  const locations = useShuffledShops() ? <Shuffled /> : <Vanilla />
-  return <ShopPool>{locations}</ShopPool>
+  return <ShopPool><Vanilla /></ShopPool>
 }
 
 export default ShopLocations
