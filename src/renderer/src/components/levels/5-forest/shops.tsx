81c7de28615089a78b0fea48c973815a12f6135e
A-l-e-x-99 <alexs23743@gmail.com>
2026-02-07 12:20:14 -0500

    Now making shuffled shops work for Forest.

--- src/renderer/src/components/levels/5-forest/shops.tsx
+++ src/renderer/src/components/levels/5-forest/shops.tsx
@@ -1,7 +1,8 @@
+import { useShallow } from 'zustand/react/shallow'
+import useDonkStore from '@renderer/store'
 import ShopGenerator from '@renderer/components/pools/ShopGenerator'
 import ShopPool from '@renderer/components/pools/Shops'
 import { useForestBean, useForestDay, usePlayForest } from '@renderer/hooks/forest'
-import { useShuffledShops } from '@renderer/hooks/settings'
 import { useCranky, whatAFunky, useSnide } from '@renderer/hooks/kongs'
 
 const Vanilla: React.FC = () => {
@@ -11,6 +12,9 @@ const Vanilla: React.FC = () => {
   const hasCranky = useCranky()
   const hasFunky = whatAFunky()
   const hasSnide = useSnide()
+  const [crankyFunky, crankySnide] = useDonkStore(useShallow((state) => [state.shuffledForestCranky.forestCrankyFunky, state.shuffledForestCranky.forestCrankySnide]))
+  const [funkyCranky, funkySnide] = useDonkStore(useShallow((state) => [state.shuffledForestFunky.forestFunkyCranky, state.shuffledForestFunky.forestFunkySnide]))
+  const [snideCranky, snideFunky] = useDonkStore(useShallow((state) => [state.shuffledForestSnide.forestSnideCranky, state.shuffledForestSnide.forestSnideFunky]))
   return (
     <>
       <ShopGenerator
@@ -18,70 +22,31 @@ const Vanilla: React.FC = () => {
         baseName="Forest Cranky"
         level="Fungi Forest"
         region="Shops"
-        inLogic={hasCranky && inStage.in}
-        outLogic={hasCranky && inStage.out}
+        inLogic={hasCranky && (crankyFunky ? beanstalk.in : crankySnide ? day.in : inStage.in)}
+        outLogic={hasCranky && (crankyFunky ? beanstalk.out : crankySnide ? day.out : inStage.out)}
       />
       <ShopGenerator
         baseId={5120}
         baseName="Forest Funky"
         level="Fungi Forest"
         region="Shops"
-        inLogic={hasFunky && beanstalk.in}
-        outLogic={hasFunky && beanstalk.out}
+        inLogic={hasFunky && (funkyCranky ? inStage.in : funkySnide ? day.in : beanstalk.in)}
+        outLogic={hasFunky && (funkyCranky ? inStage.out : funkySnide ? day.out : beanstalk.out)}
       />
       <ShopGenerator
         baseId={5140}
         baseName="Turn in Forest Blueprint for"
         level="Fungi Forest"
         region="Shops"
-        inLogic={hasSnide && day.in}
-        outLogic={hasSnide && day.out}
-      />
-    </>
-  )
-}
-
-const Shuffled: React.FC = () => {
-  const inStage = usePlayForest()
-  const beanstalk = useForestBean()
-  const day = useForestDay()
-  const hasCranky = useCranky()
-  const hasFunky = whatAFunky()
-  const hasSnide = useSnide()
-
-  return (
-    <>
-      <ShopGenerator
-        baseId={5140}
-        baseName="Forest Cranky Location"
-        level="Fungi Forest"
-        region="Shops"
-        inLogic={hasCranky && inStage.in}
-        outLogic={hasCranky && inStage.out}
-      />
-      <ShopGenerator
-        baseId={5150}
-        baseName="Forest Funky Location"
-        level="Fungi Forest"
-        region="Shops"
-        inLogic={hasFunky && beanstalk.in}
-        outLogic={hasFunky && beanstalk.out}
-      />
-      <ShopGenerator
-        baseId={5170}
-        baseName="Forest Snide Location"
-        level="Fungi Forest"
-        region="Shops"
-        inLogic={hasSnide && day.in}
-        outLogic={hasSnide && day.out}
+        inLogic={hasSnide && (snideCranky ? inStage.in : snideFunky ? beanstalk.in : day.in)}
+        outLogic={hasSnide && (snideCranky ? inStage.out : snideFunky ? beanstalk.out : day.out)}
       />
     </>
   )
 }
 
 const ShopLocations: React.FC = () => {
-  const locations = useShuffledShops() ? <Shuffled /> : <Vanilla />
-  return <ShopPool>{locations}</ShopPool>
+  return <ShopPool><Vanilla /></ShopPool>
 }
 
 export default ShopLocations
