8981fe3fb586798841527577022906cbaf3c75a3
A-l-e-x-99 <alexs23743@gmail.com>
2026-02-07 10:31:12 -0500

    Making the shop shuffler actually work for Factory.

--- src/renderer/src/components/levels/3-factory/shops.tsx
+++ src/renderer/src/components/levels/3-factory/shops.tsx
@@ -1,7 +1,8 @@
+import { useShallow } from 'zustand/react/shallow'
+import useDonkStore from '@renderer/store'
 import ShopGenerator from '@renderer/components/pools/ShopGenerator'
 import ShopPool from '@renderer/components/pools/Shops'
 import { useFactoryTesting, usePlayFactory } from '@renderer/hooks/factory'
-import { useShuffledShops } from '@renderer/hooks/settings'
 import { useCranky, whatAFunky, useCandy, useSnide } from '@renderer/hooks/kongs'
 
 const Vanilla: React.FC = () => {
@@ -11,6 +12,10 @@ const Vanilla: React.FC = () => {
   const hasFunky = whatAFunky()
   const hasCandy = useCandy()
   const hasSnide = useSnide()
+  const [crankyFunky, crankySnide] = useDonkStore(useShallow((state) => [state.shuffledFactoryCranky.factoryCrankyFunky, state.shuffledFactoryCranky.factoryCrankySnide]))
+  const [funkyCranky, funkyCandy] = useDonkStore(useShallow((state) => [state.shuffledFactoryFunky.factoryFunkyCranky, state.shuffledFactoryFunky.factoryFunkyCandy]))
+  const [candyFunky, candySnide] = useDonkStore(useShallow((state) => [state.shuffledFactoryCandy.factoryCandyFunky, state.shuffledFactoryCandy.factoryCandySnide]))
+  const [snideCranky, snideCandy] = useDonkStore(useShallow((state) => [state.shuffledFactorySnide.factorySnideCranky, state.shuffledFactorySnide.factorySnideCandy]))
   return (
     <>
       <ShopGenerator
@@ -18,86 +23,39 @@ const Vanilla: React.FC = () => {
         baseName="Factory Cranky"
         level="Frantic Factory"
         region="Shops"
-        inLogic={hasCranky && inStage.in}
-        outLogic={hasCranky && inStage.out}
+        inLogic={hasCranky && ((crankyFunky || crankySnide) ? testing.in : inStage.in)}
+        outLogic={hasCranky && ((crankyFunky || crankySnide) ? testing.out : inStage.out)}
       />
       <ShopGenerator
         baseId={3120}
         baseName="Factory Funky"
         level="Frantic Factory"
         region="Shops"
-        inLogic={hasFunky && inStage.in}
-        outLogic={hasFunky && inStage.out}
+        inLogic={hasFunky && ((funkyCranky || funkyCandy) ? inStage.in : testing.in)}
+        outLogic={hasFunky && ((funkyCranky || funkyCandy) ? inStage.out : testing.out)}
       />
       <ShopGenerator
         baseId={3130}
         baseName="Factory Candy"
         level="Frantic Factory"
         region="Shops"
-        inLogic={hasCandy && inStage.in}
-        outLogic={hasCandy && inStage.out}
+        inLogic={hasCandy && ((candyFunky || candySnide) ? testing.in : inStage.in)}
+        outLogic={hasCandy && ((candyFunky || candySnide) ? testing.out : inStage.out)}
       />
       <ShopGenerator
         baseId={3140}
         baseName="Turn in Factory Blueprint for"
         level="Frantic Factory"
         region="Shops"
-        inLogic={hasSnide && testing.in}
-        outLogic={hasSnide && testing.out}
-      />
-    </>
-  )
-}
-
-const Shuffled: React.FC = () => {
-  const inStage = usePlayFactory()
-  const testing = useFactoryTesting()
-  const hasCranky = useCranky()
-  const hasFunky = whatAFunky()
-  const hasCandy = useCandy()
-  const hasSnide = useSnide()
-
-  return (
-    <>
-      <ShopGenerator
-        baseId={3140}
-        baseName="Factory Cranky Location"
-        level="Frantic Factory"
-        region="Shops"
-        inLogic={hasCranky && inStage.in}
-        outLogic={hasCranky && inStage.out}
-      />
-      <ShopGenerator
-        baseId={3150}
-        baseName="Factory Funky Location"
-        level="Frantic Factory"
-        region="Shops"
-        inLogic={hasFunky && inStage.in}
-        outLogic={hasFunky && inStage.out}
-      />
-      <ShopGenerator
-        baseId={3160}
-        baseName="Factory Candy Location"
-        level="Frantic Factory"
-        region="Shops"
-        inLogic={hasCandy && inStage.in}
-        outLogic={hasCandy && inStage.out}
-      />
-      <ShopGenerator
-        baseId={3170}
-        baseName="Factory Snide Location"
-        level="Frantic Factory"
-        region="Shops"
-        inLogic={hasSnide && testing.in}
-        outLogic={hasSnide && testing.out}
+        inLogic={hasSnide && ((snideCranky || snideCandy) ? inStage.in : testing.in)}
+        outLogic={hasSnide && ((snideCranky || snideCandy) ? inStage.out : testing.out)}
       />
     </>
   )
 }
 
 const ShopLocations: React.FC = () => {
-  const locations = useShuffledShops() ? <Shuffled /> : <Vanilla />
-  return <ShopPool>{locations}</ShopPool>
+  return <ShopPool><Vanilla /></ShopPool>
 }
 
 export default ShopLocations
