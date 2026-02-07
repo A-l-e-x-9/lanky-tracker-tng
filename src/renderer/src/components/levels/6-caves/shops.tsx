c84171371f0aaa63149c4c27442d7e8fb8bbb197
A-l-e-x-99 <alexs23743@gmail.com>
2026-02-07 13:45:53 -0500

    Making the shop shuffler work for Crystal Caves.

--- src/renderer/src/components/levels/6-caves/shops.tsx
+++ src/renderer/src/components/levels/6-caves/shops.tsx
@@ -1,3 +1,5 @@
+import { useShallow } from 'zustand/react/shallow'
+import useDonkStore from '@renderer/store'
 import ShopGenerator from '@renderer/components/pools/ShopGenerator'
 import ShopPool from '@renderer/components/pools/Shops'
 import { usePlayCaves, useCanAccessSnide } from '@renderer/hooks/caves'
@@ -11,6 +13,8 @@ const ShopLocations: React.FC = () => {
   const hasFunky = whatAFunky()
   const hasCandy = useCandy()
   const hasSnide = useCanAccessSnide()
+  const [inSnideCave] = useDonkStore(useShallow((state) => [state.shuffledCavesCranky.cavesCrankySnide || state.shuffledCavesFunky.cavesFunkySnide || state.shuffledCavesCandy.cavesCandySnide]))
+  const [outOfSnideCave] = useDonkStore(useShallow((state) => [state.shuffledCavesSnide.cavesSnideCranky || state.shuffledCavesSnide.cavesSnideFunky || state.shuffledCavesSnide.cavesSnideCandy]))
   return (
     <ShopPool>
       <ShopGenerator
@@ -18,32 +22,32 @@ const ShopLocations: React.FC = () => {
         baseName="Caves Cranky"
         level="Crystal Caves"
         region="Shops"
-        inLogic={hasCranky && inStage.in && !angery}
-        outLogic={hasCranky && (inStage.out || angery)}
+        inLogic={hasCranky && inStage.in && (inSnideCave ? : !angery)}
+        outLogic={hasCranky && (inSnideCave ? instage.out : (inStage.out || angery))}
       />
       <ShopGenerator
         baseId={6120}
         baseName="Caves Funky"
         level="Crystal Caves"
         region="Shops"
-        inLogic={hasFunky && inStage.in && !angery}
-        outLogic={hasFunky && (inStage.out || angery)}
+        inLogic={hasFunky && inStage.in && (inSnideCave ? : !angery)}
+        outLogic={hasFunky && (inSnideCave ? instage.out : (inStage.out || angery))}
       />
       <ShopGenerator
         baseId={6130}
         baseName="Caves Candy"
         level="Crystal Caves"
         region="Shops"
-        inLogic={hasCandy && inStage.in && !angery}
-        outLogic={hasCandy && (inStage.out || angery)}
+        inLogic={hasCandy && inStage.in && (inSnideCave ? : !angery)}
+        outLogic={hasCandy && (inSnideCave ? instage.out : (inStage.out || angery))}
       />
       <ShopGenerator
         baseId={6140}
         baseName="Turn in Caves Blueprint for"
         level="Crystal Caves"
         region="Shops"
-        inLogic={hasSnide && inStage.in}
-        outLogic={hasSnide && inStage.out} //Snide is in a "safe zone", so no Angry Caves check
+        inLogic={hasSnide && inStage.in && (outOfSnideCave ? !angery)}
+        outLogic={hasSnide && (outOfSnideCave ? (inStage.out || angery) : inStage.out)} //Snide is in a "safe zone", so no Angry Caves check for his vanilla location
       />
     </ShopPool>
   )
