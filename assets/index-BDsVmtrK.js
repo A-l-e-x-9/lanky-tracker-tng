e74162a6f5ae42ec1b2b50fc5e52b8c13e0e7989
A-l-e-x-9 <alexs23743@gmail.com>
2026-02-25 22:25:43 +0000

    Deploying to gh-pages from @ A-l-e-x-9/lanky-tracker-tng@1841f9e47b83d8bd92aeca6c2ee57a0de0128edd 🚀

--- assets/index-BDsVmtrK.js
+++ assets/index-BDsVmtrK.js
@@ -22050,7 +22050,7 @@ const useDkTunnelGb = () => {
   const strong = useStrong();
   return {
     in: tunnel.in && strong,
-    out: tunnel.in || tunnel.out
+    out: tunnel.out
   };
 };
 const useDk5DoorGb = () => {
@@ -26500,51 +26500,205 @@ const Shuffled$p = () => {
 };
 const ShuffledFairies$6 = () => useShuffleFairies() ? /* @__PURE__ */ jsxRuntimeExports.jsx(Shuffled$p, {}) : null;
 const Shuffled$o = () => {
-  const thing = useGeneralThing$4();
+  const DKVanillaKasplat = useCoconutKasplat();
+  const diddyVanillaKasplat = useOasisKasplat();
+  const lankyVanillaKasplat = useLlamaLavaKasplat();
+  const tinyVanillaKasplat = useTunnelKasplat();
+  const chunkyVanillaKasplat = useChunkyKasplat();
+  const isBreathing = useGeneralThing$4();
+  const canDoQSGB = useDkTunnelGb();
+  const canEnterBack = useAztecBack();
+  const canGoInLlamaTemple = useAztecLlamaTemple();
+  const canGoInTinyTemple = useAztecTinyTemple();
+  const canGetPastTTIce = useTinyTempleIce();
+  const canDoDK5DT = useDk5DoorGb();
+  const hasDiving = useDive();
+  const hasVines = useVine();
+  const hasClimbing = useClimbing();
+  const hasJetbarrel = useRocket();
+  const hasDiddy = useDiddy();
+  const hasTiny = useTiny();
+  const hasPineapples = usePineapple();
+  const hasGrapes = useGrape();
   return /* @__PURE__ */ jsxRuntimeExports.jsxs(KasplatPool, { children: [
     /* @__PURE__ */ jsxRuntimeExports.jsx(
       AztecCheck,
       {
-        id: 2250,
-        name: "Aztec Kasplat Location #1",
-        canGetLogic: thing.in,
-        canGetBreak: thing.out
+        id: 52e3,
+        name: "Shuffled Kasplat: DK's Vanilla Location (a sub-tunnel at the level start)",
+        region: "Aztec Caves",
+        canGetLogic: DKVanillaKasplat.in,
+        canGetBreak: DKVanillaKasplat.out
       }
     ),
     /* @__PURE__ */ jsxRuntimeExports.jsx(
       AztecCheck,
       {
-        id: 2251,
-        name: "Aztec Kasplat Location #2",
-        canGetLogic: thing.in,
-        canGetBreak: thing.out
+        id: 52001,
+        name: "Shuffled Kasplat: Diddy's Vanilla Location (on top of the Tiny Temple)",
+        region: "Aztec Main Area",
+        canGetLogic: diddyVanillaKasplat.in,
+        canGetBreak: diddyVanillaKasplat.out
       }
     ),
     /* @__PURE__ */ jsxRuntimeExports.jsx(
       AztecCheck,
       {
-        id: 2252,
-        name: "Aztec Kasplat Location #3",
-        canGetLogic: thing.in,
-        canGetBreak: thing.out
+        id: 52002,
+        name: "Shuffled Kasplat: Lanky's Vanilla Location (the lava pedestal GB)",
+        region: "Llama Temple",
+        canGetLogic: lankyVanillaKasplat.in,
+        canGetBreak: lankyVanillaKasplat.out
       }
     ),
     /* @__PURE__ */ jsxRuntimeExports.jsx(
       AztecCheck,
       {
-        id: 2253,
-        name: "Aztec Kasplat Location #4",
-        canGetLogic: thing.in,
-        canGetBreak: thing.out
+        id: 52003,
+        name: "Shuffled Kasplat: Tiny's Vanilla Location (first 3-way intersection of middle tunnel)",
+        region: "Aztec Caves",
+        canGetLogic: tinyVanillaKasplat.in,
+        canGetBreak: tinyVanillaKasplat.out
       }
     ),
     /* @__PURE__ */ jsxRuntimeExports.jsx(
       AztecCheck,
       {
-        id: 2254,
-        name: "Aztec Kasplat Location #5",
-        canGetLogic: thing.in,
-        canGetBreak: thing.out
+        id: 52004,
+        name: "Shuffled Kasplat: Chunky's Vanilla Location (in his 5DT room)",
+        region: "5 Door Temple",
+        canGetLogic: chunkyVanillaKasplat.in,
+        canGetBreak: chunkyVanillaKasplat.out
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 52005,
+        name: "Shuffled Kasplat: In DK's quicksand tunnel",
+        region: "Aztec Caves",
+        canGetLogic: canDoQSGB.in,
+        canGetBreak: canDoQSGB.out
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 52006,
+        name: "Shuffled Kasplat: On the oasis",
+        region: "Aztec Main Area",
+        canGetLogic: isBreathing.in,
+        canGetBreak: isBreathing.out
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 52007,
+        name: "Shuffled Kasplat: On top of the Llama's cage",
+        region: "Aztec Main Area",
+        canGetLogic: isBreathing.in && hasClimbing && (hasVines || hasJetbarrel),
+        canGetBreak: isBreathing.out && (hasDiddy || hasTiny)
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 52008,
+        name: "Shuffled Kasplat: Near the giant boulder",
+        region: "Aztec Caves",
+        canGetLogic: canEnterBack.in,
+        canGetBreak: canEnterBack.out
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 52009,
+        name: "Shuffled Kasplat: In the vase room",
+        region: "Aztec Caves",
+        canGetLogic: isBreathing.in && hasPineapples,
+        canGetBreak: isBreathing.out && hasPineapples
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 52010,
+        name: "Shuffled Kasplat: Back top of the 5-Door Temple",
+        region: "Aztec Main Area",
+        canGetLogic: canEnterBack.in,
+        canGetBreak: canEnterBack.out
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 52011,
+        name: "Shuffled Kasplat: Platform near Snide's",
+        region: "Aztec Main Area",
+        canGetLogic: canEnterBack.in,
+        canGetBreak: canEnterBack.out
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 52012,
+        name: "Shuffled Kasplat: Behind the Llama",
+        region: "Llama Temple",
+        canGetLogic: canGoInLlamaTemple.in,
+        canGetBreak: canGoInLlamaTemple.out
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 52013,
+        name: "Shuffled Kasplat: At Tiny's Prison",
+        region: "Tiny Temple",
+        canGetLogic: canGoInTinyTemple.in && canGetPastTTIce.in && hasDiving.in,
+        canGetBreak: canGoInTinyTemple.out && canGetPastTTIce.out && hasDiving.out
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 52014,
+        name: "Shuffled Kasplat: Behind Diddy's gongs",
+        region: "Aztec Main Area",
+        canGetLogic: canEnterBack.in,
+        canGetBreak: canEnterBack.out
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 52015,
+        name: "Shuffled Kasplat: A contestant on Match Game '64",
+        region: "Llama Temple",
+        canGetLogic: canGoInLlamaTemple.in && hasGrapes,
+        canGetBreak: canGoInLlamaTemple.out && hasGrapes
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 52016,
+        name: "Shuffled Kasplat: At Tiny's barrel in the Tiny Temple",
+        region: "Tiny Temple",
+        canGetLogic: canGoInTinyTemple.in,
+        canGetBreak: canGoInTinyTemple.out
+      }
+    ),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      AztecCheck,
+      {
+        id: 52017,
+        name: "Shuffled Kasplat: Inside DK's 5DT room",
+        region: "5 Door Temple",
+        canGetLogic: canDoDK5DT.in,
+        canGetBreak: canDoDK5DT.out
       }
     )
   ] });
