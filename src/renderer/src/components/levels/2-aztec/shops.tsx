10c4c95b6f4d30d4c666e8b2d40b021a5493ef96
A-l-e-x-99 <alexs23743@gmail.com>
2026-02-24 23:01:40 -0500

    Here comes Aztec.

--- src/renderer/src/components/levels/2-aztec/shops.tsx
+++ src/renderer/src/components/levels/2-aztec/shops.tsx
@@ -12,6 +12,7 @@ const Vanilla: React.FC = () => {
   const hasFunky = whatAFunky()
   const hasCandy = useCandy()
   const hasSnide = useSnide()
+  const [crankyNoSwitch, funkyCranky, snideCranky] = useDonkStore(useShallow((state) => [state.shuffledAztecCranky.aztecCrankyNoSwitch, state.shuffledAztecFunky.aztecFunkyCranky, state.shuffledAztecSnide.aztecSnideCranky]))
   const [crankyCandy, funkyCandy, snideCandy] = useDonkStore(useShallow((state) => [state.shuffledAztecCranky.aztecCrankyCandy, state.shuffledAztecFunky.aztecFunkyCandy, state.shuffledAztecSnide.aztecSnideCandy]))
   const [candyCranky, candyFunky, candySnide] = useDonkStore(useShallow((state) => [state.shuffledAztecCandy.aztecCandyCranky, state.shuffledAztecCandy.aztecCandyFunky, state.shuffledAztecCandy.aztecCandySnide]))
   return (
@@ -20,7 +21,7 @@ const Vanilla: React.FC = () => {
         baseId={2110}
         baseName="Aztec Cranky"
         level="Angry Aztec"
-        region="Shops"
+        region={crankyNoSwitch ? "Aztec Caves" : "Aztec Main Area"}
         inLogic={hasCranky && (crankyCandy ? aztecFront.in : aztecBack.in)}
         outLogic={hasCranky && (crankyCandy ? aztecFront.out : aztecBack.out)}
       />
@@ -28,7 +29,7 @@ const Vanilla: React.FC = () => {
         baseId={2120}
         baseName="Aztec Funky"
         level="Angry Aztec"
-        region="Shops"
+        region={funkyCranky ? "Aztec Caves" : "Aztec Main Area"}
         inLogic={hasFunky && (funkyCandy ? aztecFront.in : aztecBack.in)}
         outLogic={hasFunky && (funkyCandy ? aztecFront.out : aztecBack.out)}
       />
@@ -36,7 +37,7 @@ const Vanilla: React.FC = () => {
         baseId={2130}
         baseName="Aztec Candy"
         level="Angry Aztec"
-        region="Shops"
+        region={candyCranky ? "Aztec Caves" : "Aztec Main Area"}
         inLogic={hasCandy && ((candyCranky || candyFunky || candySnide) ? aztecBack.in : aztecFront.in)}
         outLogic={hasCandy && ((candyCranky || candyFunky || candySnide) ? aztecBack.out : aztecFront.out)}
       />
@@ -44,7 +45,7 @@ const Vanilla: React.FC = () => {
         baseId={2140}
         baseName="Turn in Aztec Blueprint for"
         level="Angry Aztec"
-        region="Shops"
+        region={snideCranky ? "Aztec Caves" : "Aztec Main Area"}
         inLogic={hasSnide && (snideCandy ? aztecFront.in : aztecBack.in)}
         outLogic={hasSnide && (snideCandy ? aztecFront.out : aztecBack.out)}
       />
