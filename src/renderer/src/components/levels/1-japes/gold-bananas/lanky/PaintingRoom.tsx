64b0275cc1b966bd0ece6e92b9bd273eb9a3a52b
A-l-e-x-99 <alexs23743@gmail.com>
2026-02-26 18:36:49 -0500

    A necessary change for the Fairy win con

--- src/renderer/src/components/levels/1-japes/gold-bananas/lanky/PaintingRoom.tsx
+++ src/renderer/src/components/levels/1-japes/gold-bananas/lanky/PaintingRoom.tsx
@@ -1,3 +1,5 @@
+import useDonkStore from '@renderer/store'
+import { useShallow } from 'zustand/react/shallow'
 import GBPool from '@renderer/components/pools/GB'
 import Miniboss from '@renderer/components/pools/Miniboss'
 import { useLankyPaintingGb } from '@renderer/hooks/japes'
@@ -5,6 +7,21 @@ import JapesCheck from '../../check'
 
 const PaintingRoom: React.FC = () => {
   const paintingGb = useLankyPaintingGb()
+  const isFairySeed = useDonkStore(useShallow((state) => state.winCondition.fairies))
+  const fairiesInRotation = useDonkStore(useShallow((state) => state.settings.poolFairies))
+  if (isFairySeed && !fairiesInRotation) {
+  return (
+  <GBPool>
+    <JapesCheck
+      id={1023}
+      name="Lanky's Painting Room"
+      region="Japes Caves and Mines"
+      canGetLogic={paintingGb.in}
+      canGetBreak={paintingGb.out}
+    />
+  </GBPool>
+  )
+  } else {
   return (
   <GBPool>
   <Miniboss>
@@ -18,6 +35,7 @@ const PaintingRoom: React.FC = () => {
   </Miniboss>
   </GBPool>
   )
+  }
 }
 
 export default PaintingRoom
