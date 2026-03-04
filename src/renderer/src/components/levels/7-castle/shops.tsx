1608ae7a2df6ce5dd7d199fa5f6de6bb81b12b52
A-l-e-x-99 <alexs23743@gmail.com>
2026-03-03 20:10:01 -0500

    It's saying all Castle shops are in the Crypt. Fixing this.

--- src/renderer/src/components/levels/7-castle/shops.tsx
+++ src/renderer/src/components/levels/7-castle/shops.tsx
@@ -12,16 +12,16 @@ const Vanilla: React.FC = () => {
   const hasCandy = useCandy()
   const hasClimbing = useClimbing()
   const hasSnide = useSnide()
-  const inFunkySpot = useDonkStore(useShallow((state) => [state.shuffledCastleCranky.castleCrankyFunky || state.shuffledCastleFunky.castleFunkyNoSwitch || state.shuffledCastleCandy.castleCandyFunky || state.shuffledCastleSnide.castleSnideFunky]))
-  const inCandySpot = useDonkStore(useShallow((state) => [state.shuffledCastleCranky.castleCrankyCandy || state.shuffledCastleFunky.castleFunkyCandy || state.shuffledCastleCandy.castleCandyNoSwitch || state.shuffledCastleSnide.castleSnideCandy]))
+  const crankyInFunkySpot = useDonkStore(useShallow((state) => [state.shuffledCastleCranky.castleCrankyFunky])) //|| state.shuffledCastleFunky.castleFunkyNoSwitch || state.shuffledCastleCandy.castleCandyFunky || state.shuffledCastleSnide.castleSnideFunky]))
+  const crankyInCandySpot = useDonkStore(useShallow((state) => [state.shuffledCastleCranky.castleCrankyCandy])) //|| state.shuffledCastleFunky.castleFunkyCandy || state.shuffledCastleCandy.castleCandyNoSwitch || state.shuffledCastleSnide.castleSnideCandy]))
   return (
     <>
       <ShopGenerator
         baseId={7110}
         baseName="Castle Cranky"
         level="Creepy Castle"
-        region={inFunkySpot ? "Castle Crypt" : inCandySpot ? "Castle Dungeon" : "Creepy Castle Main"}
-        inLogic={hasCranky && (inFunkySpot ? inStage.in && hasClimbing : inStage.in)}
+        region={crankyInFunkySpot ? "Castle Crypt" : crankyInCandySpot ? "Castle Dungeon" : "Creepy Castle Main"}
+        inLogic={hasCranky && (crankyInFunkySpot ? inStage.in && hasClimbing : inStage.in)}
         outLogic={hasCranky && inStage.out}
       />
       <ShopGenerator
