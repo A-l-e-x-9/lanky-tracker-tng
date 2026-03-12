bf7478e25523b174c9190758abc0bd42dcc7f9bc
A-l-e-x-99 <alexs23743@gmail.com>
2026-03-12 09:49:40 -0400

    It said I could do the clams when I couldn't. Fixing.

--- src/renderer/src/hooks/galleon/index.ts
+++ src/renderer/src/hooks/galleon/index.ts
@@ -219,8 +219,8 @@ export const useGalleonTreasureRoom = (): LogicBool => {
   const preOpened = useDonkStore(useShallow((state) => state.removeBarriers.galleonTreasureRoom))
   const warpAll = useBananaportAll()
   return {
-    in: (inStage.in && warpAll) || (outskirts.in && (lanky && dive.in || preOpened) && highTide.in),
-    out: outskirts.out && (lanky && dive.out || preOpened)
+    in: (inStage.in && warpAll) || (outskirts.in && dive.in && (lanky || preOpened) && highTide.in),
+    out: outskirts.out && dive.out && (lanky || preOpened)
   }
 }
 
