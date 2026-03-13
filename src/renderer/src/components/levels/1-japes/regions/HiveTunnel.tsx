ba71aa60da51bcffa139424ed9a5a76d8650df34
A-l-e-x-99 <alexs23743@gmail.com>
2026-03-13 10:55:22 -0400

    Now the Hive Tunnel.

--- src/renderer/src/components/levels/1-japes/regions/HiveTunnel.tsx
+++ src/renderer/src/components/levels/1-japes/regions/HiveTunnel.tsx
@@ -6,12 +6,18 @@ import TinyKasplat from '../kasplats/Tiny'
 
 const HiveTunnelChecks: React.FC = () => {
 const isKremKaptureSeed = useDonkStore(useShallow((state) => state.winCondition.kremlingKapture)) ? 'foolish' : ''
+const [isBlueprintSeed, isKRoolChallengeSeed] = useDonkStore(useShallow((state) => [state.winCondition.blueprints, state.winCondition.kRoolChallenge])) ? 'foolish' : ''
+const kasplatsInRotation = useDonkStore(useShallow((state) => state.settings.poolBlueprints)) ? '' : 'foolish'
 return (
-  <div className={`grid ${isKremKaptureSeed}`}>
+<>
+  <div className={`grid ${isKremKaptureSeed} ${((isBlueprintSeed || isKRoolChallengeSeed) && kasplatsInRotation)}`}>
     <DkKasplat />
     <TinyKasplat />
+  </div>
+  <div className={`grid ${isKremKaptureSeed}`}>
     <HiveTunnelEnemies />
   </div>
+</>
 )
 }
 
