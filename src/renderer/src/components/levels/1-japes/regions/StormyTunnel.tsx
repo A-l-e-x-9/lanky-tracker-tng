49a539975a6a6bdbf88456ee31ab5dc62219da24
A-l-e-x-99 <alexs23743@gmail.com>
2026-03-13 10:50:40 -0400

    ...

--- src/renderer/src/components/levels/1-japes/regions/StormyTunnel.tsx
+++ src/renderer/src/components/levels/1-japes/regions/StormyTunnel.tsx
@@ -7,14 +7,18 @@ import LankyKasplat from '../kasplats/Lanky'
 
 const StormyTunnelChecks: React.FC = () => {
 const isKremKaptureSeed = useDonkStore(useShallow((state) => state.winCondition.kremlingKapture)) ? 'foolish' : ''
+const [isBlueprintSeed, isKRoolChallengeSeed] = useDonkStore(useShallow((state) => [state.winCondition.blueprints, state.winCondition.kRoolChallenge])) ? 'foolish' : ''
+const kasplatsInRotation = useDonkStore(useShallow((state) => state.settings.poolBlueprints)) ? '' : 'foolish'
 return (
   <>
   <div className="grid">
     <SlopeBarrel />
   </div>
-  <div className={`grid ${isKremKaptureSeed}`}>
+  <div className={`grid ${isKremKaptureSeed} ${((isBlueprintSeed || isKRoolChallengeSeed) && kasplatsInRotation)}`}>
     <DiddyKasplat />
     <LankyKasplat />
+  </div>
+  <div className={`grid ${isKremKaptureSeed}`}>  
     <StormyTunnelEnemies />
   </div>
   </>
