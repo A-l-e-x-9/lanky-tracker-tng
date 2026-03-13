7c096eebc6537edd12b454156a3e25ee5892fbd1
A-l-e-x-99 <alexs23743@gmail.com>
2026-03-13 10:49:22 -0400

    ...

--- src/renderer/src/components/levels/1-japes/regions/Underground.tsx
+++ src/renderer/src/components/levels/1-japes/regions/Underground.tsx
@@ -5,12 +5,14 @@ import ChunkyKasplat from '../kasplats/Chunky'
 
 const UndergroundChecks: React.FC = () => {
 const isKremKaptureSeed = useDonkStore(useShallow((state) => state.winCondition.kremlingKapture)) ? 'foolish' : ''
+const [isBlueprintSeed, isKRoolChallengeSeed] = useDonkStore(useShallow((state) => [state.winCondition.blueprints, state.winCondition.kRoolChallenge])) ? 'foolish' : ''
+const kasplatsInRotation = useDonkStore(useShallow((state) => state.settings.poolBlueprints)) ? '' : 'foolish'
 return (
   <>
   <div className="grid">
     <Underground />
   </div>
-  <div className={`grid ${isKremKaptureSeed}`}>
+  <div className={`grid ${isKremKaptureSeed} ${((isBlueprintSeed || isKRoolChallengeSeed) && kasplatsInRotation)}`}>
     <ChunkyKasplat />
   </div>
   </>
