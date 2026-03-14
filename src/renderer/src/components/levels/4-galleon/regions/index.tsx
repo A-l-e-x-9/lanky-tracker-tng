b1815a540c9b684133022ebd4671c2fbaccc5f25
A-l-e-x-99 <alexs23743@gmail.com>
2026-03-14 12:54:43 -0400

    And some more.

--- src/renderer/src/components/levels/4-galleon/regions/index.tsx
+++ src/renderer/src/components/levels/4-galleon/regions/index.tsx
@@ -15,8 +15,15 @@ import ShuffledArenas from '../arenas/Shuffled'
 import ShuffledCrates from '../crates/Shuffled'
 import ShuffledDirt from '../dirt/Shuffled'
 import ShuffledFairies from '../fairies/Shuffled'
+import ShuffledKasplats from '../kasplats/Shuffled'
 
-const GalleonRegionChecks: React.FC = () => (
+const GalleonRegionChecks: React.FC = () => {
+const isKremKaptureSeed = useDonkStore(useShallow((state) => state.winCondition.kremlingKapture)) ? 'foolish' : ''
+const isFairySeed = useDonkStore(useShallow((state) => state.winCondition.fairies)) ? 'foolish' : ''
+const [isBlueprintSeed, isKRoolChallengeSeed] = useDonkStore(useShallow((state) => [state.winCondition.blueprints, state.winCondition.kRoolChallenge])) ? 'foolish' : ''
+const fairiesInRotation = useDonkStore(useShallow((state) => state.settings.poolFairies)) ? '' : 'foolish'
+const kasplatsInRotation = useDonkStore(useShallow((state) => state.settings.poolBlueprints)) ? '' : 'foolish'
+return (
   <>
     <div className="grid">
       <BananaMedalPool>
@@ -38,9 +45,15 @@ const GalleonRegionChecks: React.FC = () => (
       <ShuffledArenas />
       <ShuffledCrates />
       <ShuffledDirt />
+    </div>
+    <div className={`grid ${isFairySeed && fairiesInRotation}`}>    
       <ShuffledFairies />
+    </div>
+    <div className={`grid ${isKremKaptureSeed} ${((isBlueprintSeed || isKRoolChallengeSeed) && kasplatsInRotation)}`}>
+      <ShuffledKasplats />
     </div>
   </>
 )
+}
 
 export default GalleonRegionChecks
