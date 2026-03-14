e3ae3e0ee9dade20343c0a6e9b5e7644ce9464f9
A-l-e-x-99 <alexs23743@gmail.com>
2026-03-14 12:50:11 -0400

    Bringing shuffled Galleon Kasplats to the region view.

--- src/renderer/src/components/levels/4-galleon/regions/Caverns.tsx
+++ src/renderer/src/components/levels/4-galleon/regions/Caverns.tsx
@@ -10,17 +10,27 @@ import TinyKasplat from '../kasplats/Tiny'
 
 const CavernsChecks: React.FC = () => {
 const isKremKaptureSeed = useDonkStore(useShallow((state) => state.winCondition.kremlingKapture)) ? 'foolish' : ''
+const isFairySeed = useDonkStore(useShallow((state) => state.winCondition.fairies)) ? 'foolish' : ''
+const [isBlueprintSeed, isKRoolChallengeSeed] = useDonkStore(useShallow((state) => [state.winCondition.blueprints, state.winCondition.kRoolChallenge])) ? 'foolish' : ''
+const fairiesInRotation = useDonkStore(useShallow((state) => state.settings.poolFairies)) ? '' : 'foolish'
+const kasplatsInRotation = useDonkStore(useShallow((state) => state.settings.poolBlueprints)) ? '' : 'foolish'
 return (
   <>
   <div className="grid">
     <CannonGame />
     <GalleonChest />
+  </div>
+  <div className={`grid ${isFairySeed && fairiesInRotation}`}>
     <ChestFairy />
+  </div>
+  <div className="grid">
     <CavernArena />
   </div>
-  <div className={`grid ${isKremKaptureSeed}`}>
+  <div className={`grid ${isKremKaptureSeed} ${((isBlueprintSeed || isKRoolChallengeSeed) && kasplatsInRotation)}`}>
     <LankyKasplat />
     <TinyKasplat />
+  </div>
+  <div className={`grid ${isKremKaptureSeed}`}>
     <CavernsEnemies />
   </div>
   </>
