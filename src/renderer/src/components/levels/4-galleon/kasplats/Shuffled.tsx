20c8c8b52e87d9c2d6c838852c404eb9cb99dd94
A-l-e-x-99 <alexs23743@gmail.com>
2026-03-14 13:04:15 -0400

    ...

--- src/renderer/src/components/levels/4-galleon/kasplats/Shuffled.tsx
+++ src/renderer/src/components/levels/4-galleon/kasplats/Shuffled.tsx
@@ -1,8 +1,8 @@
 import KasplatPool from '@renderer/components/pools/Kasplats'
-import { useGeneralThing, useProductionTopKasplat, useProductionBaseKasplat, useResearchKasplat, useStorageKasplat, useBlockKasplat, useFactoryHut, useFactoryProductionEnabled, useFactoryProductionTop } from '@renderer/hooks/factory'
+import { useGeneralThing, useProductionTopKasplat, useProductionBaseKasplat, useResearchKasplat, useStorageKasplat, useBlockKasplat, useFactoryHut, useFactoryProductionEnabled, useFactoryProductionTop } from '@renderer/hooks/galleon'
 import { useShuffleKasplats, useBananaportAll } from '@renderer/hooks/settings'
 import { useClimbing, usePunch, useMini, useDk, useTwirl, useCoconut, useTrombone } from '@renderer/hooks/kongs'
-import FactoryCheck from '../check'
+import GalleonCheck from '../check'
 
 const Shuffled: React.FC = () => {
 const DKVanillaKasplat = useProductionTopKasplat()
@@ -24,120 +24,120 @@ const hasTrombone = useTrombone()
 const prodRoomOn = useFactoryProductionEnabled()
   return (
     <KasplatPool>
-      <FactoryCheck
-        id={53000}
+      <GalleonCheck
+        id={54000}
         name="Shuffled Kasplat: DK's Vanilla Location (top of the Prod)"
         region="Prod Room"
         canGetLogic={DKVanillaKasplat.in}
         canGetBreak={DKVanillaKasplat.out}
       />
-      <FactoryCheck
-        id={53001}
+      <GalleonCheck
+        id={54001}
         name="Shuffled Kasplat: Diddy's Vanilla Location (bottom of the Prod)"
         region="Prod Room"
         canGetLogic={diddyVanillaKasplat.in}
         canGetBreak={diddyVanillaKasplat.out}
       />
-      <FactoryCheck
-        id={53002}
+      <GalleonCheck
+        id={54002}
         name="Shuffled Kasplat: Lanky's Vanilla Location (guarding R&D secrets)"
         region="R&D Room"
         canGetLogic={lankyVanillaKasplat.in}
         canGetBreak={lankyVanillaKasplat.out}
       />
-      <FactoryCheck
-        id={53003}
+      <GalleonCheck
+        id={54003}
         name="Shuffled Kasplat: Tiny's Vanilla Location (bottom of the pole to the Arcade)"
         region="Storage and Arcade Area"
         canGetLogic={tinyVanillaKasplat.in}
         canGetBreak={tinyVanillaKasplat.out}
       />
-      <FactoryCheck
-        id={53004}
+      <GalleonCheck
+        id={54004}
         name="Shuffled Kasplat: Chunky's Vanilla Location (at the Block Tower)"
         region="Testing Room"
         canGetLogic={chunkyVanillaKasplat.in}
         canGetBreak={chunkyVanillaKasplat.out}
       />
-      <FactoryCheck
-        id={53005}
+      <GalleonCheck
+        id={54005}
         name="Shuffled Kasplat: At the window in front of Snide's"
         region="Factory Start Area"
         canGetLogic={isBreathing.in}
         canGetBreak={isBreathing.out}
       />
-      <FactoryCheck
-        id={53006}
+      <GalleonCheck
+        id={54006}
         name="Shuffled Kasplat: In front of the Power Hut"
         region="Storage and Arcade Area"
         canGetLogic={canReachHut.in}
         canGetBreak={canReachHut.out}
       />
-      <FactoryCheck
-        id={53007}
+      <GalleonCheck
+        id={54007}
         name="Shuffled Kasplat: Halfway down the hatch"
         region="Prod Room"
         canGetLogic={isBreathing.in && (hasClimbing || hasAllBananaports)}
         canGetBreak={isBreathing.out}
       />
-      <FactoryCheck
-        id={53008}
+      <GalleonCheck
+        id={54008}
         name="Shuffled Kasplat: In the Dark Room"
         region="Storage and Arcade Area"
         canGetLogic={isBreathing.in && hasPrimatePunch}
         canGetBreak={isBreathing.out && hasPrimatePunch}
       />
-      <FactoryCheck
-        id={53009}
+      <GalleonCheck
+        id={54009}
         name="Shuffled Kasplat: A step up in the Prod Room"
         region="Prod Room"
         canGetLogic={prodRoomOn.in || hasAllBananaports}
         canGetBreak={prodRoomOn.out}
       />
-      <FactoryCheck
-        id={53010}
+      <GalleonCheck
+        id={54010}
         name="Shuffled Kasplat: Window shopping"
         region=""
         canGetLogic={isBreathing.in}
         canGetBreak={isBreathing.out}
       />
-      <FactoryCheck
-        id={53011}
+      <GalleonCheck
+        id={54011}
         name="Shuffled Kasplat: Inside the Power Hut"
         region="Storage and Arcade Area"
         canGetLogic={canReachHut.in && hasCoconuts}
         canGetBreak={canReachHut.out && hasCoconuts}
       />
-      <FactoryCheck
-        id={53012}
+      <GalleonCheck
+        id={54012}
         name="Shuffled Kasplat: By the car race entrance"
         region="R&D Room"
         canGetLogic={lankyVanillaKasplat.in}
         canGetBreak={lankyVanillaKasplat.out}
       />
-      <FactoryCheck
-        id={53013}
+      <GalleonCheck
+        id={54013}
         name="Shuffled Kasplat: At Tiny's dartboard"
         region="Testing Room"
         canGetLogic={chunkyVanillaKasplat.in && hasMiniMonkey}
         canGetBreak={chunkyVanillaKasplat.out && hasMiniMonkey}
       />
-      <FactoryCheck
-        id={53014}
+      <GalleonCheck
+        id={54014}
         name="Shuffled Kasplat: In the crusher"
         region="Prod Room"
         canGetLogic={prodRoomOn.in}
         canGetBreak={prodRoomOn.out}
       />
-      <FactoryCheck
-        id={53015}
+      <GalleonCheck
+        id={54015}
         name="Shuffled Kasplat: Past Tiny's Prod Room GB"
         region="Prod Room"
         canGetLogic={canDoTinyProd.in && isHinaKagiyama}
         canGetBreak={canDoTinyProd.out && hasDK}
       />
-      <FactoryCheck
-        id={53016}
+      <GalleonCheck
+        id={54016}
         name="Shuffled Kasplat: Ripping his hair out while playing Lanky's damn piano game"
         region="R&D Room"
         canGetLogic={lankyVanillaKasplat.in && hasTrombone}
