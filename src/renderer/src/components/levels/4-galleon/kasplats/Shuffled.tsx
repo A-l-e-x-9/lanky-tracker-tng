48f4e0d8207db84090b4a7e58a50b07a29955e82
A-l-e-x-99 <alexs23743@gmail.com>
2026-03-14 13:22:24 -0400

    Fixing broke stuff.

--- src/renderer/src/components/levels/4-galleon/kasplats/Shuffled.tsx
+++ src/renderer/src/components/levels/4-galleon/kasplats/Shuffled.tsx
@@ -1,16 +1,19 @@
 import KasplatPool from '@renderer/components/pools/Kasplats'
-import { useGeneralThing, useProductionTopKasplat, useProductionBaseKasplat, useResearchKasplat, useStorageKasplat, useBlockKasplat, useFactoryHut, useFactoryProductionEnabled, useFactoryProductionTop } from '@renderer/hooks/galleon'
+import { useGeneralThing, useDiddyGoldGb, useKevin, useCannonKasplat, useVineKasplat, useCactusKasplat, useFactoryHut, useGalleonLighthousePlatform, useGalleonLighthouseInside, useDiddyMechGb, useFactoryProductionEnabled, useFactoryProductionTop } from '@renderer/hooks/galleon'
 import { useShuffleKasplats, useBananaportAll } from '@renderer/hooks/settings'
 import { useClimbing, usePunch, useMini, useDk, useTwirl, useCoconut, useTrombone } from '@renderer/hooks/kongs'
 import GalleonCheck from '../check'
 
 const Shuffled: React.FC = () => {
-const DKVanillaKasplat = useProductionTopKasplat()
-const diddyVanillaKasplat = useProductionBaseKasplat()
-const lankyVanillaKasplat = useResearchKasplat()
-const tinyVanillaKasplat = useStorageKasplat()
-const chunkyVanillaKasplat = useBlockKasplat()
+const DKVanillaKasplat = useDiddyGoldGb()
+const diddyVanillaKasplat = useKevin()
+const lankyVanillaKasplat = useCannonKasplat()
+const tinyVanillaKasplat = useVineKasplat()
+const chunkyVanillaKasplat = useCactusKasplat()
 const isBreathing = useGeneralThing()
+const canReachLighthouseBase = useGalleonLighthousePlatform()
+const canGoInLighthouse = useGalleonLighthouseInside()
+const mechfishAgenda = useDiddyMechGb()
 const canReachHut = useFactoryHut()
 const canDoTinyProd = useFactoryProductionTop()
 const hasClimbing = useClimbing()
@@ -26,73 +29,73 @@ const prodRoomOn = useFactoryProductionEnabled()
     <KasplatPool>
       <GalleonCheck
         id={54000}
-        name="Shuffled Kasplat: DK's Vanilla Location (top of the Prod)"
-        region="Prod Room"
+        name="Shuffled Kasplat: DK's Vanilla Location (on Diddy's gold pile)"
+        region="Treasure Room"
         canGetLogic={DKVanillaKasplat.in}
         canGetBreak={DKVanillaKasplat.out}
       />
       <GalleonCheck
         id={54001}
-        name="Shuffled Kasplat: Diddy's Vanilla Location (bottom of the Prod)"
-        region="Prod Room"
+        name="Shuffled Kasplat: Diddy's Vanilla Location (Lighthouse alcove)"
+        region="Lighthouse Area"
         canGetLogic={diddyVanillaKasplat.in}
         canGetBreak={diddyVanillaKasplat.out}
       />
       <GalleonCheck
         id={54002}
-        name="Shuffled Kasplat: Lanky's Vanilla Location (guarding R&D secrets)"
-        region="R&D Room"
+        name="Shuffled Kasplat: Lanky's Vanilla Location (the cannon game room)"
+        region="Galleon Caves"
         canGetLogic={lankyVanillaKasplat.in}
         canGetBreak={lankyVanillaKasplat.out}
       />
       <GalleonCheck
         id={54003}
-        name="Shuffled Kasplat: Tiny's Vanilla Location (bottom of the pole to the Arcade)"
-        region="Storage and Arcade Area"
+        name="Shuffled Kasplat: Tiny's Vanilla Location (near vanilla warp 3 in the cave)"
+        region="Galleon Caves"
         canGetLogic={tinyVanillaKasplat.in}
         canGetBreak={tinyVanillaKasplat.out}
       />
       <GalleonCheck
         id={54004}
-        name="Shuffled Kasplat: Chunky's Vanilla Location (at the Block Tower)"
-        region="Testing Room"
+        name="Shuffled Kasplat: Chunky's Vanilla Location (the &quot;cactus&quot;)"
+        region="Shipyard Outskirts"
         canGetLogic={chunkyVanillaKasplat.in}
         canGetBreak={chunkyVanillaKasplat.out}
       />
       <GalleonCheck
         id={54005}
-        name="Shuffled Kasplat: At the window in front of Snide's"
-        region="Factory Start Area"
-        canGetLogic={isBreathing.in}
-        canGetBreak={isBreathing.out}
+        name="Shuffled Kasplat: Base of the Lighthouse"
+        region="Lighthouse Area"
+        canGetLogic={canReachLighthouseBase.in}
+        canGetBreak={canReachLighthouseBase.out}
       />
       <GalleonCheck
         id={54006}
-        name="Shuffled Kasplat: In front of the Power Hut"
-        region="Storage and Arcade Area"
-        canGetLogic={canReachHut.in}
-        canGetBreak={canReachHut.out}
-      />
-      <GalleonCheck
-        id={54007}
-        name="Shuffled Kasplat: Halfway down the hatch"
-        region="Prod Room"
-        canGetLogic={isBreathing.in && (hasClimbing || hasAllBananaports)}
-        canGetBreak={isBreathing.out}
-      />
-      <GalleonCheck
-        id={54008}
-        name="Shuffled Kasplat: In the Dark Room"
-        region="Storage and Arcade Area"
+        name="Shuffled Kasplat: At the vanilla Battle Arena"
+        region="Galleon Caves"
         canGetLogic={isBreathing.in && hasPrimatePunch}
         canGetBreak={isBreathing.out && hasPrimatePunch}
       />
       <GalleonCheck
+        id={54007}
+        name="Shuffled Kasplat: In front of Cranky's"
+        region="Galleon Caves"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
+      />
+      <GalleonCheck
+        id={54008}
+        name="Shuffled Kasplat: Top of the Whomp's Fortress ripoff area"
+        region="Lighthouse Area"
+        canGetLogic={canGoInLighthouse.in && hasClimbing}
+        canGetBreak={canGoInLighthouse.out && hasClimbing}
+      />
+      <GalleonCheck
         id={54009}
-        name="Shuffled Kasplat: A step up in the Prod Room"
-        region="Prod Room"
-        canGetLogic={prodRoomOn.in || hasAllBananaports}
-        canGetBreak={prodRoomOn.out}
+        name="Shuffled Kasplat: Inside the Mechfish"
+        region="Shipyard Outskirts"
+        canGetLogic={mechfishAgenda.in}
+        canGetBreak={mechfishAgenda.out}
       />
       <GalleonCheck
         id={54010}
