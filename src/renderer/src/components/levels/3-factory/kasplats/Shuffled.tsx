10cf9394aaad2f55dfb35fa3dd8540d0b5e4c037
A-l-e-x-99 <alexs23743@gmail.com>
2026-03-13 21:52:31 -0400

    Fixing some of the mistakes.

--- src/renderer/src/components/levels/3-factory/kasplats/Shuffled.tsx
+++ src/renderer/src/components/levels/3-factory/kasplats/Shuffled.tsx
@@ -1,16 +1,17 @@
 import KasplatPool from '@renderer/components/pools/Kasplats'
-import { useGeneralThing, useCoconutKasplat, useOasisKasplat, useLlamaLavaKasplat, useTunnelKasplat, useChunkyKasplat, useDkTunnelGb, useAztecBack, useAztecLlamaTemple, useAztecTinyTemple, useTinyTempleIce, useDk5DoorGb } from '@renderer/hooks/factory'
-import { useShuffleKasplats } from '@renderer/hooks/settings'
-import { useDive, useVine, useClimbing, useRocket, useDiddy, useTiny, usePineapple, useGrape } from '@renderer/hooks/kongs'
+import { useGeneralThing, useProductionTopKasplat, useProductionBaseKasplat, useResearchKasplat, useStorageKasplat, useBlockKasplat, useFactoryHut, useDkTunnelGb, useAztecBack, useAztecLlamaTemple, useAztecTinyTemple, useTinyTempleIce, useDk5DoorGb, useFactoryProductionEnabled } from '@renderer/hooks/factory'
+import { useShuffleKasplats, useBananaportAll } from '@renderer/hooks/settings'
+import { useDive, useVine, useClimbing, useRocket, useDiddy, useTiny, usePineapple, useGrape, usePunch } from '@renderer/hooks/kongs'
 import FactoryCheck from '../check'
 
 const Shuffled: React.FC = () => {
-const DKVanillaKasplat = useCoconutKasplat()
-const diddyVanillaKasplat = useOasisKasplat()
-const lankyVanillaKasplat = useLlamaLavaKasplat()
-const tinyVanillaKasplat = useTunnelKasplat()
-const chunkyVanillaKasplat = useChunkyKasplat()
+const DKVanillaKasplat = useProductionTopKasplat()
+const diddyVanillaKasplat = useProductionBaseKasplat()
+const lankyVanillaKasplat = useResearchKasplat()
+const tinyVanillaKasplat = useStorageKasplat()
+const chunkyVanillaKasplat = useBlockKasplat()
 const isBreathing = useGeneralThing()
+const canReachHut = useFactoryHut()
 const canDoQSGB = useDkTunnelGb()
 const canEnterBack = useAztecBack()
 const canGoInLlamaTemple = useAztecLlamaTemple()
@@ -25,76 +26,79 @@ const hasDiddy = useDiddy()
 const hasTiny = useTiny()
 const hasPineapples = usePineapple()
 const hasGrapes = useGrape()
+const hasAllBananaports = useBananaportAll()
+const hasPrimatePunch = usePunch()
+const prodRoomOn = useFactoryProductionEnabled()
   return (
     <KasplatPool>
       <FactoryCheck
         id={53000}
-        name="Shuffled Kasplat: DK's Vanilla Location (a sub-tunnel at the level start)"
-        region="Aztec Caves"
+        name="Shuffled Kasplat: DK's Vanilla Location (top of the Prod)"
+        region="Prod Room"
         canGetLogic={DKVanillaKasplat.in}
         canGetBreak={DKVanillaKasplat.out}
       />
       <FactoryCheck
         id={53001}
-        name="Shuffled Kasplat: Diddy's Vanilla Location (on top of the Tiny Temple)"
-        region="Aztec Main Area"
+        name="Shuffled Kasplat: Diddy's Vanilla Location (bottom of the Prod)"
+        region="Prod Room"
         canGetLogic={diddyVanillaKasplat.in}
         canGetBreak={diddyVanillaKasplat.out}
       />
       <FactoryCheck
         id={53002}
-        name="Shuffled Kasplat: Lanky's Vanilla Location (the lava pedestal GB)"
-        region="Llama Temple"
+        name="Shuffled Kasplat: Lanky's Vanilla Location (guarding R&D secrets)"
+        region="R&D Room"
         canGetLogic={lankyVanillaKasplat.in}
         canGetBreak={lankyVanillaKasplat.out}
       />
       <FactoryCheck
         id={53003}
-        name="Shuffled Kasplat: Tiny's Vanilla Location (first 3-way intersection of middle tunnel)"
-        region="Aztec Caves"
+        name="Shuffled Kasplat: Tiny's Vanilla Location (bottom of the pole to the Arcade)"
+        region="Storage and Arcade Area"
         canGetLogic={tinyVanillaKasplat.in}
         canGetBreak={tinyVanillaKasplat.out}
       />
       <FactoryCheck
         id={53004}
-        name="Shuffled Kasplat: Chunky's Vanilla Location (in his 5DT room)"
-        region="5 Door Temple"
+        name="Shuffled Kasplat: Chunky's Vanilla Location (at the Block Tower)"
+        region="Testing Room"
         canGetLogic={chunkyVanillaKasplat.in}
         canGetBreak={chunkyVanillaKasplat.out}
       />
       <FactoryCheck
         id={53005}
-        name="Shuffled Kasplat: In DK's quicksand tunnel"
-        region="Aztec Caves"
-        canGetLogic={canDoQSGB.in}
-        canGetBreak={canDoQSGB.out}
-      />
-      <FactoryCheck
-        id={53006}
-        name="Shuffled Kasplat: On the oasis"
-        region="Aztec Main Area"
+        name="Shuffled Kasplat: At the window in front of Snide's"
+        region="Factory Start Area"
         canGetLogic={isBreathing.in}
         canGetBreak={isBreathing.out}
       />
       <FactoryCheck
+        id={53006}
+        name="Shuffled Kasplat: In front of the Power Hut"
+        region="Storage and Arcade Area"
+        canGetLogic={canReachHut.in}
+        canGetBreak={canReachHut.out}
+      />
+      <FactoryCheck
         id={53007}
-        name="Shuffled Kasplat: On top of the Llama's cage"
-        region="Aztec Main Area"
-        canGetLogic={isBreathing.in && hasClimbing && (hasVines || hasJetbarrel)}
-        canGetBreak={isBreathing.out && (hasDiddy || hasTiny)}
+        name="Shuffled Kasplat: Halfway down the hatch"
+        region="Prod Room"
+        canGetLogic={isBreathing.in && (hasClimbing || hasAllBananaports)}
+        canGetBreak={isBreathing.out}
       />
       <FactoryCheck
         id={53008}
-        name="Shuffled Kasplat: Near the giant boulder"
-        region="Aztec Caves"
-        canGetLogic={canEnterBack.in}
-        canGetBreak={canEnterBack.out}
+        name="Shuffled Kasplat: In the Dark Room"
+        region="Storage and Arcade Area"
+        canGetLogic={isBreathing.in && hasPrimatePunch}
+        canGetBreak={isBreathing.out && hasPrimatePunch}
       />
       <FactoryCheck
         id={53009}
-        name="Shuffled Kasplat: In the vase room"
-        region="Aztec Caves"
-        canGetLogic={isBreathing.in && hasPineapples}
+        name="Shuffled Kasplat: A step up in the Prod Room"
+        region="Prod Room"
+        canGetLogic={prodRoomOn || hasAllBananaports}
         canGetBreak={isBreathing.out && hasPineapples}
       />
       <FactoryCheck
