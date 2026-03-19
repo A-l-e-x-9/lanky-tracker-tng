38150582b593d35938b71b8bfd16941e71074185
A-l-e-x-99 <alexs23743@gmail.com>
2026-03-19 01:46:52 -0400

    Now starting to implement shuffled Castle Kasplats.

--- src/renderer/src/components/levels/7-castle/kasplats/Shuffled.tsx
+++ src/renderer/src/components/levels/7-castle/kasplats/Shuffled.tsx
@@ -1,8 +1,8 @@
 import KasplatPool from '@renderer/components/pools/Kasplats'
-import { useGeneralThing, useIceCastleKasplat, useFunkyKasplat, usePillarKasplat, useCabinKasplat, useIglooKasplat, useCanAccessSnide, useTinyCaveGb, useChunkyClearGb, useSlamCaves, useCavesIgloo } from '@renderer/hooks/caves'
+import { useGeneralThing, useIceCastleKasplat, useFunkyKasplat, usePillarKasplat, useCabinKasplat, useIglooKasplat, useSlamCastle } from '@renderer/hooks/castle'
 import { useShuffleKasplats } from '@renderer/hooks/settings'
 import { useAnyGun, useAnyMusic, useDive, useBalloon, useHighGrab, useMonkeyport, useGuitar, useBongos } from '@renderer/hooks/kongs'
-import CavesCheck from '../check'
+import CastleCheck from '../check'
 
 const Shuffled: React.FC = () => {
 const DKVanillaKasplat = useIceCastleKasplat()
@@ -26,140 +26,140 @@ const hasGuitar = useGuitar()
 const hasBongoes = useBongos()
   return (
     <KasplatPool>
-      <CavesCheck
-        id={56000}
+      <CastleCheck
+        id={57000}
         name="Shuffled Kasplat: DK's Vanilla Location (behind the Ice Castle)"
         region="Crystal Caves Main"
         canGetLogic={DKVanillaKasplat.in}
         canGetBreak={DKVanillaKasplat.out}
       />
-      <CavesCheck
-        id={56001}
+      <CastleCheck
+        id={57001}
         name="Shuffled Kasplat: Diddy's Vanilla Location (in Tiny's room near Funky's)"
         region="Crystal Caves Main"
         canGetLogic={diddyVanillaKasplat.in}
         canGetBreak={diddyVanillaKasplat.out}
       />
-      <CavesCheck
-        id={56002}
+      <CastleCheck
+        id={57002}
         name="Shuffled Kasplat: Lanky's Vanilla Location (a high platform near Funky's)"
         region="Crystal Caves Main"
         canGetLogic={lankyVanillaKasplat.in}
         canGetBreak={lankyVanillaKasplat.out}
       />
-      <CavesCheck
-        id={56003}
+      <CastleCheck
+        id={57003}
         name="Shuffled Kasplat: Tiny's Vanilla Location (a high platform near Candy's)"
         region="Caves Cabins"
         canGetLogic={tinyVanillaKasplat.in}
         canGetBreak={tinyVanillaKasplat.out}
       />
-      <CavesCheck
-        id={56004}
+      <CastleCheck
+        id={57004}
         name="Shuffled Kasplat: Chunky's Vanilla Location (top of the 5DI)"
         region="Caves Igloo"
         canGetLogic={chunkyVanillaKasplat.in}
         canGetBreak={chunkyVanillaKasplat.out}
       />
-      <CavesCheck
-        id={56005}
+      <CastleCheck
+        id={57005}
         name="Shuffled Kasplat: Why doesn't Snide kill this one himself?"
         region="Crystal Caves Main"
         canGetLogic={isBreathing.in && canReachSnide}
         canGetBreak={isBreathing.out && canReachSnide}
       />
-      <CavesCheck
-        id={56006}
+      <CastleCheck
+        id={57006}
         name="Shuffled Kasplat: Stuck in Tiny's cave near the 5DI"
         region="Crystal Caves Main"
         canGetLogic={canDoTinyMiniGB.in}
         canGetBreak={canDoTinyMiniGB.out}
       />
-      <CavesCheck
-        id={56007}
+      <CastleCheck
+        id={57007}
         name="Shuffled Kasplat: Guarding the Monkeyport pad to the giant Kosha"
         region="Caves Igloo"
         canGetLogic={canDoGiantKoshaGB.in}
         canGetBreak={canDoGiantKoshaGB.out}
       />
-      <CavesCheck
-        id={56008}
+      <CastleCheck
+        id={57008}
         name="Shuffled Kasplat: At warp 5, 5DC-side"
         region="Caves Cabins"
         canGetLogic={isBreathing.in}
         canGetBreak={isBreathing.out}
       />
-      <CavesCheck
-        id={56009}
+      <CastleCheck
+        id={57009}
         name="Shuffled Kasplat: Across the river from Candy's"
         region="Caves Cabins"
         canGetLogic={isBreathing.in}
         canGetBreak={isBreathing.out}
       />
-      <CavesCheck
-        id={56010}
+      <CastleCheck
+        id={57010}
         name="Shuffled Kasplat: In the big rock room"
         region="Crystal Caves Main"
         canGetLogic={isBreathing.in && canReachSnide}
         canGetBreak={isBreathing.out && canReachSnide}
       />
-      <CavesCheck
-        id={56011}
+      <CastleCheck
+        id={57011}
         name="Shuffled Kasplat: In the water, under DK's Blast pad"
         region="Crystal Caves Main"
         canGetLogic={isBreathing.in && hasDiving.in && hasAnInstrument}
         canGetBreak={isBreathing.out && hasDiving.out}
       />
-      <CavesCheck
-        id={56012}
+      <CastleCheck
+        id={57012}
         name="Shuffled Kasplat: In the water, anchored to vanilla warp 4 near Cranky's"
         region="Crystal Caves Main"
         canGetLogic={isBreathing.in && hasDiving.in && (hasAGun || hasAnInstrument)}
         canGetBreak={isBreathing.out && hasDiving.out}
       />
-      <CavesCheck
-        id={56013}
+      <CastleCheck
+        id={57013}
         name="Shuffled Kasplat: In between Funky's and the Ice Castle"
         region="Crystal Caves Main"
         canGetLogic={isBreathing.in}
         canGetBreak={isBreathing.out}
       />
-      <CavesCheck
-        id={56014}
+      <CastleCheck
+        id={57014}
         name="Shuffled Kasplat: In Lanky's beetle race! D:"
         region="Crystal Caves Main"
         canGetBreak={(isBreathing.in || isBreathing.out) && hasSlam && (hasBalloon || canHighGrab)}
       />
-      <CavesCheck
-        id={56015}
+      <CastleCheck
+        id={57015}
         name="Shuffled Kasplat: Guarding the giant Kosha"
         region="Caves Igloo"
         canGetLogic={canDoGiantKoshaGB.in && hasMonkeyport}
         canGetBreak={canDoGiantKoshaGB.out && hasMonkeyport}
       />
-      <CavesCheck
-        id={56016}
+      <CastleCheck
+        id={57016}
         name="Shuffled Kasplat: In Diddy's 5DI room"
         region="Caves Igloo"
         canGetLogic={is5DIOpen.in && hasGuitar}
         canGetBreak={is5DIOpen.out && hasGuitar}
       />
-      <CavesCheck
-        id={56017}
+      <CastleCheck
+        id={57017}
         name="Shuffled Kasplat: In DK's 5DC room"
         region="Caves Cabins"
         canGetLogic={isBreathing.in && hasBongoes}
         canGetBreak={isBreathing.out && hasBongoes}
       />
-      <CavesCheck
-        id={56018}
+      <CastleCheck
+        id={57018}
         name="Shuffled Kasplat: In Chunky's cave at the start"
         region="Crystal Caves Main"
         canGetLogic={isBreathing.in && canReachSnide}
         canGetBreak={isBreathing.out && canReachSnide}
       />
-      <CavesCheck
-        id={56019}
+      <CastleCheck
+        id={57019}
         name="Shuffled Kasplat: At the vanilla level entrance"
         region="Forest Area 1"
         canGetLogic={isBreathing.in}
