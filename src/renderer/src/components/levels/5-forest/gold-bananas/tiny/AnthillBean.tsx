c23f4d552f376164946d62d8c7f1a5e4d15c8481
A-l-e-x-99 <alexs23743@gmail.com>
2026-03-25 20:31:25 -0400

    Perhaps this, maybe?

--- src/renderer/src/components/levels/5-forest/gold-bananas/tiny/AnthillBean.tsx
+++ src/renderer/src/components/levels/5-forest/gold-bananas/tiny/AnthillBean.tsx
@@ -1,11 +1,25 @@
 import MiscPool, { BeanPool } from '@renderer/components/pools/Misc'
+import { useBean } from '@renderer/hooks/settings'
 import { useTinyAntGb } from '@renderer/hooks/forest'
 import ForestCheck from '../../check'
 
 const AnthillBean: React.FC = () => {
 const canDo = useTinyAntGb()
+const beanWinCon = useBean()
+if (beanWinCon) {
 return (
-<BeanPool>
+  <BeanPool>
+    <ForestCheck
+      id={5034}
+      name="THE BEAN (Tree Stump Gauntlet's Second Reward)"
+      region="Forest Area 4"
+      canGetLogic={canDo.in}
+      canGetBreak={canDo.out}
+    />
+  </BeanPool>
+)
+} else {
+return (
   <MiscPool>
     <ForestCheck
       id={5034}
@@ -15,8 +29,8 @@ return (
       canGetBreak={canDo.out}
     />
   </MiscPool>
-  </BeanPool>
 )
+}
 }
 
 export default AnthillBean
