8315ae13ccf2763ccc935a9bc5f11600c87da471
A-l-e-x-99 <alexs23743@gmail.com>
2026-03-27 01:12:03 -0400

    More checking of checks

--- src/renderer/src/hooks/japes/index.ts
+++ src/renderer/src/hooks/japes/index.ts
@@ -135,8 +135,8 @@ export const useJapesMine = (): LogicBool => {
   const DKPortal = usePortalNearDiddy()
   const DKPortal2 = useDiddyMinePortal()
   return {
-    in: peanut && (hasClimbing || hasBananaports || DKPortal) && canPlay.in,
-    out: (peanut || DKPortal2) && canPlay.out //DKPortal2 is logic-breaking because if you don't have Peanuts, you can't get back in
+    in: ((peanut && (hasClimbing || hasBananaports || DKPortal)) || DKPortal2) && canPlay.in,
+    out: ((peanut && (hasClimbing || hasBananaports || DKPortal)) || DKPortal2) && canPlay.out
   }
 }
 
