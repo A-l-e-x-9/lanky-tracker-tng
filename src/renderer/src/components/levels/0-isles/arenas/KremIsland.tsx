c385ef2da700c09cb57dd98552231103fbfc2f85
A-l-e-x-99 <alexs23743@gmail.com>
2026-04-04 00:51:27 -0400

    Here comes one more.

--- src/renderer/src/components/levels/0-isles/arenas/KremIsland.tsx
+++ src/renderer/src/components/levels/0-isles/arenas/KremIsland.tsx
@@ -8,6 +8,7 @@ const KremIslandArenas: React.FC = () => {
   const canDoIslesArena1 = useSnideArena()
   const canReachFactoryLobby = useIslesKremAscent()
   const canReachHelmLobby = useIslesKremTop()
+  const hasAllEightKeys = key1 && key2 && key3 && key4 && key5 && key6 && key7 && key8
     return (
     <ArenaPool>
       <IslesCheck
--- src/renderer/src/components/levels/0-isles/dirt/KremIsland.tsx
+++ src/renderer/src/components/levels/0-isles/dirt/KremIsland.tsx
@@ -7,6 +7,7 @@ const isBreathing = useGeneralDirt()
 const canReachFactoryLobby = useIslesKremAscent()
 const canDoIslesArena1 = canReachFactoryLobby && hasShockwave
 const canReachHelmLobby = useIslesKremTop()
+const hasAllEightKeys = key1 && key2 && key3 && key4 && key5 && key6 && key7 && key8
   return (
     <RainbowCoinPool>
       <IslesCheck
