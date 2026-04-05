8919c23a5f17b57208f4459fc8505c6c3cc08f74
A-l-e-x-99 <alexs23743@gmail.com>
2026-04-05 15:00:32 -0400

    Here comes some more.

---
+++ src/renderer/src/components/levels/0-isles/arenas/JapesForest.tsx
@@ -0,0 +1,23 @@
+import ArenaPool from '@renderer/components/pools/Arenas'
+import { useShuffledArenas } from '@renderer/hooks/settings'
+import IslesCheck from '../check'
+import JapesLobbyArenas from './JapesLobby'
+import AztecLobbyArenas from './AztecLobby'
+import FactoryLobbyArenas from './FactoryLobby'
+import GalleonLobbyArenas from './GalleonLobby'
+import ForestLobbyArenas from './ForestLobby'
+
+const Shuffled: React.FC = () => {
+  return (
+    <ArenaPool>
+      <JapesLobbyArenas />
+      <AztecLobbyArenas />
+      <FactoryLobbyArenas />
+      <GalleonLobbyArenas />
+      <ForestLobbyArenas />
+    </ArenaPool>
+  )
+}
+
+const JapesForestArenas: React.FC = () => (useShuffledArenas() ? <Shuffled /> : null)
+export default JapesForestArenas
