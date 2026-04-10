ed96005e68decfcefb5fff1ede04bb4f16cf7d3f
A-l-e-x-99 <alexs23743@gmail.com>
2026-04-10 09:26:07 -0400

    Here come the Hillside Battle Arenas.

---
+++ src/renderer/src/components/levels/1-japes/arenas/Hillside.tsx
@@ -0,0 +1,112 @@
+import ArenaPool from '@renderer/components/pools/Arenas'
+import { useShuffledArenas } from '@renderer/hooks/settings'
+import JapesCheck from '../check'
+import {  } from '@renderer/hooks/japes'
+import {  } from '@renderer/hooks/kongs'
+
+const Shuffled: React.FC = () => {
+  return (
+    <ArenaPool>
+      <JapesCheck
+        id={11000}
+        name="Shuffled Battle Arena: Vanilla Arena (what a Funky's!)"
+        region="Japes Hillside"
+        canGetLogic={canReachFunky.in}
+        canGetBreak={canReachFunky.out}
+      />
+      <JapesCheck
+        id={11000}
+        name="Shuffled Battle Arena: In front of door to Lanky's painting room"
+        region="Japes Hillside"
+        canGetLogic={canReachPaintingRoom.in}
+        canGetBreak={canReachPaintingRoom.out}
+      />
+      <JapesCheck
+        id={11000}
+        name="Shuffled Battle Arena: In front of the Painting Hill"
+        region="Japes Hillside"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
+      />
+      <JapesCheck
+        id={11000}
+        name="Shuffled Battle Arena: At the minecart exit"
+        region="Japes Hillside"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
+      />
+      <JapesCheck
+        id={11000}
+        name="Shuffled Battle Arena: Near Snide's"
+        region="Japes Hillside"
+        canGetLogic={canReachFunky.in}
+        canGetBreak={canReachFunky.out}
+      />
+      <JapesCheck
+        id={11000}
+        name="Shuffled Battle Arena: Behind Diddy's Mountain"
+        region="Japes Hillside"
+        canGetLogic={canDoVanillaCrate1.in}
+        canGetBreak={canDoVanillaCrate1.out}
+      />
+      <JapesCheck
+        id={11000}
+        name="Shuffled Battle Arena: In front of Diddy's cage"
+        region="Japes Hillside"
+        canGetLogic={canReachDiddyCage.in}
+        canGetBreak={canReachDiddyCage.out}
+      />
+      <JapesCheck
+        id={11000}
+        name="Shuffled Battle Arena: Near the Barrel Cannon to Diddy's cage"
+        region="Japes Hillside"
+        canGetLogic={canReachDiddyCage.in}
+        canGetBreak={canReachDiddyCage.out}
+      />
+      <JapesCheck
+        id={11000}
+        name="Shuffled Battle Arena: Behind Diddy's Mountain"
+        region="Japes Hillside"
+        canGetLogic={canReachMtn.in}
+        canGetBreak={canReachMtn.out}
+      />
+      <JapesCheck
+        id={11000}
+        name="Shuffled Battle Arena: Near the cannon platform"
+        region="Japes Hillside"
+        canGetLogic={canReachDiddyCage.in}
+        canGetBreak={canReachDiddyCage.out}
+      />
+      <JapesCheck
+        id={11000}
+        name="Shuffled Battle Arena: In the Troff 'n' Scoff alcove"
+        region="Japes Hillside"
+        canGetLogic={isBreathing.in && hasClimbing && hasVines}
+        canGetBreak={isBreathing.out && canReachDiddyCage.out}
+      />
+      <JapesCheck
+        id={11000}
+        name="Shuffled Battle Arena: Vanilla Warp 2, in front of Diddy's mountain"
+        region="Japes Hillside"
+        canGetLogic={canReachMtn.in}
+        canGetBreak={canReachMtn.out}
+      />
+      <JapesCheck
+        id={11000}
+        name="Shuffled Battle Arena: Vanilla Warp 3, at the painting room"
+        region="Japes Hillside"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
+      />
+      <JapesCheck
+        id={11000}
+        name="Shuffled Battle Arena: Vanilla Warp 5, on top of Diddy's Mountain"
+        region="Japes Hillside"
+        canGetLogic={didGB[1011] || hasAllBananaports}
+      />
+    </ArenaPool>
+  )
+}
+
+const HillsideArenas: React.FC = () => (useShuffledArenas() ? <Shuffled /> : null)
+export default HillsideArenas
--- src/renderer/src/components/levels/1-japes/arenas/Shuffled.tsx
+++ src/renderer/src/components/levels/1-japes/arenas/Shuffled.tsx
@@ -31,27 +31,6 @@ const canGetInHivePastRoom1 = useTinyHiveGb()
       <LowlandArenas />
       <HillsideArenas />
       <JapesCheck
-        id={11000}
-        name="Shuffled Battle Arena: Vanilla Arena (what a Funky's!)"
-        region="Japes Hillside"
-        canGetLogic={canReachFunky.in}
-        canGetBreak={canReachFunky.out}
-      />
-      <JapesCheck
-        id={11001}
-        name="Shuffled Battle Arena: In front of door to Lanky's painting room"
-        region="Japes Hillside"
-        canGetLogic={canReachPaintingRoom.in}
-        canGetBreak={canReachPaintingRoom.out}
-      />
-      <JapesCheck
-        id={11008}
-        name="Shuffled Battle Arena: In front of the Painting Hill"
-        region="Japes Hillside"
-        canGetLogic={isBreathing.in}
-        canGetBreak={isBreathing.out}
-      />
-      <JapesCheck
         id={11009}
         name="Shuffled Battle Arena: Under Chunky's barrel in the hive zone"
         region="Hive Area"
@@ -143,76 +122,6 @@ const canGetInHivePastRoom1 = useTinyHiveGb()
         canGetBreak={canReachStormyZone.out}
       />
       <JapesCheck
-        id={11023}
-        name="Shuffled Battle Arena: At the minecart exit"
-        region="Japes Hillside"
-        canGetLogic={isBreathing.in}
-        canGetBreak={isBreathing.out}
-      />
-      <JapesCheck
-        id={11024}
-        name="Shuffled Battle Arena: Near Snide's"
-        region="Japes Hillside"
-        canGetLogic={canReachFunky.in}
-        canGetBreak={canReachFunky.out}
-      />
-      <JapesCheck
-        id={11025}
-        name="Shuffled Battle Arena: Behind Diddy's Mountain"
-        region="Japes Hillside"
-        canGetLogic={canDoVanillaCrate1.in}
-        canGetBreak={canDoVanillaCrate1.out}
-      />
-      <JapesCheck
-        id={11026}
-        name="Shuffled Battle Arena: In front of Diddy's cage"
-        region="Japes Hillside"
-        canGetLogic={canReachDiddyCage.in}
-        canGetBreak={canReachDiddyCage.out}
-      />
-      <JapesCheck
-        id={11027}
-        name="Shuffled Battle Arena: Near the Barrel Cannon to Diddy's cage"
-        region="Japes Hillside"
-        canGetLogic={canReachDiddyCage.in}
-        canGetBreak={canReachDiddyCage.out}
-      />
-      <JapesCheck
-        id={11028}
-        name="Shuffled Battle Arena: Behind Diddy's Mountain"
-        region="Japes Hillside"
-        canGetLogic={canReachMtn.in}
-        canGetBreak={canReachMtn.out}
-      />
-      <JapesCheck
-        id={11029}
-        name="Shuffled Battle Arena: Near the cannon platform"
-        region="Japes Hillside"
-        canGetLogic={canReachDiddyCage.in}
-        canGetBreak={canReachDiddyCage.out}
-      />
-      <JapesCheck
-        id={11030}
-        name="Shuffled Battle Arena: In the Troff 'n' Scoff alcove"
-        region="Japes Hillside"
-        canGetLogic={isBreathing.in && hasClimbing && hasVines}
-        canGetBreak={isBreathing.out && canReachDiddyCage.out}
-      />
-      <JapesCheck
-        id={11035}
-        name="Shuffled Battle Arena: Vanilla Warp 2, in front of Diddy's mountain"
-        region="Japes Hillside"
-        canGetLogic={canReachMtn.in}
-        canGetBreak={canReachMtn.out}
-      />
-      <JapesCheck
-        id={11037}
-        name="Shuffled Battle Arena: Vanilla Warp 3, at the painting room"
-        region="Japes Hillside"
-        canGetLogic={isBreathing.in}
-        canGetBreak={isBreathing.out}
-      />
-      <JapesCheck
         id={11038}
         name="Shuffled Battle Arena: Vanilla Warp 4, Stormy Tunnel entrance"
         region="Stormy Area"
@@ -225,12 +134,6 @@ const canGetInHivePastRoom1 = useTinyHiveGb()
         region="Stormy Area"
         canGetLogic={canReachStormyZone.in}
         canGetBreak={canReachStormyZone.out}
-      />
-      <JapesCheck
-        id={11040}
-        name="Shuffled Battle Arena: Vanilla Warp 5, on top of Diddy's Mountain"
-        region="Japes Hillside"
-        canGetLogic={didGB[1011] || hasAllBananaports}
       />
       <JapesCheck
         id={11041}
