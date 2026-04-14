fcbb3f33f6ab83896914aeddf5254c6f93b7cd76
A-l-e-x-99 <alexs23743@gmail.com>
2026-04-14 19:49:38 -0400

    It just wouldn't be me without some glitch to fix...

--- src/renderer/src/components/levels/1-japes/kasplats/Shuffled.tsx
+++ src/renderer/src/components/levels/1-japes/kasplats/Shuffled.tsx
@@ -1,5 +1,5 @@
 import KasplatPool from '@renderer/components/pools/Kasplats'
-import { useGateKasplat, useJapesRambi, useJapesMine, useJapesHive, useJapesKongGates } from '@renderer/hooks/japes'
+import { useGateKasplat, useJapesRambi, useJapesHive, useJapesKongGates } from '@renderer/hooks/japes'
 import { useDive, useAnyGun, useOrange, useAnyMusic, useMini } from '@renderer/hooks/kongs'
 import JapesCheck from '../check'
 import LowlandKasplats from './Lowlands'
@@ -9,7 +9,6 @@ import MineKasplats from './Mine'
 const ShuffledKasplats: React.FC = () => {
 const notChunkyKasplat = useGateKasplat()
 const canReachRambiArea = useJapesRambi()
-const canDoDiddyMtnGB = useJapesMine()
 const canReachHiveArea = useJapesHive()
 const canReachStormyArea = useJapesKongGates()
 const hasDiving = useDive()
