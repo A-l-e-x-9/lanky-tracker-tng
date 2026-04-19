d7062b1834ecb29452b01fbab6f8ae810c6f3b3d
A-l-e-x-99 <alexs23743@gmail.com>
2026-04-19 15:48:53 -0400

    Here come the shuffled dirt patches.

---
+++ src/renderer/src/components/levels/2-aztec/dirt/Cave.tsx
@@ -0,0 +1,93 @@
+import RainbowCoinPool from '@renderer/components/pools/RainbowCoins'
+import { useShuffleDirt } from '@renderer/hooks/settings'
+import AztecCheck from '../check'
+import {  } from '@renderer/hooks/japes'
+import { useShockwave } from '@renderer/hooks/kongs'
+
+const Shuffled: React.FC = () => {
+const hasShockwave = useShockwave()
+  return (
+    <RainbowCoinPool>
+      <AztecCheck
+        id={32008}
+        name="Shuffled Dirt Patch: In front of DK's room in the first tunnel"
+        region="Aztec Caves"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
+      />
+      <AztecCheck
+        id={32009}
+        name="Shuffled Dirt Patch: In DK's room in the first tunnel"
+        region="Aztec Caves"
+        canGetLogic={canReachDKKasplat.in && hasShockwave}
+        canGetBreak={canReachDKKasplat.out && hasShockwave}
+      />
+      <AztecCheck
+        id={32017}
+        name="Shuffled Dirt Patch: Near Chunky's barrel"
+        region="Aztec Caves"
+        canGetLogic={canReachArea2.in && hasShockwave}
+        canGetBreak={canReachArea2.out && hasShockwave}
+      />
+      <AztecCheck
+        id={32018}
+        name="Shuffled Dirt Patch: Left of the caged GB"
+        region="Aztec Caves"
+        canGetLogic={canReachArea2.in && hasShockwave}
+        canGetBreak={canReachArea2.out && hasShockwave}
+      />
+      <AztecCheck
+        id={32019}
+        name="Shuffled Dirt Patch: Right of the caged GB"
+        region="Aztec Caves"
+        canGetLogic={canReachArea2.in && hasShockwave}
+        canGetBreak={canReachArea2.out && hasShockwave}
+      />
+      <AztecCheck
+        id={32020}
+        name="Shuffled Dirt Patch: Behind the caged GB"
+        region="Aztec Caves"
+        canGetLogic={canReachArea2.in && hasShockwave}
+        canGetBreak={canReachArea2.out && hasShockwave}
+      />
+      <AztecCheck
+        id={32035}
+        name="Shuffled Dirt Patch: Inside DK's quicksand tunnel"
+        region="Aztec Caves"
+        canGetLogic={canReachQSTunnel.in && hasShockwave}
+        canGetBreak={canReachQSTunnel.out && hasShockwave}
+      />
+      <AztecCheck
+        id={32036}
+        name="Shuffled Dirt Patch: Vanilla Warp 1, level start"
+        region="Aztec Caves"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
+      />
+      <AztecCheck
+        id={32039}
+        name="Shuffled Dirt Patch: Vanilla Warp 2, end of the middle tunnel"
+        region="Aztec Caves"
+        canGetLogic={canReachArea2.in && hasShockwave}
+        canGetBreak={canReachArea2.out && hasShockwave}
+      />
+      <AztecCheck
+        id={32040}
+        name="Shuffled Dirt Patch: Vanilla Warp 3, at Cranky's"
+        region="Aztec Caves"
+        canGetLogic={canReachArea2.in && hasShockwave}
+        canGetBreak={canReachArea2.out && hasShockwave}
+      />
+      <AztecCheck
+        id={32045}
+        name="Shuffled Dirt Patch: Vanilla Warp 5, at DK's quicksand tunnel"
+        region="Aztec Caves"
+        canGetLogic={(didGB[2002] || hasAllBananaports) && isBreathing.in}
+        canGetBreak={(didGB[2002] || hasAllBananaports) && isBreathing.out}
+      />
+    </RainbowCoinPool>
+  )
+}
+
+const CaveDirt: React.FC = () => (useShuffleDirt() ? <Shuffled /> : null)
+export default CaveDirt
--- src/renderer/src/components/levels/2-aztec/dirt/Shuffled.tsx
+++ src/renderer/src/components/levels/2-aztec/dirt/Shuffled.tsx
@@ -94,20 +94,6 @@ const canReachLavaRoom = useAztecLlamaLava()
         canGetBreak={canReachTinyTemple.out && iceMelted.out && hasDiving.out && hasShockwave}
       />
       <AztecCheck
-        id={32008}
-        name="Shuffled Dirt Patch: In front of DK's room in the first tunnel"
-        region="Aztec Caves"
-        canGetLogic={isBreathing.in}
-        canGetBreak={isBreathing.out}
-      />
-      <AztecCheck
-        id={32009}
-        name="Shuffled Dirt Patch: In DK's room in the first tunnel"
-        region="Aztec Caves"
-        canGetLogic={canReachDKKasplat.in && hasShockwave}
-        canGetBreak={canReachDKKasplat.out && hasShockwave}
-      />
-      <AztecCheck
         id={32010}
         name="Shuffled Dirt Patch: Near oasis sand"
         region="Aztec Main Area"
@@ -155,34 +141,6 @@ const canReachLavaRoom = useAztecLlamaLava()
         region="Aztec Main Area"
         canGetLogic={isBreathing.in}
         canGetBreak={isBreathing.out}
-      />
-      <AztecCheck
-        id={32017}
-        name="Shuffled Dirt Patch: Near Chunky's barrel"
-        region="Aztec Caves"
-        canGetLogic={canReachArea2.in && hasShockwave}
-        canGetBreak={canReachArea2.out && hasShockwave}
-      />
-      <AztecCheck
-        id={32018}
-        name="Shuffled Dirt Patch: Left of the caged GB"
-        region="Aztec Caves"
-        canGetLogic={canReachArea2.in && hasShockwave}
-        canGetBreak={canReachArea2.out && hasShockwave}
-      />
-      <AztecCheck
-        id={32019}
-        name="Shuffled Dirt Patch: Right of the caged GB"
-        region="Aztec Caves"
-        canGetLogic={canReachArea2.in && hasShockwave}
-        canGetBreak={canReachArea2.out && hasShockwave}
-      />
-      <AztecCheck
-        id={32020}
-        name="Shuffled Dirt Patch: Behind the caged GB"
-        region="Aztec Caves"
-        canGetLogic={canReachArea2.in && hasShockwave}
-        canGetBreak={canReachArea2.out && hasShockwave}
       />
       <AztecCheck
         id={32021}
@@ -283,20 +241,6 @@ const canReachLavaRoom = useAztecLlamaLava()
         canGetBreak={canReachArea2.out && hasShockwave}
       />
       <AztecCheck
-        id={32035}
-        name="Shuffled Dirt Patch: Inside DK's quicksand tunnel"
-        region="Aztec Caves"
-        canGetLogic={canReachQSTunnel.in && hasShockwave}
-        canGetBreak={canReachQSTunnel.out && hasShockwave}
-      />
-      <AztecCheck
-        id={32036}
-        name="Shuffled Dirt Patch: Vanilla Warp 1, level start"
-        region="Aztec Caves"
-        canGetLogic={isBreathing.in}
-        canGetBreak={isBreathing.out}
-      />
-      <AztecCheck
         id={32037}
         name="Shuffled Dirt Patch: Vanilla Warp 1, at the oasis"
         region="Aztec Main Area"
@@ -309,20 +253,6 @@ const canReachLavaRoom = useAztecLlamaLava()
         region="Aztec Main Area"
         canGetLogic={isBreathing.in}
         canGetBreak={isBreathing.out}
-      />
-      <AztecCheck
-        id={32039}
-        name="Shuffled Dirt Patch: Vanilla Warp 2, end of the middle tunnel"
-        region="Aztec Caves"
-        canGetLogic={canReachArea2.in && hasShockwave}
-        canGetBreak={canReachArea2.out && hasShockwave}
-      />
-      <AztecCheck
-        id={32040}
-        name="Shuffled Dirt Patch: Vanilla Warp 3, at Cranky's"
-        region="Aztec Caves"
-        canGetLogic={canReachArea2.in && hasShockwave}
-        canGetBreak={canReachArea2.out && hasShockwave}
       />
       <AztecCheck
         id={32041}
@@ -351,13 +281,6 @@ const canReachLavaRoom = useAztecLlamaLava()
         region="Aztec Main Area"
         canGetLogic={canReachArea2.in && hasShockwave}
         canGetBreak={canReachArea2.out && hasShockwave}
-      />
-      <AztecCheck
-        id={32045}
-        name="Shuffled Dirt Patch: Vanilla Warp 5, at DK's quicksand tunnel"
-        region="Aztec Caves"
-        canGetLogic={(didGB[2002] || hasAllBananaports) && isBreathing.in}
-        canGetBreak={(didGB[2002] || hasAllBananaports) && isBreathing.out}
       />
       <AztecCheck
         id={32046}
