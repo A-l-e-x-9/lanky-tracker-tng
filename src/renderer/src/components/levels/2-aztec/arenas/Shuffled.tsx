94c71f03e9977f3acb06af4a3fc2cc5654937d61
A-l-e-x-99 <alexs23743@gmail.com>
2026-04-20 23:52:52 -0400

    Now putting in the shuffled Llama Temple arenas.

--- src/renderer/src/components/levels/2-aztec/arenas/Shuffled.tsx
+++ src/renderer/src/components/levels/2-aztec/arenas/Shuffled.tsx
@@ -5,6 +5,7 @@ import AztecCheck from '../check'
 import CaveArenas from './Cave'
 import AztecMainArenas from './AztecMain'
 import TTArenas from './TinyTemple'
+import LlamaArenas from './LlamaTemple'
 
 const ShuffledArenas: React.FC = () => {
 const canReachVanillaDirt2 = useChunky5DoorGb()
@@ -21,6 +22,7 @@ const canReachLavaRoom = useAztecLlamaLava()
       <CaveArenas />
       <AztecMainArenas />
       <TTArenas />
+      <LlamaArenas />
       <AztecCheck
         id={12002}
         name="Shuffled Battle Arena: In Chunky's 5DT room"
@@ -76,90 +78,6 @@ const canReachLavaRoom = useAztecLlamaLava()
         region="5 Door Temple"
         canGetLogic={canReachVanillaDirt2.in}
         canGetBreak={canReachVanillaDirt2.out}
-      />
-      <AztecCheck
-        id={12053}
-        name="Shuffled Battle Arena: Right of the Llama, lower"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in}
-        canGetBreak={canReachLT.out}
-      />
-      <AztecCheck
-        id={12053}
-        name="Shuffled Battle Arena: Llama Temple entrance"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in}
-        canGetBreak={canReachLT.out}
-      />
-      <AztecCheck
-        id={12054}
-        name="Shuffled Battle Arena: Right of the Llama, upper"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in}
-        canGetBreak={canReachLT.out}
-      />
-      <AztecCheck
-        id={12055}
-        name="Shuffled Battle Arena: Left of the Llama, upper"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in}
-        canGetBreak={canReachLT.out}
-      />
-      <AztecCheck
-        id={12056}
-        name="Shuffled Battle Arena: Left of the Llama, lower"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in}
-        canGetBreak={canReachLT.out}
-      />
-      <AztecCheck
-        id={12057}
-        name="Shuffled Battle Arena: Fraternizing with Gene Rayburn"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in && hasGrapes}
-        canGetBreak={canReachLT.out && hasGrapes}
-      />
-      <AztecCheck
-        id={12058}
-        name="Shuffled Battle Arena: LT Vanilla Warp 1, entrance left"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in}
-        canGetBreak={canReachLT.out}
-      />
-      <AztecCheck
-        id={12059}
-        name="Shuffled Battle Arena: LT Vanilla Warp 1, near the Match Game studio"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in}
-        canGetBreak={canReachLT.out}
-      />
-      <AztecCheck
-        id={12060}
-        name="Shuffled Battle Arena: LT Vanilla Warp 2, entrance right"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in}
-        canGetBreak={canReachLT.out}
-      />
-      <AztecCheck
-        id={12061}
-        name="Shuffled Battle Arena: LT Vanilla Warp 2, in Tiny's lava room"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in && hasMiniMonkey}
-        canGetBreak={canReachLT.out && hasMiniMonkey}
-      />
-      <AztecCheck
-        id={12063}
-        name="Shuffled Battle Arena: DK's quicksand tunnel switch"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in}
-        canGetBreak={canReachLT.out}
-      />
-      <AztecCheck
-        id={12064}
-        name="Shuffled Battle Arena: Tiny's lava room"
-        region="Llama Temple"
-        canGetLogic={canReachLavaRoom.in}
-        canGetBreak={canReachLavaRoom.out}
       />
     </ArenaPool>
   )
