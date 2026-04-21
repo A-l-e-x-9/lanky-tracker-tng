cac835eb21af8a55b55eadcbdadd297703eb4405
A-l-e-x-99 <alexs23743@gmail.com>
2026-04-21 08:45:10 -0400

    Getting shuffled Llama Temple dirt ready.

--- src/renderer/src/components/levels/2-aztec/dirt/Shuffled.tsx
+++ src/renderer/src/components/levels/2-aztec/dirt/Shuffled.tsx
@@ -5,6 +5,7 @@ import AztecCheck from '../check'
 import CaveDirt from './Cave'
 import AztecMainDirt from './AztecMain'
 import TTDirt from './TinyTemple'
+import LlamaDirt from './LlamaTemple'
 
 const ShuffledDirt: React.FC = () => {
 const hasShockwave = useShockwave()
@@ -22,6 +23,7 @@ const canReachLavaRoom = useAztecLlamaLava()
       <CaveDirt />
       <AztecMainDirt />
       <TTDirt />
+      <LlamaDirt />
       <AztecCheck
         id={32002}
         name="Shuffled Dirt Patch: Vanilla Dirt 2 (in Chunky's 5DT room)"
@@ -77,90 +79,6 @@ const canReachLavaRoom = useAztecLlamaLava()
         region="5 Door Temple"
         canGetLogic={canReachVanillaDirt2.in}
         canGetBreak={canReachVanillaDirt2.out}
-      />
-      <AztecCheck
-        id={32053}
-        name="Shuffled Dirt Patch: Right of the Llama, lower"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in && hasShockwave}
-        canGetBreak={canReachLT.out && hasShockwave}
-      />
-      <AztecCheck
-        id={32054}
-        name="Shuffled Dirt Patch: Llama Temple entrance"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in && hasShockwave}
-        canGetBreak={canReachLT.out && hasShockwave}
-      />
-      <AztecCheck
-        id={32055}
-        name="Shuffled Dirt Patch: Right of the Llama, upper"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in && hasShockwave}
-        canGetBreak={canReachLT.out && hasShockwave}
-      />
-      <AztecCheck
-        id={32056}
-        name="Shuffled Dirt Patch: Left of the Llama, upper"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in && hasShockwave}
-        canGetBreak={canReachLT.out && hasShockwave}
-      />
-      <AztecCheck
-        id={32057}
-        name="Shuffled Dirt Patch: Left of the Llama, lower"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in && hasShockwave}
-        canGetBreak={canReachLT.out && hasShockwave}
-      />
-      <AztecCheck
-        id={32058}
-        name="Shuffled Dirt Patch: Fraternizing with Gene Rayburn"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in && hasGrapes && hasShockwave}
-        canGetBreak={canReachLT.out && hasGrapes && hasShockwave}
-      />
-      <AztecCheck
-        id={32059}
-        name="Shuffled Dirt Patch: LT Vanilla Warp 1, entrance left"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in && hasShockwave}
-        canGetBreak={canReachLT.out && hasShockwave}
-      />
-      <AztecCheck
-        id={32060}
-        name="Shuffled Dirt Patch: LT Vanilla Warp 1, near the Match Game studio"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in && hasShockwave}
-        canGetBreak={canReachLT.out && hasShockwave}
-      />
-      <AztecCheck
-        id={32061}
-        name="Shuffled Dirt Patch: LT Vanilla Warp 2, entrance right"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in && hasShockwave}
-        canGetBreak={canReachLT.out && hasShockwave}
-      />
-      <AztecCheck
-        id={32062}
-        name="Shuffled Dirt Patch: LT Vanilla Warp 2, in Tiny's lava room"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in && hasMiniMonkey && hasShockwave}
-        canGetBreak={canReachLT.out && hasMiniMonkey && hasShockwave}
-      />
-      <AztecCheck
-        id={32064}
-        name="Shuffled Dirt Patch: DK's quicksand tunnel switch"
-        region="Llama Temple"
-        canGetLogic={canReachLT.in && hasShockwave}
-        canGetBreak={canReachLT.out && hasShockwave}
-      />
-      <AztecCheck
-        id={32065}
-        name="Shuffled Dirt Patch: Tiny's lava room"
-        region="Llama Temple"
-        canGetLogic={canReachLavaRoom.in && hasShockwave}
-        canGetBreak={canReachLavaRoom.out && hasShockwave}
       />
     </RainbowCoinPool>
   )
