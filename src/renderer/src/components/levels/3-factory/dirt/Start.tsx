a9c1d107087366ca00a70d443da332d7cc261f9a
A-l-e-x-99 <alexs23743@gmail.com>
2026-04-22 00:26:22 -0400

    Fixing the bugs

---
+++ src/renderer/src/components/levels/3-factory/dirt/Start.tsx
@@ -0,0 +1,71 @@
+import RainbowCoinPool from '@renderer/components/pools/RainbowCoins'
+import { useShuffleDirt } from '@renderer/hooks/settings'
+import AztecCheck from '../check'
+import { useGeneralDirt } from '@renderer/hooks/aztec'
+
+const Shuffled: React.FC = () => {
+const isBreathing = useGeneralDirt()
+  return (
+    <RainbowCoinPool>
+      <FactoryCheck
+        id={33002}
+        name="Shuffled Dirt Patch: On carpet at vanilla level start"
+        region="Factory Start Area"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
+      />
+      <FactoryCheck
+        id={33003}
+        name="Shuffled Dirt Patch: Near carpet at vanilla level start"
+        region="Factory Start Area"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
+      />
+      <FactoryCheck
+        id={33004}
+        name="Shuffled Dirt Patch: Clock-in room, left side"
+        region="Factory Start Area"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
+      />
+      <FactoryCheck
+        id={33005}
+        name="Shuffled Dirt Patch: Clock-in room, right side"
+        region="Factory Start Area"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
+      />
+      <FactoryCheck
+        id={33006}
+        name="Shuffled Dirt Patch: Top of the hatch"
+        region="Factory Start Area"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
+      />
+      <FactoryCheck
+        id={33041}
+        name="Shuffled Dirt Patch: Vanilla Warp 1, level start"
+        region="Factory Start Area"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
+      />
+      <FactoryCheck
+        id={33043}
+        name="Shuffled Dirt Patch: Vanilla Warp 2, level start"
+        region="Factory Start Area"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
+      />
+      <FactoryCheck
+        id={33045}
+        name="Shuffled Dirt Patch: Vanilla Warp 3, in front of Snide's"
+        region="Factory Start Area"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
+      />
+    </RainbowCoinPool>
+  )
+}
+
+const StartDirt: React.FC = () => (useShuffleDirt() ? <Shuffled /> : null)
+export default StartDirt
