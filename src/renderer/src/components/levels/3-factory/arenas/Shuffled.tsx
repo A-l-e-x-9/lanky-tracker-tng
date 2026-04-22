baaaf9949bdc73f8daf755af821ba50e2f782c2b
A-l-e-x-99 <alexs23743@gmail.com>
2026-04-22 10:18:51 -0400

    Here come the shuffled "Starcade" arenas.

--- src/renderer/src/components/levels/3-factory/arenas/Shuffled.tsx
+++ src/renderer/src/components/levels/3-factory/arenas/Shuffled.tsx
@@ -4,6 +4,7 @@ import { useBananaportAll } from '@renderer/hooks/settings'
 import { useClimbing, usePunch, useTwirl, useCoconut } from '@renderer/hooks/kongs'
 import FactoryCheck from '../check'
 import StartArenas from './Start'
+import StarcadeArenas from './Starcade'
 
 const ShuffledArenas: React.FC = () => {
 const isBreathing = useGeneralThing()
@@ -23,19 +24,13 @@ const hasCoconuts = useCoconut()
   return (
     <ArenaPool>
       <StartArenas />
+      <StarcadeArenas />
       <FactoryCheck
         id={13000}
         name="Shuffled Battle Arena: Vanilla Arena (in the R&D Room)"
         region="R&D Room"
         canGetLogic={canReachVanillaArena.in}
         canGetBreak={canReachVanillaArena.out}
-      />
-      <FactoryCheck
-        id={13000}
-        name="Shuffled Battle Arena: In Chunky's dark room"
-        region="Storage and Arcade Area"
-        canGetLogic={isBreathing.in && hasPrimatePunch}
-        canGetBreak={isBreathing.out && hasPrimatePunch}
       />
       <FactoryCheck
         id={13000}
@@ -85,48 +80,6 @@ const hasCoconuts = useCoconut()
         region="Prod Room"
         canGetLogic={upperProd.in && isHinaKagiyama}
         canGetBreak={upperProd.out}
-      />
-      <FactoryCheck
-        id={13000}
-        name="Shuffled Battle Arena: Corner of the Storage Room"
-        region="Storage and Arcade Area"
-        canGetLogic={isBreathing.in}
-        canGetBreak={isBreathing.out}
-      />
-      <FactoryCheck
-        id={13000}
-        name="Shuffled Battle Arena: In front of Cranky's"
-        region="Storage and Arcade Area"
-        canGetLogic={isBreathing.in}
-        canGetBreak={isBreathing.out}
-      />
-      <FactoryCheck
-        id={13000}
-        name="Shuffled Battle Arena: In front of Candy's"
-        region="Storage and Arcade Area"
-        canGetLogic={isBreathing.in}
-        canGetBreak={isBreathing.out}
-      />
-      <FactoryCheck
-        id={13000}
-        name="Shuffled Battle Arena: Corner of Chunky's dark room"
-        region="Storage and Arcade Area"
-        canGetLogic={isBreathing.in && hasPrimatePunch}
-        canGetBreak={isBreathing.out && hasPrimatePunch}
-      />
-      <FactoryCheck
-        id={13000}
-        name="Shuffled Battle Arena: Bench in the arcade room"
-        region="Storage and Arcade Area"
-        canGetLogic={isBreathing.in && (hasClimbing || hasAllBananaports)}
-        canGetBreak={isBreathing.out && (hasClimbing || hasAllBananaports)}
-      />
-      <FactoryCheck
-        id={13000}
-        name="Shuffled Battle Arena: Next to the arcade game"
-        region="Storage and Arcade Area"
-        canGetLogic={isBreathing.in && (hasClimbing || hasAllBananaports)}
-        canGetBreak={isBreathing.out && (hasClimbing || hasAllBananaports)}
       />
       <FactoryCheck
         id={13000}
@@ -228,13 +181,6 @@ const hasCoconuts = useCoconut()
       />
       <FactoryCheck
         id={13000}
-        name="Shuffled Battle Arena: Vanilla Warp 1, in storage"
-        region="Storage and Arcade Area"
-        canGetLogic={isBreathing.in}
-        canGetBreak={isBreathing.out}
-      />
-      <FactoryCheck
-        id={13000}
         name="Shuffled Battle Arena: Vanilla Warp 2, R&D-side"
         region="R&D Room"
         canGetLogic={canReachTesting.in && hasClimbing}
@@ -263,13 +209,6 @@ const hasCoconuts = useCoconut()
       />
       <FactoryCheck
         id={13000}
-        name="Shuffled Battle Arena: Vanilla Warp 5, in the arcade"
-        region="Storage and Arcade Area"
-        canGetLogic={isBreathing.in && (hasClimbing || hasAllBananaports)}
-        canGetBreak={isBreathing.out && (hasClimbing || hasAllBananaports)}
-      />
-      <FactoryCheck
-        id={13000}
         name="Shuffled Battle Arena: Vanilla Warp 5, at Funky's"
         region="Testing Room"
         canGetLogic={canReachTesting.in}
@@ -281,13 +220,6 @@ const hasCoconuts = useCoconut()
         region="Prod Room"
         canGetLogic={canDoCrusher.in}
         canGetBreak={canDoCrusher.out}
-      />
-      <FactoryCheck
-        id={13000}
-        name="Shuffled Battle Arena: Inside DK's power hut"
-        region="Storage and Arcade Area"
-        canGetLogic={canReachHut.in && hasCoconuts}
-        canGetBreak={canReachHut.out && hasCoconuts}
       />
     </ArenaPool>
   )
