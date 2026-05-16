9159090fe38f2574d86da7f38562d4ee9cd51b87
A-l-e-x-99 <alexs23743@gmail.com>
2026-05-16 00:18:04 -0400

    grr

--- src/renderer/src/hooks/factory/medals/tiny.ts
+++ src/renderer/src/hooks/factory/medals/tiny.ts
@@ -1,34 +1,46 @@
-import { useDk, useFeather, useMini, useTiny, useTwirl } from '@renderer/hooks/kongs'
+import { useDk, useFeather, useMini, useTiny, useTwirl, useClimbing } from '@renderer/hooks/kongs'
 import { useShuffleColoredBananas } from '@renderer/hooks/settings'
 import {
   useFactoryProductionEnabled,
   useFactoryProductionTop,
   useFactoryTesting,
-  usePlayFactory
+  usePlayFactory, useFoyerFromStorage
 } from '..'
 
 const useTinyMedalCommonLogic = (): number => {
   const testing = useFactoryTesting()
-  const production = useFactoryProductionEnabled()
   const prodTop = useFactoryProductionTop()
   const gun = useFeather()
   const crystal = useMini()
+  const hasClimbing = useClimbing()
+  const canReachHatch = useFoyerFromStorage()
 
-  let banana = 15
+  let banana = 0
+  
+  if (canReachHatch) {
+    banana += 10 //the two bunches in the hatch to Prod Room
+  }
+  
+  if (hasClimbing) {
+    banana += 5 //bunch at Tiny's vent in the Arcade
+  }
+  
   if (testing.in || testing.out) {
-    banana += 25
+    banana += 25 //10 on path to Testing itself, 15? on path to Car Race
     if (crystal) {
-      banana += 5
+      banana += 5 //bunch at Dartboard
     }
     if (gun) {
-      banana += 20
+      banana += 20 //balloons at Funky's and Snide's
     }
   }
-  if (prodTop.in || prodTop.out) {
-    banana += 10
+
+  if (gun) {
+    banana += 10 //balloon in Prod Room
   }
-  if (production.in || production.out) {
-    banana += 20
+
+  if (prodTop.in || prodTop.out) {
+    banana += 20 //bunches up ramps to Tiny's Bonus Barrel
   }
 
   return banana
@@ -36,7 +48,7 @@ const useTinyMedalCommonLogic = (): number => {
 
 export const useTinyMedalInLogic = (): number => {
   const inStage = usePlayFactory()
-  const production = useFactoryProductionEnabled()
+  const prodTop = useFactoryProductionTop()
   const kong = useTiny()
   const move = useTwirl()
   const shuffleBananas = useShuffleColoredBananas()
@@ -52,8 +64,8 @@ export const useTinyMedalInLogic = (): number => {
     return 100
   }
 
-  if (production.in && move) {
-    bananas += 5
+  if (prodTop.in && move) {
+    bananas += 5 //bunch past Tiny's Bonus Barrel
   }
 
   return bananas
@@ -61,7 +73,7 @@ export const useTinyMedalInLogic = (): number => {
 
 export const useTinyMedalOutLogic = (): number => {
   const inStage = usePlayFactory()
-  const production = useFactoryProductionEnabled()
+  const prodTop = useFactoryProductionTop()
   const kong = useTiny()
   const dk = useDk()
   const shuffleBananas = useShuffleColoredBananas()
@@ -77,8 +89,8 @@ export const useTinyMedalOutLogic = (): number => {
     return 100
   }
 
-  if (production.out && dk) {
-    bananas += 5
+  if (prodTop.out && dk) {
+    bananas += 5 //bunch past Tiny's Bonus Barrel
   }
 
   return bananas
