30068fb65c7123f223311162e044d0b8276fb241
A-l-e-x-99 <alexs23743@gmail.com>
2026-05-23 20:54:32 -0400

    Here come the Prod Room Melon Crates.

---
+++ src/renderer/src/components/levels/3-factory/crates/ProdRoom.tsx
@@ -0,0 +1,93 @@
+import CratePool from '@renderer/components/pools/Crates'
+import { useShuffleCrates, useBananaportAll } from '@renderer/hooks/settings'
+import FactoryCheck from '../check'
+import { useGeneralThing, useFactoryProductionEnabled, useFactoryProductionTop, useTinyProductionGb, useDkProdGb } from '@renderer/hooks/factory'
+import { useClimbing, useTwirl } from '@renderer/hooks/kongs'
+
+const Shuffled: React.FC = () => {
+const isBreathing = useGeneralThing()
+const hasClimbing = useClimbing()
+const prodRoomOn = useFactoryProductionEnabled()
+const hasAllBananaports = useBananaportAll()
+const upperProd = useFactoryProductionTop()
+const canDoTinyProd = useTinyProductionGb()
+const isHinaKagiyama = useTwirl()
+const canDoCrusher = useDkProdGb()
+  return (
+    <CratePool>
+      <FactoryCheck
+        id={23007}
+        name="Shuffled Melon Crate: Center of the hatch"
+        region="Prod Room"
+        canGetLogic={isBreathing.in && hasClimbing}
+        canGetBreak={isBreathing.out}
+      />
+      <FactoryCheck
+        id={23008}
+        name="Shuffled Melon Crate: Bottom of the hatch"
+        region="Prod Room"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
+      />
+      <FactoryCheck
+        id={23009}
+        name="Shuffled Melon Crate: In the tunnel to the Prod Room"
+        region="Prod Room"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
+      />
+      <FactoryCheck
+        id={23010}
+        name="Shuffled Melon Crate: Prod Room lower area"
+        region="Prod Room"
+        canGetLogic={isBreathing.in && (prodRoomOn.in || hasAllBananaports)}
+        canGetBreak={isBreathing.out && (prodRoomOn.out || hasAllBananaports)}
+      />
+      <FactoryCheck
+        id={23011}
+        name="Shuffled Melon Crate: Under conveyors to Tiny's Prod Room GB"
+        region="Prod Room"
+        canGetLogic={upperProd.in}
+        canGetBreak={upperProd.out}
+      />
+      <FactoryCheck
+        id={23012}
+        name="Shuffled Melon Crate: The platform past Tiny's Prod Room GB"
+        region="Prod Room"
+        canGetLogic={canDoTinyProd.in}
+        canGetBreak={canDoTinyProd.out}
+      />
+      <FactoryCheck
+        id={23013}
+        name="Shuffled Melon Crate: On a vent platform near the elevators"
+        region="Prod Room"
+        canGetLogic={upperProd.in && isHinaKagiyama}
+        canGetBreak={upperProd.out}
+      />
+      <FactoryCheck
+        id={23047}
+        name="Shuffled Melon Crate: Vanilla Warp 4, bottom of the Prod Room"
+        region="Prod Room"
+        canGetLogic={isBreathing.in}
+        canGetBreak={isBreathing.out}
+      />
+      <FactoryCheck
+        id={23048}
+        name="Shuffled Melon Crate: Vanilla Warp 4, top of the Prod Room"
+        region="Prod Room"
+        canGetLogic={upperProd.in}
+        canGetBreak={upperProd.out}
+      />
+      <FactoryCheck
+        id={23051}
+        name="Shuffled Melon Crate: At the bend in the crusher's path"
+        region="Prod Room"
+        canGetLogic={canDoCrusher.in}
+        canGetBreak={canDoCrusher.out}
+      />
+    </CratePool>
+  )
+}
+
+const ProdRoomCrates: React.FC = () => (useShuffleCrates() ? <Shuffled /> : null)
+export default ProdRoomCrates
--- src/renderer/src/components/levels/3-factory/crates/Shuffled.tsx
+++ src/renderer/src/components/levels/3-factory/crates/Shuffled.tsx
@@ -1,8 +1,4 @@
 import CratePool from '@renderer/components/pools/Crates'
-import { useGeneralThing, useFactoryProductionEnabled, useFactoryProductionTop, useTinyProductionGb, useDkProdGb } from '@renderer/hooks/factory'
-import { useBananaportAll } from '@renderer/hooks/settings'
-import { useClimbing, useTwirl } from '@renderer/hooks/kongs'
-import FactoryCheck from '../check'
 import StartCrates from './Start'
 import StarcadeCrates from './Starcade'
 import TestingCrates from './Testing'
@@ -10,14 +6,6 @@ import RNDCrates from './RND'
 import ProdRoomCrates from './ProdRoom'
 
 const ShuffledCrates: React.FC = () => {
-const isBreathing = useGeneralThing()
-const hasClimbing = useClimbing()
-const prodRoomOn = useFactoryProductionEnabled()
-const hasAllBananaports = useBananaportAll()
-const upperProd = useFactoryProductionTop()
-const canDoTinyProd = useTinyProductionGb()
-const isHinaKagiyama = useTwirl()
-const canDoCrusher = useDkProdGb()
   return (
     <CratePool>
       <StartCrates />
@@ -25,76 +13,6 @@ const canDoCrusher = useDkProdGb()
       <TestingCrates />
       <RNDCrates />
       <ProdRoomCrates />
-      <FactoryCheck
-        id={23007}
-        name="Shuffled Melon Crate: Center of the hatch"
-        region="Prod Room"
-        canGetLogic={isBreathing.in && hasClimbing}
-        canGetBreak={isBreathing.out}
-      />
-      <FactoryCheck
-        id={23008}
-        name="Shuffled Melon Crate: Bottom of the hatch"
-        region="Prod Room"
-        canGetLogic={isBreathing.in}
-        canGetBreak={isBreathing.out}
-      />
-      <FactoryCheck
-        id={23009}
-        name="Shuffled Melon Crate: In the tunnel to the Prod Room"
-        region="Prod Room"
-        canGetLogic={isBreathing.in}
-        canGetBreak={isBreathing.out}
-      />
-      <FactoryCheck
-        id={23010}
-        name="Shuffled Melon Crate: Prod Room lower area"
-        region="Prod Room"
-        canGetLogic={isBreathing.in && (prodRoomOn.in || hasAllBananaports)}
-        canGetBreak={isBreathing.out && (prodRoomOn.out || hasAllBananaports)}
-      />
-      <FactoryCheck
-        id={23011}
-        name="Shuffled Melon Crate: Under conveyors to Tiny's Prod Room GB"
-        region="Prod Room"
-        canGetLogic={upperProd.in}
-        canGetBreak={upperProd.out}
-      />
-      <FactoryCheck
-        id={23012}
-        name="Shuffled Melon Crate: The platform past Tiny's Prod Room GB"
-        region="Prod Room"
-        canGetLogic={canDoTinyProd.in}
-        canGetBreak={canDoTinyProd.out}
-      />
-      <FactoryCheck
-        id={23013}
-        name="Shuffled Melon Crate: On a vent platform near the elevators"
-        region="Prod Room"
-        canGetLogic={upperProd.in && isHinaKagiyama}
-        canGetBreak={upperProd.out}
-      />
-      <FactoryCheck
-        id={23047}
-        name="Shuffled Melon Crate: Vanilla Warp 4, bottom of the Prod Room"
-        region="Prod Room"
-        canGetLogic={isBreathing.in}
-        canGetBreak={isBreathing.out}
-      />
-      <FactoryCheck
-        id={23048}
-        name="Shuffled Melon Crate: Vanilla Warp 4, top of the Prod Room"
-        region="Prod Room"
-        canGetLogic={upperProd.in}
-        canGetBreak={upperProd.out}
-      />
-      <FactoryCheck
-        id={23051}
-        name="Shuffled Melon Crate: At the bend in the crusher's path"
-        region="Prod Room"
-        canGetLogic={canDoCrusher.in}
-        canGetBreak={canDoCrusher.out}
-      />
     </CratePool>
   )
 }
