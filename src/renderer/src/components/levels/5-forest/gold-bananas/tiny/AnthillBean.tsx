bfea3e186565739e622bea8d008fc110c99bea26
A-l-e-x-99 <alexs23743@gmail.com>
2026-06-13 13:04:33 -0400

    Here comes Forest

--- src/renderer/src/components/levels/5-forest/gold-bananas/tiny/AnthillBean.tsx
+++ src/renderer/src/components/levels/5-forest/gold-bananas/tiny/AnthillBean.tsx
@@ -1,36 +1,20 @@
-import MiscPool, { BeanPool } from '@renderer/components/pools/Misc'
-import { useBean } from '@renderer/hooks/settings'
+import { BeanPool } from '@renderer/components/pools/Misc'
 import { useTinyAntGb } from '@renderer/hooks/forest'
 import ForestCheck from '../../check'
 
 const AnthillBean: React.FC = () => {
 const canDo = useTinyAntGb()
-const beanWinCon = useBean()
-if (beanWinCon) {
 return (
   <BeanPool>
     <ForestCheck
       id={5034}
-      name="THE BEAN (Tree Stump Gauntlet's Second Reward)"
+      name="Tree Stump Gauntlet's Second Reward (home of The Bean™ in vanilla)"
       region="Forest Area 4"
       canGetLogic={canDo.in}
       canGetBreak={canDo.out}
     />
   </BeanPool>
 )
-} else {
-return (
-  <MiscPool>
-    <ForestCheck
-      id={5034}
-      name="THE BEAN (Tree Stump Gauntlet's Second Reward)"
-      region="Forest Area 4"
-      canGetLogic={canDo.in}
-      canGetBreak={canDo.out}
-    />
-  </MiscPool>
-)
-}
 }
 
 export default AnthillBean
--- src/renderer/src/components/levels/5-forest/medals/DiddyMedal.tsx
+++ src/renderer/src/components/levels/5-forest/medals/DiddyMedal.tsx
@@ -1,6 +1,7 @@
 import { useDiddyMedalInLogic, useDiddyMedalOutLogic } from '@renderer/hooks/forest/medals/diddy'
 import { useCbCount } from '@renderer/hooks/settings'
 import ForestCheck from '../check'
+import { HalfMedalPool } from '@renderer/components/pools/BananaMedals'
 
 const DiddyMedal: React.FC = () => {
   const inLogic = useDiddyMedalInLogic()
@@ -17,6 +18,7 @@ const DiddyMedal: React.FC = () => {
       canGetLogic={inLogic >= cbCount}
       canGetBreak={outLogic >= cbCount}
     />
+    <HalfMedalPool>
     <ForestCheck
       id={5201}
       name="Diddy's Half-Medal"
@@ -24,6 +26,7 @@ const DiddyMedal: React.FC = () => {
       canGetLogic={inLogic >= halfMedal}
       canGetBreak={outLogic >= halfMedal}
     />
+    </HalfMedalPool>
   </>
   )
 }
