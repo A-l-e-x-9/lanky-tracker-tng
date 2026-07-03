86d2be57b17fdbffe4480e1b91b56505fec470f9
A-l-e-x-99 <alexs23743@gmail.com>
2026-07-03 01:08:26 -0400

    Taking steps to ensure the right check shows up for a KtW seed

--- src/renderer/src/components/levels/6-caves/gold-bananas/chunky/Igloo.tsx
+++ src/renderer/src/components/levels/6-caves/gold-bananas/chunky/Igloo.tsx
@@ -1,4 +1,6 @@
 //hope VerdantAsh doesn't blow a gasket...okay, maybe I do! >xD
+import { useShallow } from 'zustand/react/shallow'
+import useDonkStore from '@renderer/store'
 import GBPool from '@renderer/components/pools/GB'
 import Miniboss from '@renderer/components/pools/Miniboss'
 import { useChunkyIglooGb } from '@renderer/hooks/caves'
@@ -6,6 +8,20 @@ import CavesCheck from '../../check'
 
 const ChunkyIgloo: React.FC = () => {
 const thatFuckingRabbit = useChunkyIglooGb()
+const isKillTheWabbit = useDonkStore(useShallow((state) => state.winCondition.killTheWabbit))
+if (isKillTheWabbit) {
+return (
+<GBPool>
+  <CavesCheck
+    id={6042}
+    name="Chunky's 5-Door Igloo Room (SHOOT THIS BASTARD NOW!!!)"
+    region="Caves Igloo"
+    canGetLogic={thatFuckingRabbit.in}
+    canGetBreak={thatFuckingRabbit.out}
+  />
+</GBPool>
+)
+} else {
 return (
 <GBPool>
 <Miniboss>
