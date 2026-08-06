1ea0dd4579fb5f6e53d345730adf028e0b5d843a
A-l-e-x-99 <alexs23743@gmail.com>
2026-08-05 20:59:04 -0400

    Attempting to properly implement Krem Kapture stuff.

---
+++ src/renderer/src/components/levels/9-krem-kapture/enemy-list.tsx
@@ -0,0 +1,18 @@
+//import {  } from '@renderer/hooks/helm'
+import { useCamera } from '@renderer/hooks/kongs'
+import KKCheck from '../check'
+
+const KremKapture: React.FC = () => {
+  const hasFairyCam = useCamera()
+  return (
+    <>
+      <KKCheck
+        id={-1}
+        name="Gnawty"
+        canGetLogic={hasFairyCam}
+      />
+    </>
+  )
+}
+
+export default KremKapture
