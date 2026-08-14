f1d90c2a5018b3bdd84906d3f012b7f5b1d53e5a
A-l-e-x-99 <alexs23743@gmail.com>
2026-08-13 22:38:51 -0400

    ...

--- src/renderer/src/components/levels/4-galleon/kasplats/Outskirts.tsx
+++ src/renderer/src/components/levels/4-galleon/kasplats/Outskirts.tsx
@@ -1,16 +1,10 @@
 import KasplatPool from '@renderer/components/pools/Kasplats'
-import { useCactusKasplat, useChunkySeasickGb, useDiddyMechGb, useLankyGoldGb } from '@renderer/hooks/galleon'
+import { useCactusKasplat } from '@renderer/hooks/galleon'
 import { useShuffleKasplats } from '@renderer/hooks/settings'
-import { useClimbing, usePunch } from '@renderer/hooks/kongs'
 import GalleonCheck from '../check'
 
 const Shuffled: React.FC = () => {
 const chunkyVanillaKasplat = useCactusKasplat()
-const mechfishAgenda = useDiddyMechGb()
-const canDoLankyGoldTower = useLankyGoldGb()
-const canDoChunkyShip = useChunkySeasickGb()
-const hasClimbing = useClimbing()
-const hasPrimatePunch = usePunch()
   return (
     <KasplatPool>
       <GalleonCheck
--- src/renderer/src/components/levels/4-galleon/kasplats/Shuffled.tsx
+++ src/renderer/src/components/levels/4-galleon/kasplats/Shuffled.tsx
@@ -1,5 +1,5 @@
 import KasplatPool from '@renderer/components/pools/Kasplats'
-import { useGeneralThing, useDiddyGoldGb, useKevin, useCannonKasplat, useVineKasplat, useCactusKasplat, useGalleonLighthousePlatform, useChunkySeasickGb, useGalleonLighthouseInside, useDiddyMechGb, useLankyGoldGb } from '@renderer/hooks/galleon'
+import { useGeneralThing, useDiddyGoldGb, useKevin, useCannonKasplat, useChunkySeasickGb, useGalleonLighthouseInside, useDiddyMechGb, useLankyGoldGb } from '@renderer/hooks/galleon'
 import { useShuffleKasplats } from '@renderer/hooks/settings'
 import { useClimbing, usePunch } from '@renderer/hooks/kongs'
 import GalleonCheck from '../check'
@@ -8,10 +8,7 @@ const Shuffled: React.FC = () => {
 const DKVanillaKasplat = useDiddyGoldGb()
 const diddyVanillaKasplat = useKevin()
 const lankyVanillaKasplat = useCannonKasplat()
-const tinyVanillaKasplat = useVineKasplat()
-const chunkyVanillaKasplat = useCactusKasplat()
 const isBreathing = useGeneralThing()
-const canReachLighthouseBase = useGalleonLighthousePlatform()
 const canGoInLighthouse = useGalleonLighthouseInside()
 const mechfishAgenda = useDiddyMechGb()
 const canDoLankyGoldTower = useLankyGoldGb()
