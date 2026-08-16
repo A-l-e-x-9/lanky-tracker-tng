3e3867b636543799e8f032b2d641a8c607c3cf02
A-l-e-x-99 <alexs23743@gmail.com>
2026-08-16 13:54:12 -0400

    ...

--- src/renderer/src/components/levels/5-forest/arenas/MushInterior.tsx
+++ src/renderer/src/components/levels/5-forest/arenas/MushInterior.tsx
@@ -1,21 +1,11 @@
 import ArenaPool from '@renderer/components/pools/Arenas'
-import { useGeneralThing, useForestMushroomRoof, useForestOwl, useForestMushroomTop, useTinyAntGb, useSlamForest, useForestBarn, useForestSpiderBoss, useLankyMushGb } from '@renderer/hooks/forest'
+import { useGeneralThing, useForestSpiderBoss, useLankyMushGb } from '@renderer/hooks/forest'
 import { useShuffledArenas } from '@renderer/hooks/settings'
-import { useRocket, useChunky, useBalloon, useDiddy, usePunch, useDk } from '@renderer/hooks/kongs'
+import { usePunch, useDk } from '@renderer/hooks/kongs'
 import ForestCheck from '../check'
 
 const Shuffled: React.FC = () => {
 const isBreathing = useGeneralThing()
-const hasJetbarrel = useRocket()
-const hasChunky = useChunky()
-const canReachTopOfShroom = useForestMushroomRoof()
-const canReachArea4 = useForestOwl()
-const hasBalloon = useBalloon()
-const canGetNearTopOfShroom = useForestMushroomTop()
-const canReachStump = useTinyAntGb()
-const hasDiddy = useDiddy()
-const hasSlam = useSlamForest()
-const canGetInThornvine = useForestBarn()
 const canReachSpiderBoss = useForestSpiderBoss()
 const hasPrimatePunch = usePunch()
 const hasDK = useDk()
