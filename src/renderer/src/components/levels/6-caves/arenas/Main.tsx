23a51975db77f3e966f762fd3f7ec7b91b003a18
A-l-e-x-99 <alexs23743@gmail.com>
2026-08-17 01:26:15 -0400

    Next wave

--- src/renderer/src/components/levels/6-caves/arenas/Main.tsx
+++ src/renderer/src/components/levels/6-caves/arenas/Main.tsx
@@ -1,26 +1,16 @@
 import useDonkStore from '@renderer/store'
 import { useShallow } from 'zustand/react/shallow'
 import ArenaPool from '@renderer/components/pools/Arenas'
-import { useGeneralThing, useDkRotateGb, useChunkyClearGb, useTinyPortGb, useTinyCaveGb, useIceWalls, useCavesMiniFunky, useCavesPillar, useCavesIgloo, useLankyIglooGb, useLankyCastleGb } from '@renderer/hooks/caves'
+import { useGeneralThing, useTinyCaveGb, useCavesMiniFunky, useCavesPillar, useCavesIgloo, useLankyIglooGb, useLankyCastleGb } from '@renderer/hooks/caves'
 import { useShuffledArenas } from '@renderer/hooks/settings'
-import { useMonkeyport, useRocket, useTwirl, usePunch, useBalloon, useHighGrab, useChunky, useBarrel, useDk, useBongos, useSax, useGuitar, useTrombone } from '@renderer/hooks/kongs'
+import { useBalloon, useHighGrab, useDk, useBongos, useSax, useGuitar, useTrombone } from '@renderer/hooks/kongs'
 import CavesCheck from '../check'
 
 const Shuffled: React.FC = () => {
 const isBreathing = useGeneralThing()
-const canReachVanillaArena = useDkRotateGb()
-const canDoKoshaGB = useChunkyClearGb()
-const hasMonkeyport = useMonkeyport()
-const canDoTinyIglooGB = useTinyPortGb()
-const hasJetbarrel = useRocket()
-const isHinaKagiyama = useTwirl()
 const canDoShrinkGB = useTinyCaveGb()
-const hasPunch = usePunch()
-const canGetPastIceWalls = useIceWalls()
 const hasBalloon = useBalloon()
 const canHighGrab = useHighGrab()
-const hasChunky = useChunky()
-const hasBarrels = useBarrel()
 const canReachWarp4 = useCavesMiniFunky()
 const [didCheck] = useDonkStore(useShallow((state) => [state.checks]))
 const hasDK = useDk()
