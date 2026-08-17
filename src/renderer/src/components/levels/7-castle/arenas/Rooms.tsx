fbef3efc54c3ad3343f68f85cdf63fd649bea8f5
A-l-e-x-99 <alexs23743@gmail.com>
2026-08-17 10:12:34 -0400

    ...

--- src/renderer/src/components/levels/7-castle/arenas/Rooms.tsx
+++ src/renderer/src/components/levels/7-castle/arenas/Rooms.tsx
@@ -1,24 +1,16 @@
 import ArenaPool from '@renderer/components/pools/Arenas'
 import { useGeneralThing, useSlamCastle, useOpenCrypt } from '@renderer/hooks/castle'
 import { useShuffledArenas } from '@renderer/hooks/settings'
-import { useDiddy, useDk, usePunch, useCoconut, usePeanut, usePineapple, useGrape, useFeather, useSprint, useStrong, useMonkeyport, useChunky, useMini, useBlast } from '@renderer/hooks/kongs'
+import { useDk, useGrape, useStrong, useChunky, useMini, useBlast } from '@renderer/hooks/kongs'
 import CastleCheck from '../check'
 
 const Shuffled: React.FC = () => {
 const isBreathing = useGeneralThing()
-const hasDiddy = useDiddy()
 const hasSlam = useSlamCastle()
 const hasDK = useDk()
-const hasPunch = usePunch()
-const hasCoconuts = useCoconut()
-const hasPeanuts = usePeanut()
-const hasPineapples = usePineapple()
 const cryptPreOpened = useOpenCrypt()
 const hasGrapes = useGrape()
-const hasFeathers = useFeather()
-const hasSprint = useSprint()
 const hasStrongKong = useStrong()
-const hasMonkeyport = useMonkeyport()
 const hasChunky = useChunky()
 const hasMini = useMini()
 const hasBlast = useBlast()
