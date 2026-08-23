1f3859c9e4ea1562ea2b2f193d0c944ecb2c0547
A-l-e-x-99 <alexs23743@gmail.com>
2026-08-22 21:07:12 -0400

    ...

--- src/renderer/src/components/levels/4-galleon/crates/Outskirts.tsx
+++ src/renderer/src/components/levels/4-galleon/crates/Outskirts.tsx
@@ -1,28 +1,13 @@
 import CratePool from '@renderer/components/pools/Crates'
-import { useGeneralThing, useArena, useGalleonLighthouseInside, useGalleonCannon, useGalleonCavernTop, useGalleonLighthousePlatform, useLankyGoldGb, useGalleonOutskirts, useDiddyGoldGb, useGalleonSeasickShip, useLanky2DoorShipGb, useGalleonTreasureRoom } from '@renderer/hooks/galleon'
+import { useGalleonOutskirts } from '@renderer/hooks/galleon'
 import { useShuffleCrates } from '@renderer/hooks/settings'
-import { usePunch, useClimbing, useDive, useLanky, useAnyGun, useOrange } from '@renderer/hooks/kongs'
+import { useDive, useLanky } from '@renderer/hooks/kongs'
 import GalleonCheck from '../check'
 
 const Shuffled: React.FC = () => {
-const isBreathing = useGeneralThing()
-const canReachVanillaArena = useArena()
-const canReachLighthouse = useGalleonLighthouseInside()
-const canReachCannonGame = useGalleonCannon()
-const canReachTinysKasplat = useGalleonCavernTop()
-const canReachLighthouseOuter = useGalleonLighthousePlatform()
-const canReachLankyGold = useLankyGoldGb()
 const canReachShipyard = useGalleonOutskirts()
-const canReachDiddyGold = useDiddyGoldGb()
-const canReachChunkyShip = useGalleonSeasickShip()
-const hasPrimatePunch = usePunch()
-const hasClimbing = useClimbing()
-const canReachLankys2DS = useLanky2DoorShipGb()
-const canReachTreasureRoom = useGalleonTreasureRoom()
 const hasDiving = useDive()
 const hasLanky = useLanky()
-const hasAnyGun = useAnyGun()
-const hasOranges = useOrange()
   return (
     <CratePool>
       <GalleonCheck
