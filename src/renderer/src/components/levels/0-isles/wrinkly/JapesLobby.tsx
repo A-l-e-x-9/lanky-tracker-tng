76e4d3f583995de3a433b2830d4d9c4337bba3c0
A-l-e-x-99 <alexs23743@gmail.com>
2026-09-08 01:30:18 -0400

    Doing the Kong locks first because they're easier.

--- src/renderer/src/components/levels/0-isles/wrinkly/JapesLobby.tsx
+++ src/renderer/src/components/levels/0-isles/wrinkly/JapesLobby.tsx
@@ -2,12 +2,19 @@ import { useShallow } from 'zustand/react/shallow'
 import useDonkStore from '@renderer/store'
 import WrinklyPool from '@renderer/components/pools/WrinklyDoors'
 import { useJapesLobbyGeneric } from '@renderer/hooks/isles'
+import { useDk, useDiddy, useLanky, useTiny, useChunky } from '@renderer/hooks/kongs'
 import IslesCheck from '../check'
 
 /*Since Version 4 of the DK64 Randomizer, you can have major rewards in Wrinkly Kong's hint doors (and hints on checks).*/
 const JapesLobby: React.FC = () => {
 const canDo = useJapesLobbyGeneric()
 const [locked, kongLocked] = useDonkStore(useShallow((state) => [state.settings.lockedWrinklyDoors, state.ui.konglessHintDoorsOff]))
+const hasDK = useDk()
+const hasDiddy = useDiddy()
+const hasLanky = useLanky()
+const hasTiny = useTiny()
+const hasChunky = useChunky()
+
 if (!locked && !kongLocked) {
   return (
     <WrinklyPool>
@@ -95,36 +102,36 @@ if (!locked && !kongLocked) {
           id={65}
           name="Wrinkly Door: Japes DK"
           region="Japes-Forest Lobbies"
-          canGetLogic={canDo.in}
-          canGetBreak={canDo.out}
+          canGetLogic={canDo.in && hasDK}
+          canGetBreak={canDo.out && hasDK}
         />
         <IslesCheck
           id={64}
           name="Wrinkly Door: Japes Diddy"
           region="Japes-Forest Lobbies"
-          canGetLogic={canDo.in}
-          canGetBreak={canDo.out}
+          canGetLogic={canDo.in && hasDiddy}
+          canGetBreak={canDo.out && hasDiddy}
         />
         <IslesCheck
           id={66}
           name="Wrinkly Door: Japes Lanky"
           region="Japes-Forest Lobbies"
-          canGetLogic={canDo.in}
-          canGetBreak={canDo.out}
+          canGetLogic={canDo.in && hasLanky}
+          canGetBreak={canDo.out && hasLanky}
         />
         <IslesCheck
           id={67}
           name="Wrinkly Door: Japes Tiny"
           region="Japes-Forest Lobbies"
-          canGetLogic={canDo.in}
-          canGetBreak={canDo.out}
+          canGetLogic={canDo.in && hasTiny}
+          canGetBreak={canDo.out && hasTiny}
         />
         <IslesCheck
           id={63}
           name="Wrinkly Door: Japes Chunky"
           region="Japes-Forest Lobbies"
-          canGetLogic={canDo.in}
-          canGetBreak={canDo.out}
+          canGetLogic={canDo.in && hasChunky}
+          canGetBreak={canDo.out && hasChunky}
         />
     </WrinklyPool>
   )
@@ -135,36 +142,36 @@ if (!locked && !kongLocked) {
           id={65}
           name="Wrinkly Door: Japes DK"
           region="Japes-Forest Lobbies"
-          canGetLogic={canDo.in}
-          canGetBreak={canDo.out}
+          canGetLogic={canDo.in && hasDK}
+          canGetBreak={canDo.out && hasDK}
         />
         <IslesCheck
           id={64}
           name="Wrinkly Door: Japes Diddy"
           region="Japes-Forest Lobbies"
-          canGetLogic={canDo.in}
-          canGetBreak={canDo.out}
+          canGetLogic={canDo.in && hasDiddy}
+          canGetBreak={canDo.out && hasDiddy}
         />
         <IslesCheck
           id={66}
           name="Wrinkly Door: Japes Lanky"
           region="Japes-Forest Lobbies"
-          canGetLogic={canDo.in}
-          canGetBreak={canDo.out}
+          canGetLogic={canDo.in && hasLanky}
+          canGetBreak={canDo.out && hasLanky}
         />
         <IslesCheck
           id={67}
           name="Wrinkly Door: Japes Tiny"
           region="Japes-Forest Lobbies"
-          canGetLogic={canDo.in}
-          canGetBreak={canDo.out}
+          canGetLogic={canDo.in && hasTiny}
+          canGetBreak={canDo.out && hasTiny}
         />
         <IslesCheck
           id={63}
           name="Wrinkly Door: Japes Chunky"
           region="Japes-Forest Lobbies"
-          canGetLogic={canDo.in}
-          canGetBreak={canDo.out}
+          canGetLogic={canDo.in && hasChunky}
+          canGetBreak={canDo.out && hasChunky}
         />
     </WrinklyPool>
   )
