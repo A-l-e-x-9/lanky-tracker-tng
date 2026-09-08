cae74ae179cb1949662d69090bda21cfd9f7e6ec
A-l-e-x-99 <alexs23743@gmail.com>
2026-09-08 00:48:18 -0400

    ...

--- src/renderer/src/components/levels/0-isles/wrinkly/FactoryLobby.tsx
+++ src/renderer/src/components/levels/0-isles/wrinkly/FactoryLobby.tsx
@@ -1,10 +1,11 @@
 import WrinklyPool from '@renderer/components/pools/WrinklyDoors'
-import { useFactoryLobbyLower } from '@renderer/hooks/isles'
+import { useFactoryLobbyLower, useFactoryLobbyUpper } from '@renderer/hooks/isles'
 import IslesCheck from '../check'
 
 /*Since Version 4 of the DK64 Randomizer, you can have major rewards in Wrinkly Kong's hint doors (and hints on checks).*/
 const FactoryLobbyDK: React.FC = () => {
 const canDo = useFactoryLobbyLower()
+const check = useFactoryLobbyUpper()
   return (
     <WrinklyPool>
         <IslesCheck
