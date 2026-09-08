ea9a063f4a5052df2096ccd428aa8000dca2152a
A-l-e-x-99 <alexs23743@gmail.com>
2026-09-08 01:15:41 -0400

    Let's try it again...

--- src/renderer/src/components/levels/0-isles/wrinkly/JapesLobby.tsx
+++ src/renderer/src/components/levels/0-isles/wrinkly/JapesLobby.tsx
@@ -1,3 +1,5 @@
+import { useShallow } from 'zustand/react/shallow'
+import useDonkStore from '@renderer/store'
 import WrinklyPool from '@renderer/components/pools/WrinklyDoors'
 import { useJapesLobbyGeneric } from '@renderer/hooks/isles'
 import IslesCheck from '../check'
@@ -5,45 +7,168 @@ import IslesCheck from '../check'
 /*Since Version 4 of the DK64 Randomizer, you can have major rewards in Wrinkly Kong's hint doors (and hints on checks).*/
 const JapesLobby: React.FC = () => {
 const canDo = useJapesLobbyGeneric()
+const [locked, kongLocked] = useDonkStore(useShallow((state) => [state.settings.lockedWrinklyDoors, state.ui.konglessHintDoorsOff]))
+if (!locked && !kongLocked) {
   return (
     <WrinklyPool>
         <IslesCheck
           id={65}
-          name="Japes Lobby DK"
+          name="Wrinkly Door: Japes DK"
           region="Japes-Forest Lobbies"
           canGetLogic={canDo.in}
           canGetBreak={canDo.out}
         />
         <IslesCheck
           id={64}
-          name="Japes Lobby Diddy"
+          name="Wrinkly Door: Japes Diddy"
           region="Japes-Forest Lobbies"
           canGetLogic={canDo.in}
           canGetBreak={canDo.out}
         />
         <IslesCheck
           id={66}
-          name="Japes Lobby Lanky"
+          name="Wrinkly Door: Japes Lanky"
           region="Japes-Forest Lobbies"
           canGetLogic={canDo.in}
           canGetBreak={canDo.out}
         />
         <IslesCheck
           id={67}
-          name="Japes Lobby Tiny"
+          name="Wrinkly Door: Japes Tiny"
           region="Japes-Forest Lobbies"
           canGetLogic={canDo.in}
           canGetBreak={canDo.out}
         />
         <IslesCheck
           id={63}
-          name="Japes Lobby Chunky"
+          name="Wrinkly Door: Japes Chunky"
           region="Japes-Forest Lobbies"
           canGetLogic={canDo.in}
           canGetBreak={canDo.out}
         />
     </WrinklyPool>
   )
+  } else if (locked && !kongLocked) {
+  return (
+    <WrinklyPool>
+        <IslesCheck
+          id={65}
+          name="Wrinkly Door: Japes DK"
+          region="Japes-Forest Lobbies"
+          canGetLogic={canDo.in}
+          canGetBreak={canDo.out}
+        />
+        <IslesCheck
+          id={64}
+          name="Wrinkly Door: Japes Diddy"
+          region="Japes-Forest Lobbies"
+          canGetLogic={canDo.in}
+          canGetBreak={canDo.out}
+        />
+        <IslesCheck
+          id={66}
+          name="Wrinkly Door: Japes Lanky"
+          region="Japes-Forest Lobbies"
+          canGetLogic={canDo.in}
+          canGetBreak={canDo.out}
+        />
+        <IslesCheck
+          id={67}
+          name="Wrinkly Door: Japes Tiny"
+          region="Japes-Forest Lobbies"
+          canGetLogic={canDo.in}
+          canGetBreak={canDo.out}
+        />
+        <IslesCheck
+          id={63}
+          name="Wrinkly Door: Japes Chunky"
+          region="Japes-Forest Lobbies"
+          canGetLogic={canDo.in}
+          canGetBreak={canDo.out}
+        />
+    </WrinklyPool>
+  )
+  } else if (!locked && kongLocked) {
+  return (
+    <WrinklyPool>
+        <IslesCheck
+          id={65}
+          name="Wrinkly Door: Japes DK"
+          region="Japes-Forest Lobbies"
+          canGetLogic={canDo.in}
+          canGetBreak={canDo.out}
+        />
+        <IslesCheck
+          id={64}
+          name="Wrinkly Door: Japes Diddy"
+          region="Japes-Forest Lobbies"
+          canGetLogic={canDo.in}
+          canGetBreak={canDo.out}
+        />
+        <IslesCheck
+          id={66}
+          name="Wrinkly Door: Japes Lanky"
+          region="Japes-Forest Lobbies"
+          canGetLogic={canDo.in}
+          canGetBreak={canDo.out}
+        />
+        <IslesCheck
+          id={67}
+          name="Wrinkly Door: Japes Tiny"
+          region="Japes-Forest Lobbies"
+          canGetLogic={canDo.in}
+          canGetBreak={canDo.out}
+        />
+        <IslesCheck
+          id={63}
+          name="Wrinkly Door: Japes Chunky"
+          region="Japes-Forest Lobbies"
+          canGetLogic={canDo.in}
+          canGetBreak={canDo.out}
+        />
+    </WrinklyPool>
+  )
+  } else if (locked && kongLocked) {
+  return (
+    <WrinklyPool>
+        <IslesCheck
+          id={65}
+          name="Wrinkly Door: Japes DK"
+          region="Japes-Forest Lobbies"
+          canGetLogic={canDo.in}
+          canGetBreak={canDo.out}
+        />
+        <IslesCheck
+          id={64}
+          name="Wrinkly Door: Japes Diddy"
+          region="Japes-Forest Lobbies"
+          canGetLogic={canDo.in}
+          canGetBreak={canDo.out}
+        />
+        <IslesCheck
+          id={66}
+          name="Wrinkly Door: Japes Lanky"
+          region="Japes-Forest Lobbies"
+          canGetLogic={canDo.in}
+          canGetBreak={canDo.out}
+        />
+        <IslesCheck
+          id={67}
+          name="Wrinkly Door: Japes Tiny"
+          region="Japes-Forest Lobbies"
+          canGetLogic={canDo.in}
+          canGetBreak={canDo.out}
+        />
+        <IslesCheck
+          id={63}
+          name="Wrinkly Door: Japes Chunky"
+          region="Japes-Forest Lobbies"
+          canGetLogic={canDo.in}
+          canGetBreak={canDo.out}
+        />
+    </WrinklyPool>
+  )
+  }
 }
 
 export default JapesLobby
