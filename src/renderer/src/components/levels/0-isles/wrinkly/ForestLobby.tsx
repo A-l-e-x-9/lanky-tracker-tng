b3d9bfd2f26a10c041984ce21c1110dba78f05b3
A-l-e-x-99 <alexs23743@gmail.com>
2026-09-09 00:49:05 -0400

    Here comes the most complicated Wrinkly Door set...

--- src/renderer/src/components/levels/0-isles/wrinkly/ForestLobby.tsx
+++ src/renderer/src/components/levels/0-isles/wrinkly/ForestLobby.tsx
@@ -1,10 +1,74 @@
+import { useShallow } from 'zustand/react/shallow'
+import useDonkStore from '@renderer/store'
 import WrinklyPool from '@renderer/components/pools/WrinklyDoors'
 import { useForestLobbyGeneric } from '@renderer/hooks/isles'
+import { useDk, useDiddy, useLanky, useTiny, useChunky, useGrab } from '@renderer/hooks/kongs'
+import { useWrinklyDoorItem } from '@renderer/hooks/settings'
+import { useCurrentGBCount, useCurrentBlueprintCount, useCurrentKeyCount, useCurrentBananaMedalCount, useCurrentCrownCount, useCurrentFairyCount, useCurrentRainbowCoinCount, useCurrentPearlCount, useCurrentCBCount } from '@renderer/hooks/consumables'
 import IslesCheck from '../check'
 
 /*Since Version 4 of the DK64 Randomizer, you can have major rewards in Wrinkly Kong's hint doors (and hints on checks).*/
 const ForestLobbyDK: React.FC = () => {
 const canDo = useForestLobbyGeneric()
+const [locked, kongLocked, grabLocked] = useDonkStore(useShallow((state) => [state.settings.lockedWrinklyDoors, state.ui.konglessHintDoorsOff, state.ui.fungiLobbyOptionOff]))
+const doorItemCount = useDonkStore(useShallow((state) => state.wrinklyDoors.gloomyGalleon))
+const hasDK = useDk()
+const hasDiddy = useDiddy()
+const hasLanky = useLanky()
+const hasTiny = useTiny()
+const hasChunky = useChunky()
+const doorItem = useWrinklyDoorItem()
+let currentItemCount = 0
+const currentGBs = useCurrentGBCount()
+const currentBPs = useCurrentBlueprintCount()
+const currentCrowns = useCurrentCrownCount()
+const currentKeys = useCurrentKeyCount()
+const currentMedals = useCurrentBananaMedalCount()
+const currentFairies = useCurrentFairyCount()
+const currentRainbowCoins = useCurrentRainbowCoinCount()
+const currentPearls = useCurrentPearlCount()
+const currentCBs = useCurrentCBCount()
+const hasGrab = useGrab()
+
+switch (doorItem) {
+  case 1: {
+    currentItemCount = currentBPs
+    break
+  }
+  case 2: {
+    currentItemCount = currentCrowns
+    break
+  }
+  case 3: {
+    currentItemCount = currentKeys
+    break
+  }
+  case 4: {
+    currentItemCount = currentMedals
+    break
+  }
+  case 5: {
+    currentItemCount = currentFairies
+    break
+  }
+  case 6: {
+    currentItemCount = currentRainbowCoins
+    break
+  }
+  case 7: {
+    currentItemCount = currentPearls
+    break
+  }
+  case 8: {
+    currentItemCount = currentCBs
+    break
+  }
+  default: {
+    currentItemCount = currentGBs
+  }
+}
+
+if (!locked && !kongLocked && !grabLocked) {
   return (
     <WrinklyPool>
         <IslesCheck
@@ -43,7 +107,7 @@ const canDo = useForestLobbyGeneric()
           canGetBreak={canDo.out}
         />
     </WrinklyPool>
-  )
+  )}
 }
 
 export default ForestLobbyDK
