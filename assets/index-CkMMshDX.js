99bd1bc2c81d44cdc2bc3d214f654e7902185b25
A-l-e-x-9 <alexs23743@gmail.com>
2026-09-11 15:32:57 +0000

    Deploying to gh-pages from @ A-l-e-x-9/lanky-tracker-tng@28ba2be6be1b9ef34effbe3ac9db40ad9037c760 🚀

--- assets/index-CkMMshDX.js
+++ assets/index-CkMMshDX.js
@@ -54026,13 +54026,10 @@ const MoveTable = () => {
       ),
       " ",
       /* @__PURE__ */ jsxRuntimeExports.jsx(SimpleIcon, { storeKey: "cranky", title: "Cranky Kong", imgUrl: crankyIcon, prefix: "moves", updateItem: setMove }),
-      " ",
       /* @__PURE__ */ jsxRuntimeExports.jsx(SimpleIcon, { storeKey: "funky", title: "New Funky Mode!", imgUrl: funkyIcon, prefix: "moves", updateItem: setMove }),
-      " ",
       /* @__PURE__ */ jsxRuntimeExports.jsx(SimpleIcon, { storeKey: "candy", title: "Candy Kong", imgUrl: anyInstIcon, prefix: "moves", updateItem: setMove }),
-      " ",
       /* @__PURE__ */ jsxRuntimeExports.jsx(SimpleIcon, { storeKey: "snide", title: "Snide", imgUrl: snideIcon, prefix: "moves", updateItem: setMove }),
-      " "
+      "  //icon showing total number of Blueprints coming soon  "
     ] })
   ] });
 };
